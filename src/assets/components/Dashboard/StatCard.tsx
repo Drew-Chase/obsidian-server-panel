e560824fc486e5982e0f4dd0f8c7845651a335ea
Drew Chase (Home) <dcmanproductions@gmail.com>
2024-09-13 14:23:02 -0400

    Refactor StatCard component layout and props
    Revised StatCard's layout to optimize styling and removed unused Dropdown elements. Introduced type flexibility for `value` and added a progress bar for numerical values. This enhances clarity and visual presentation

--- src/assets/components/Dashboard/StatCard.tsx
+++ src/assets/components/Dashboard/StatCard.tsx
@@ -1,45 +1,33 @@
 import {ReactElement} from "react";
-import {Button, Dropdown, DropdownItem, DropdownMenu, DropdownTrigger} from "@nextui-org/react";
-import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {faEllipsisH} from "@fortawesome/free-solid-svg-icons";
+import {Progress} from "@nextui-org/react";
 
 interface StatCardProps
 {
     title?: string;
-    value?: number;
+    value?: number | string;
     maxValue?: number;
-    valueContext?: string;
     icon?: ReactElement;
-    children?: ReactElement<typeof DropdownItem>[] | ReactElement<typeof DropdownItem>;
 }
 
 export default function StatCard(props: StatCardProps)
 {
     return (
-        <div className={"flex flex-col bg-neutral-600 rounded-lg shadow-lg"}>
-            <div className={"flex flex-row w-full"}>
-
+        <div className={"flex flex-col bg-neutral-600 rounded-2xl shadow-lg p-8 max-w-md w-full mx-2"}>
+            <div className={"flex flex-row w-full items-center mb-3"}>
                 <span className={"text-primary"}>{props.icon}</span>
-                <p className={"text-lg font-semibold ml-2 mr-auto"}>{props.title}</p>
-                {props.children && (
-                    <Dropdown>
-                        <DropdownTrigger><Button variant={"ghost"}><FontAwesomeIcon icon={faEllipsisH}/></Button></DropdownTrigger>
-                        <DropdownMenu>
-                            {
-                                (() =>
-                                    {
-                                        if (Array.isArray(props.children))
-                                            return props.children.map((child) => (<>{child}</>));
-                                        return <>{props.children}</>;
-                                    }
-                                )();
-                            }
-                        </DropdownMenu>
-                    </Dropdown>
-                )}
-
-
+                <p className={"text-tiny font-medium ml-2 mr-auto"}>{props.title}</p>
             </div>
+            <p className={"text-4xl font-semibold mb-4"}>{props.value}</p>
+            {props.maxValue && (
+                <>
+                    <p className={"text-sm text-neutral-500 ml-auto"}>of {props.maxValue}</p>
+                    <Progress
+                        size={"sm"}
+                        value={Number.parseInt(props.value?.toString().replace(/\D/g, "") ?? "0")}
+                        maxValue={props.maxValue}
+                    />
+                </>
+            )}
         </div>
     );
 }
