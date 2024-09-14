b85c6576a4402757490a68228137c2df544a7d73
Drew Chase (Home) <dcmanproductions@gmail.com>
2024-09-13 22:07:39 -0400

    Create MinecraftVersionsList component
    Add MinecraftVersionsList component with a styled list overview of Minecraft versions. Includes a switch for displaying snapshots and buttons for version switching actions using NextUI and custom styles.

---
+++ src/assets/components/Server/MinecraftVersionsList.tsx
@@ -0,0 +1,45 @@
+import {Button, Listbox, ListboxItem, ScrollShadow, Tooltip} from "@nextui-org/react";
+import ExtendedSwitch from "../Extends/ExtendedSwitch.tsx";
+import DownloadFile from "../../images/DownloadFile.svg.tsx";
+
+export default function MinecraftVersionsList()
+{
+    return (
+        <div className={"flex flex-col bg-neutral-600 rounded-3xl shadow-lg p-8 grow w-full mx-2 max-h-[400px] h-dvh overflow-y-auto"}>
+            <div className={"flex flex-row"}>
+
+                <p className={"text-lg font-semibold mr-auto"}>Minecraft Versions</p>
+                <ExtendedSwitch
+                    label={"Include Snapshots"}
+                />
+            </div>
+
+            <div className={"flex flex-row w-full text-tiny my-4"}>
+                <p className={"mr-auto"}>Version / Type</p>
+                <p>Actions</p>
+            </div>
+            <ScrollShadow className={"max-h-[300px] h-[400px] overflow-y-auto"}>
+                <Listbox>
+                    {Array.from({length: 10}, (_, i) => (
+                        <ListboxItem
+                            key={i}
+                            title={
+                                <div className={"flex flex-row items-center gap-2"}>
+                                    <p className={"max-w-[90px] truncate"}>1.20.4</p>
+                                </div>
+                            }
+                            description={i === 0 ? "latest release" : i === 1 ? "latest snapshot" : "release"}
+                            endContent={
+                                <div className={"flex flex-row"}>
+                                    <Tooltip content={"Switch to this version."}>
+                                        <Button variant={"light"} size={"sm"} className={"min-w-0"}><DownloadFile/></Button>
+                                    </Tooltip>
+                                </div>
+                            }
+                        />
+                    ))}
+                </Listbox>
+            </ScrollShadow>
+        </div>
+    );
+}
