6275f1cd3455f0f95fda4766d91d019abee000e0
Drew Chase (Home) <dcmanproductions@gmail.com>
2024-09-13 22:08:02 -0400

    Add BackupsList component to display backup items
    Introduced a new BackupsList component that features a scrollable list of backups with timestamps and descriptions. Included a button with a tooltip for creating manual backups. The layout is styled with flexbox and NextUI components for consistency and responsiveness.

---
+++ src/assets/components/Server/BackupsList.tsx
@@ -0,0 +1,38 @@
+import {Button, Listbox, ListboxItem, ScrollShadow, Tooltip} from "@nextui-org/react";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faPlus} from "@fortawesome/free-solid-svg-icons";
+
+export default function BackupsList()
+{
+    return (
+        <div className={"flex flex-col bg-neutral-600 rounded-3xl shadow-lg p-8 w-full mx-2 max-h-[400px] h-dvh overflow-y-auto grow"}>
+            <div className={"flex flex-row"}>
+
+                <p className={"text-lg font-semibold mr-auto"}>Backups</p>
+                <Tooltip content={"Create a manual backup"}>
+                    <Button><FontAwesomeIcon icon={faPlus}/></Button>
+                </Tooltip>
+            </div>
+
+            <div className={"flex flex-row w-full text-tiny my-4"}>
+                <p className={"mr-auto"}>Name / Type</p>
+                <p>Timestamp</p>
+            </div>
+            <ScrollShadow className={"max-h-[300px] h-[400px] overflow-y-auto"}>
+                <Listbox>
+                    {Array.from({length: 10}, (_, i) => (
+                        <ListboxItem
+                            key={i}
+                            description={"Automatic backup"}
+                            endContent={<p className={"text-tiny text-nowrap"}>2:36:50 PM</p>}
+                        >
+                            <div className={"flex flex-row items-center gap-2"}>
+                                <p className={"max-w-[90px] truncate"}>Backup #{i}</p>
+                            </div>
+                        </ListboxItem>
+                    ))}
+                </Listbox>
+            </ScrollShadow>
+        </div>
+    );
+}
