875877b3f59599fdeed8990c6b00c14dcea1ab17
Drew Chase (Home) <dcmanproductions@gmail.com>
2024-09-14 01:51:21 -0400

    Add ServerBackups page component
    Created the ServerBackups component to display server backups and backup options. This includes integrating ExtendedBackupsList and BackupOptionsPanel components for better functionality and organization.

---
+++ src/assets/pages/Server/ServerBackups.tsx
@@ -0,0 +1,17 @@
+import ExtendedBackupsList from "../../components/Server/ExtendedBackupsList.tsx";
+import BackupOptionsPanel from "../../components/Server/BackupOptionsPanel.tsx";
+
+export default function ServerBackups()
+{
+    return (
+        <div className={"flex flex-col gap-8"}>
+            <div className={"flex flex-row"}>
+                <p className={"text-xl font-semibold mr-auto"}>Server Backup</p>
+            </div>
+            <div className={"flex flex-row"}>
+                <ExtendedBackupsList/>
+                <BackupOptionsPanel/>
+            </div>
+        </div>
+    );
+}
