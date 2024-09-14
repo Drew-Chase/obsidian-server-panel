63e442fd57e01dd4d31bc1cbb6114286f38b183a
Drew Chase (Home) <dcmanproductions@gmail.com>
2024-09-13 22:08:52 -0400

    Add RecentConnectionsStat component
    Introduced the RecentConnectionsStat component for the dashboard. This component displays a list of recent connections with player names, server details, and duration using the NextUI library. It features a scrollable view with avatars and connection details.

---
+++ src/assets/components/Dashboard/RecentConnectionsStat.tsx
@@ -0,0 +1,35 @@
+import {Avatar, Listbox, ListboxItem, ScrollShadow} from "@nextui-org/react";
+import testImage from "../../images/test-server.png";
+
+export default function RecentConnectionsStat()
+{
+    return (
+        <div className={"flex flex-col bg-neutral-600 rounded-3xl shadow-lg p-8 max-w-md w-full mx-2 max-h-[400px] h-dvh overflow-y-auto"}>
+            <p className={"text-lg font-semibold"}>Recent Connections</p>
+
+            <div className={"flex flex-row w-full text-tiny my-4"}>
+                <p className={"mr-auto"}>Players / Server</p>
+                <p>Duration</p>
+            </div>
+            <ScrollShadow className={"max-h-[300px] h-[400px] overflow-y-auto"}>
+                <Listbox>
+                    {Array.from({length: 10}, (_, i) => (
+                        <ListboxItem
+                            key={i}
+                            startContent={
+                                <div className={"rounded-md bg-neutral-800 p-1"}>
+                                    <Avatar src={testImage}/>
+                                </div>
+                            }
+                            description={"SMP Server"}
+                            endContent={<p className={"text-tiny text-nowrap"}>4 Hours</p>}
+                        >
+                            <p>Shroototem</p>
+                        </ListboxItem>
+
+                    ))}
+                </Listbox>
+            </ScrollShadow>
+        </div>
+    );
+}
