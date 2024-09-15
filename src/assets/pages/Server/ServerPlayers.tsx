d408b05f36ceb46ea4e0b52dda8adc6c6d5984c6
Drew Chase (Home) <dcmanproductions@gmail.com>
2024-09-15 00:08:55 -0400

    Add new ServerPlayers component
    Introduce ServerPlayers component to display online players, recent connections, whitelisted players, and banned players in a structured layout. This enhances the server management functionality by providing a comprehensive view of player activity.

---
+++ src/assets/pages/Server/ServerPlayers.tsx
@@ -0,0 +1,21 @@
+import ExtendedRecentConnectionsStat from "../../components/Server/ExtendedRecentConnectionsStat.tsx";
+import ExtendedCurrentOnlinePlayersList from "../../components/Server/ExtendedCurrentOnlinePlayersList.tsx";
+import WhiteListPlayers from "../../components/Server/WhiteListPlayers.tsx";
+import BanPlayersList from "../../components/Server/BanPlayersList.tsx";
+
+export default function ServerPlayers()
+{
+    return (
+        <div className={"flex flex-col gap-8"}>
+            <div className={"flex flex-row"}>
+                <p className={"text-xl font-semibold mr-auto"}>Players</p>
+            </div>
+            <ExtendedCurrentOnlinePlayersList/>
+            <ExtendedRecentConnectionsStat/>
+            <div className={"flex flex-row w-full gap-4"}>
+                <WhiteListPlayers/>
+                <BanPlayersList/>
+            </div>
+        </div>
+    );
+}
