8dfdb0798bc064e6c722b39c9078e750137aeb2a
Drew Chase (Home) <dcmanproductions@gmail.com>
2024-09-15 20:19:50 -0400

    Add InstalledModItem component to Server directory
    This component displays mod details such as icon, name, author, and version. It includes buttons for updating, deleting, and toggling the mod state. The component uses NextUI for UI elements and FontAwesome for icons.

---
+++ src/assets/components/Server/InstalledModItem.tsx
@@ -0,0 +1,48 @@
+import {Avatar, Button, Switch, Tooltip} from "@nextui-org/react";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faArrowsRotate, faCheck, faTrash} from "@fortawesome/free-solid-svg-icons";
+import {useEffect, useState} from "react";
+
+interface ModItemProps
+{
+    icon?: string;
+    name: string;
+    author?: string;
+    version: string;
+}
+
+export default function InstalledModItem(props: ModItemProps)
+{
+    const [disabled, setDisabled] = useState(false);
+    const [requiresUpdate, setRequiresUpdate] = useState(false);
+
+    useEffect(() =>
+    {
+        const random = Math.random();
+        setDisabled(random > 0.5);
+        setRequiresUpdate(random > 0.5);
+    }, []);
+
+
+    return (
+        <div className={"flex flex-row shrink-0 p-4 justify-between items-center"}>
+            <div className={"flex flex-row shrink-0"}>
+                {props.icon && <Avatar src={props.icon} radius={"sm"} className={"mr-4"}/>}
+                <div className={"flex flex-col"}>
+                    <p>{props.name}</p>
+                    <p className={"text-tiny opacity-70"}>By {props.author}</p>
+                </div>
+            </div>
+            <p className={"opacity-70"}>{props.version}</p>
+            <div className={"flex flex-row items-center gap-4"}>
+                <Tooltip content={`Delete '${props.name}'`}>
+                    <Button color={"danger"} className={"min-w-0"}><FontAwesomeIcon icon={faTrash}/></Button>
+                </Tooltip>
+                <Tooltip content={`Update '${props.name}'`}>
+                    <Button isDisabled={!requiresUpdate} className={"min-w-0"}><FontAwesomeIcon icon={requiresUpdate ? faArrowsRotate : faCheck}/></Button>
+                </Tooltip>
+                <Switch isSelected={!disabled} onValueChange={value => setDisabled(!value)}/>
+            </div>
+        </div>
+    );
+}
