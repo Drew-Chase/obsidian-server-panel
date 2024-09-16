1a0d35df6de9e94292446f08f031eccc9919da4c
Drew Chase (Home) <dcmanproductions@gmail.com>
2024-09-15 20:18:39 -0400

    Simplify ServerBackups component layout
    Refactor the ServerBackups component by combining nested div elements into a single flex-row container and adjusting the max height to 60px. This reduces the complexity of the layout and ensures a cleaner, more maintainable codebase.

--- src/assets/pages/Server/ServerBackups.tsx
+++ src/assets/pages/Server/ServerBackups.tsx
@@ -4,14 +4,9 @@ import BackupOptionsPanel from "../../components/Server/BackupOptionsPanel.tsx";
 export default function ServerBackups()
 {
     return (
-        <div className={"flex flex-col gap-8"}>
-            <div className={"flex flex-row"}>
-                <p className={"text-xl font-semibold mr-auto"}>Server Backup</p>
-            </div>
-            <div className={"flex flex-row max-h-[calc(100dvh_-_130px)]"}>
-                <ExtendedBackupsList/>
-                <BackupOptionsPanel/>
-            </div>
+        <div className={"flex flex-row max-h-[calc(100dvh_-_60px)]"}>
+            <ExtendedBackupsList/>
+            <BackupOptionsPanel/>
         </div>
     );
 }
