80be47f129c2cb4958d5a72331a1c8f117dc20a8
Drew Chase (Work) <dcmanproductions@gmail.com>
2024-09-16 16:04:37 -0400

    Add user-related data structures to authentication module
    Introduce various structs for handling user data including User, UserLogin, UserRegistration, UserResponse, Token, PublicUser, and PublicUsersList. These structs utilize serde for serialization and deserialization.

---
+++ src-actix/authentication/src/data.rs
@@ -0,0 +1,56 @@
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct User {
+    pub id: i64,
+    pub username: String,
+    pub password: String,
+    pub admin: bool,
+    pub created_at: String,
+    pub updated_at: String,
+    pub last_login: String,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct UserLogin {
+    pub username: String,
+    pub password: String,
+}
+#[derive(Debug, Serialize, Deserialize)]
+pub struct UserRegistration {
+    pub username: String,
+    pub password: String,
+    pub access_token: String,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct UserResponse {
+    pub id: i64,
+    pub username: String,
+    pub admin: bool,
+    pub created_at: String,
+    pub updated_at: String,
+    pub last_login: String,
+    pub token: String,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Token {
+    pub username: String,
+    pub hash: String,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct PublicUser {
+    pub id: i64,
+    pub username: String,
+    pub admin: bool,
+    pub created_at: String,
+    pub updated_at: String,
+    pub last_login: String,
+}
+#[derive(Debug, Serialize, Deserialize)]
+pub struct PublicUsersList {
+    pub users: Vec<PublicUser>,
+    pub count: i64,
+}
