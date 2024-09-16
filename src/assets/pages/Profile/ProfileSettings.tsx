3232ff0fb7b2bed6557274861c754b9f6079fe74
Drew Chase (Home) <dcmanproductions@gmail.com>
2024-09-15 20:54:16 -0400

    Set document title in ProfileSettings component
    Added a call to setTitle() to set the document title to "Profile Settings" when the ProfileSettings component is rendered. This enhances the user experience by providing a more descriptive and relevant page title.

--- src/assets/pages/Profile/ProfileSettings.tsx
+++ src/assets/pages/Profile/ProfileSettings.tsx
@@ -1,5 +1,8 @@
+import {setTitle} from "../../../main.tsx";
+
 export default function ProfileSettings()
 {
+    setTitle("Profile Settings")
     return (
         <div className={"flex flex-col bg-neutral-600 rounded-3xl shadow-lg p-8 w-full mx-2 overflow-y-auto"}>
             <p className={"text-lg font-semibold mr-auto mb-8"}>Profile Settings</p>
