da3d5e9d2fab44b82818077ae447d8bb2fe53109
Drew Chase (Home) <dcmanproductions@gmail.com>
2024-09-15 20:25:02 -0400

    Add ProfileSettings component
    Create ProfileSettings.tsx to handle user profile settings UI. Includes a flexible, fully styled container with a heading, ready for further content expansion.

---
+++ src/assets/pages/Profile/ProfileSettings.tsx
@@ -0,0 +1,8 @@
+export default function ProfileSettings()
+{
+    return (
+        <div className={"flex flex-col bg-neutral-600 rounded-3xl shadow-lg p-8 w-full mx-2 overflow-y-auto"}>
+            <p className={"text-lg font-semibold mr-auto mb-8"}>Profile Settings</p>
+        </div>
+    );
+}
