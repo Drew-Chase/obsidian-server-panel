c02aecf7d358b6b19ed8e9a9491dbc9493195649
Drew Chase (Home) <dcmanproductions@gmail.com>
2024-09-15 20:54:25 -0400

    Set page title in ServerBackups component
    Added a setTitle call to set the page title to "Server Backups" in the ServerBackups component. This enhances user experience by clearly indicating the purpose of the page.

--- src/assets/pages/Server/ServerBackups.tsx
+++ src/assets/pages/Server/ServerBackups.tsx
@@ -1,8 +1,10 @@
 import ExtendedBackupsList from "../../components/Server/ExtendedBackupsList.tsx";
 import BackupOptionsPanel from "../../components/Server/BackupOptionsPanel.tsx";
+import {setTitle} from "../../../main.tsx";
 
 export default function ServerBackups()
 {
+    setTitle("Server Backups");
     return (
         <div className={"flex flex-row max-h-[calc(100dvh_-_60px)]"}>
             <ExtendedBackupsList/>
