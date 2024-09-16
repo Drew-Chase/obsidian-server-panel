e2b365193f50e9e44633de0981f6307a774694e8
Drew Chase (Work) <dcmanproductions@gmail.com>
2024-09-16 16:01:46 -0400

    Restrict registration to users with a valid code
    Added code validation to restrict the registration process to users who provide a valid registration code. Improved error handling for missing or mismatched input fields and included user feedback for missing registration code.

--- src/assets/pages/Register.tsx
+++ src/assets/pages/Register.tsx
@@ -1,7 +1,7 @@
 import {setTitle} from "../../main.tsx";
 import {useState} from "react";
-import {useNavigate} from "react-router-dom";
-import {Button, Card, CardBody, CardHeader, Input} from "@nextui-org/react";
+import {useNavigate, useSearchParams} from "react-router-dom";
+import {Button, Card, CardBody, CardHeader, Input, Link} from "@nextui-org/react";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faEnvelope, faEye, faEyeSlash, faKey} from "@fortawesome/free-solid-svg-icons";
 
@@ -20,7 +20,13 @@ export default function Register()
     const [usernameError, setUsernameError] = useState("");
     const [passwordError, setPasswordError] = useState("");
     const [error, setError] = useState("");
+
+    const [params, _] = useSearchParams();
+    const code = params.get("code");
+    // @ts-ignore
     const navigate = useNavigate();
+
+    console.log("Code", code);
 
     const resetErrors = () =>
     {
@@ -33,6 +39,14 @@ export default function Register()
     {
         setIsLoggingIn(true);
         resetErrors();
+
+        if (!username || !password || password !== confirmPassword)
+        {
+            setUsernameError(!username ? "Username is required" : "");
+            setPasswordError(!password ? "Password is required" : password !== confirmPassword ? "Passwords do not match" : "");
+            setIsLoggingIn(false);
+            return;
+        }
 
         setIsLoggingIn(false);
     };
@@ -47,66 +61,74 @@ export default function Register()
                 >
                     <CardHeader><h1 className={"text-5xl"}>Register</h1></CardHeader>
                     <CardBody>
-                        <div className={"flex flex-col gap-4"}>
-                            <Input
-                                autoFocus
-                                label={"Email or Username"}
-                                placeholder={"Enter your username or email"}
-                                type={"text"}
-                                variant={"underlined"}
-                                startContent={<FontAwesomeIcon icon={faEnvelope} opacity={.5}/>}
-                                autoComplete={"username webauthn"}
-                                value={username}
-                                onValueChange={setUsername}
-                                isInvalid={!!usernameError}
-                                errorMessage={usernameError}
-                                onKeyUp={(e) => e.key === "Enter" && register()}
-                            />
-                            <Input
-                                label={"Password"}
-                                variant={"underlined"}
-                                placeholder={"Enter your password"}
-                                type={showPassword ? "text" : "password"}
-                                autoComplete={"current-password"}
-                                startContent={<FontAwesomeIcon icon={faKey} opacity={.5}/>}
-                                endContent={
-                                    <FontAwesomeIcon
-                                        onClick={() => setShowPassword(prev => !prev)}
-                                        icon={showPassword ? faEye : faEyeSlash}
-                                        opacity={showPassword ? 1 : 0.5}
-                                        className={"cursor-pointer"}
-                                    />
-                                }
-                                value={password}
-                                onValueChange={setPassword}
-                                isInvalid={!!passwordError}
-                                errorMessage={passwordError}
-                                onKeyUp={(e) => e.key === "Enter" && register()}
-                            />
-                            <Input
-                                label={"Confirm Password"}
-                                variant={"underlined"}
-                                placeholder={"Confirm your password"}
-                                type={showConfirmPassword ? "text" : "password"}
-                                autoComplete={"current-password"}
-                                startContent={<FontAwesomeIcon icon={faKey} opacity={.5}/>}
-                                endContent={
-                                    <FontAwesomeIcon
-                                        onClick={() => setShowConfirmPassword(prev => !prev)}
-                                        icon={showConfirmPassword ? faEye : faEyeSlash}
-                                        opacity={showConfirmPassword ? 1 : 0.5}
-                                        className={"cursor-pointer"}
-                                    />
-                                }
-                                value={confirmPassword}
-                                onValueChange={setConfirmPassword}
-                                isInvalid={!!passwordError}
-                                errorMessage={passwordError}
-                                onKeyUp={(e) => e.key === "Enter" && register()}
-                            />
-                            {error && <p className={"text-danger"}><strong>Error:</strong> {error}</p>}
-                            <Button radius={"lg"} color={"primary"} isLoading={isLoggingIn} onClick={register}>Register</Button>
-                        </div>
+
+                        {!code ? (
+                            <>
+                                <p className={"text-lg text-danger italic mb-4"}>No registration code was provided, please contact your system administrator for more information.</p>
+                                <Button as={Link} href={"/"} radius={"lg"} color={"primary"}>Back to Login</Button>
+                            </>
+                        ) : (
+                            <div className={"flex flex-col gap-4"}>
+                                <Input
+                                    autoFocus
+                                    label={"Email or Username"}
+                                    placeholder={"Enter your username or email"}
+                                    type={"text"}
+                                    variant={"underlined"}
+                                    startContent={<FontAwesomeIcon icon={faEnvelope} opacity={.5}/>}
+                                    autoComplete={"username webauthn"}
+                                    value={username}
+                                    onValueChange={setUsername}
+                                    isInvalid={!!usernameError}
+                                    errorMessage={usernameError}
+                                    onKeyUp={(e) => e.key === "Enter" && register()}
+                                />
+                                <Input
+                                    label={"Password"}
+                                    variant={"underlined"}
+                                    placeholder={"Enter your password"}
+                                    type={showPassword ? "text" : "password"}
+                                    autoComplete={"current-password"}
+                                    startContent={<FontAwesomeIcon icon={faKey} opacity={.5}/>}
+                                    endContent={
+                                        <FontAwesomeIcon
+                                            onClick={() => setShowPassword(prev => !prev)}
+                                            icon={showPassword ? faEye : faEyeSlash}
+                                            opacity={showPassword ? 1 : 0.5}
+                                            className={"cursor-pointer"}
+                                        />
+                                    }
+                                    value={password}
+                                    onValueChange={setPassword}
+                                    isInvalid={!!passwordError}
+                                    errorMessage={passwordError}
+                                    onKeyUp={(e) => e.key === "Enter" && register()}
+                                />
+                                <Input
+                                    label={"Confirm Password"}
+                                    variant={"underlined"}
+                                    placeholder={"Confirm your password"}
+                                    type={showConfirmPassword ? "text" : "password"}
+                                    autoComplete={"current-password"}
+                                    startContent={<FontAwesomeIcon icon={faKey} opacity={.5}/>}
+                                    endContent={
+                                        <FontAwesomeIcon
+                                            onClick={() => setShowConfirmPassword(prev => !prev)}
+                                            icon={showConfirmPassword ? faEye : faEyeSlash}
+                                            opacity={showConfirmPassword ? 1 : 0.5}
+                                            className={"cursor-pointer"}
+                                        />
+                                    }
+                                    value={confirmPassword}
+                                    onValueChange={setConfirmPassword}
+                                    isInvalid={!!passwordError}
+                                    errorMessage={passwordError}
+                                    onKeyUp={(e) => e.key === "Enter" && register()}
+                                />
+                                {error && <p className={"text-danger"}><strong>Error:</strong> {error}</p>}
+                                <Button radius={"lg"} color={"primary"} isLoading={isLoggingIn} onClick={register}>Register</Button>
+                            </div>
+                        )}
                     </CardBody>
 
                 </Card>
