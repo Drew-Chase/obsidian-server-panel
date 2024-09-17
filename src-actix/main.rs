796fe9612893b8f20878edf6286320deb66f0342
Drew Chase (Home) <dcmanproductions@gmail.com>
2024-09-16 20:46:14 -0400

    Add Java API endpoint and optimize server initialization
    Introduced a new module `java_endpoint` and included it in the server configuration for handling Java-specific API requests. Additionally, improved the server startup message with structured logging and streamlined some initialization code for better readability.

--- src-actix/main.rs
+++ src-actix/main.rs
@@ -1,5 +1,6 @@
 mod authentication_endpoint;
 mod minecraft_endpoint;
+mod java_endpoint;
 
 use actix_files::Files;
 use actix_files::NamedFile;
@@ -7,63 +8,68 @@ use actix_web::error::ErrorInternalServerError;
 use actix_web::{error, middleware, web, App, HttpResponse, HttpServer, Responder};
 use serde_json::json;
 
+use log::info;
+
 // Function to serve the index.html file
 async fn index() -> Result<impl Responder, actix_web::Error> {
-    match NamedFile::open_async("wwwroot/index.html").await {
-        Ok(file) => Ok(file),
-        Err(_) => Err(ErrorInternalServerError("Error serving index.html")),
-    }
+	match NamedFile::open_async("wwwroot/index.html").await {
+		Ok(file) => Ok(file),
+		Err(_) => Err(ErrorInternalServerError("Error serving index.html")),
+	}
 }
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    std::env::set_var("RUST_LOG", "actix_web=trace");
-    env_logger::init();
+	std::env::set_var("RUST_LOG", "trace");
+	env_logger::init();
+	let port = 1420; // Port to listen on
 
-    let port = 1420; // Port to listen on
+	let server = HttpServer::new(move || {
+		App::new()
+			.wrap(middleware::Logger::default())
+			.app_data(
+				web::JsonConfig::default()
+					.limit(4096)
+					.error_handler(|err, _req| {
+						let error = json!({ "error": format!("{}", err) });
+						error::InternalError::from_response(
+							err,
+							HttpResponse::BadRequest().json(error),
+						)
+							.into()
+					}),
+			)
+			// Handle API routes here
+			.service(
+				web::scope("api")
+					.service(
+						web::scope("auth")
+							.service(authentication_endpoint::create_user)
+							.service(authentication_endpoint::login)
+							.service(authentication_endpoint::login_with_token)
+							.service(authentication_endpoint::generate_access_token)
+							.service(authentication_endpoint::get_access_tokens)
+							.service(authentication_endpoint::check_if_access_token_exists)
+							.service(authentication_endpoint::get_users),
+					)
+					.service(
+						web::scope("minecraft")
+							.service(minecraft_endpoint::get_minecraft_versions)
+					)
+					.service(
+						web::scope("java")
+							.service(java_endpoint::get_java_versions),
+					)
 
-    println!("Starting server at http://127.0.0.1:{port}", port = port,);
-
-    HttpServer::new(move || {
-        App::new()
-            .wrap(middleware::Logger::default())
-            .app_data(
-                web::JsonConfig::default()
-                    .limit(4096)
-                    .error_handler(|err, _req| {
-                        let error = json!({ "error": format!("{}", err) });
-                        error::InternalError::from_response(
-                            err,
-                            HttpResponse::BadRequest().json(error),
-                        )
-                        .into()
-                    }),
-            )
-            // Handle API routes here
-            .service(
-                web::scope("api")
-                    .service(
-                        web::scope("auth")
-                            .service(authentication_endpoint::create_user)
-                            .service(authentication_endpoint::login)
-                            .service(authentication_endpoint::login_with_token)
-                            .service(authentication_endpoint::generate_access_token)
-                            .service(authentication_endpoint::get_access_tokens)
-                            .service(authentication_endpoint::check_if_access_token_exists)
-                            .service(authentication_endpoint::get_users),
-                    )
-                    .service(
-                        web::scope("minecraft")
-                            .service(minecraft_endpoint::get_minecraft_versions)
-                            .service(minecraft_endpoint::get_java_versions),
-                    ),
-            )
-            // Serve static files from the wwwroot directory
-            .service(Files::new("/", "wwwroot").index_file("index.html"))
-            // Handle all other routes by serving the index.html file
-            .default_service(web::route().to(index))
-    })
-    .workers(4)
-    .bind(format!("0.0.0.0:{port}", port = port))?
-    .run()
-    .await
+				,
+			)
+			// Serve static files from the wwwroot directory
+			.service(Files::new("/", "wwwroot").index_file("index.html"))
+			// Handle all other routes by serving the index.html file
+			.default_service(web::route().to(index))
+	})
+		.workers(4)
+		.bind(format!("0.0.0.0:{port}", port = port))?
+		.run();
+	info!("Starting server at http://127.0.0.1:{port}...");
+	server.await
 }
