0c05bb949286a844d8e3c1123890b73c215e6b5d
Drew Chase (Home) <dcmanproductions@gmail.com>
2024-09-16 20:46:03 -0400

    Update Minecraft endpoint functions
    Replaced `get_java_versions` with `get_latest_release`, `get_latest_snapshot`, and `get_version_by_id` endpoints for fetching specific Minecraft versions. Enhanced error handling to provide more detailed HTTP responses.

--- src-actix/minecraft_endpoint.rs
+++ src-actix/minecraft_endpoint.rs
@@ -10,10 +10,27 @@ pub async fn get_minecraft_versions() -> impl Responder {
     }
 }
 
-#[get("/java-versions")]
-pub async fn get_java_versions() -> impl Responder {
-    match minecraft_java::get_java_versions().await {
-        Ok(versions) => HttpResponse::Ok().json(versions),
+#[get("/latest-release")]
+pub async fn get_latest_release() -> impl Responder {
+    match minecraft_version::get_latest_release().await {
+        Ok(version) => HttpResponse::Ok().json(version),
+        Err(e) => HttpResponse::BadRequest().json(json!({"error": e})),
+    }
+}
+
+#[get("/latest-snapshot")]
+pub async fn get_latest_snapshot() -> impl Responder {
+    match minecraft_version::get_latest_snapshot().await {
+        Ok(version) => HttpResponse::Ok().json(version),
+        Err(e) => HttpResponse::BadRequest().json(json!({"error": e})),
+    }
+}
+
+
+#[get("/version/{version}")]
+pub async fn get_version_by_id(version: actix_web::web::Path<String>) -> impl Responder {
+    match minecraft_version::get_version_by_id(&version, None).await {
+        Ok(version) => HttpResponse::Ok().json(version),
         Err(e) => HttpResponse::BadRequest().json(json!({"error": e})),
     }
 }
