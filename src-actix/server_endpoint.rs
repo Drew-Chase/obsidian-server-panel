8306ce4e9561aa10244c2cbe0cfef5289447b49b
Drew (Linux) <dcmanproductions@gmail.com>
2024-09-20 14:41:22 -0400

    Enhance server endpoint authorization
    Added authentication checks for the `get_servers` and `get_server_by_id` endpoints to ensure only authorized users can access server data. Integrated `hashids` decode functionality to handle ID parsing and improved error messaging for better communication in-case of failures.

--- src-actix/server_endpoint.rs
+++ src-actix/server_endpoint.rs
@@ -1,5 +1,6 @@
 use actix_web::{get, post, web, HttpMessage, HttpRequest, HttpResponse, Responder};
 use authentication::data::User;
+use crypto::hashids::decode;
 use log::error;
 use serde::Deserialize;
 use serde_json::json;
@@ -10,13 +11,42 @@ use servers::server_db::Server;
 use std::path::Path;
 
 #[get("/")]
-pub async fn get_servers() -> impl Responder {
-    HttpResponse::Ok()
+pub async fn get_servers(req: HttpRequest) -> impl Responder {
+    if let Some(user) = req.extensions().get::<User>() {
+        let servers = match server_db::get_servers_by_owner(user.id) {
+            Ok(s) => s,
+            Err(e) => {
+                error!("{}", e);
+                return HttpResponse::BadRequest().json(json!({"error":e}));
+            }
+        };
+        return HttpResponse::Ok().json(servers);
+    }
+
+    HttpResponse::Unauthorized().json(json!({"error":"Unauthorized"}))
 }
 
 #[get("/{id}")]
-pub async fn get_server_by_id(id: web::Path<String>) -> impl Responder {
-    HttpResponse::Ok()
+pub async fn get_server_by_id(id: web::Path<String>, req: HttpRequest) -> impl Responder {
+    if let Some(user) = req.extensions().get::<User>() {
+        let id_number = match decode(id.as_str()).first() {
+            Some(i) => *i as u32,
+            None => return HttpResponse::BadRequest().json(json!({"error":"Invalid ID"})),
+        };
+        let server = match server_db::get_server_by_id(id_number) {
+            Some(s) => s,
+            None => {
+                let msg = format!("Server with id: {} not found", id_number);
+                error!("{}", msg);
+                return HttpResponse::BadRequest().json(json!({"error":msg}));
+            }
+        };
+        if server.owner == user.id {
+            return HttpResponse::Ok().json(server);
+        }
+    }
+
+    HttpResponse::Unauthorized().json(json!({"error":"Unauthorized"}))
 }
 
 #[derive(Deserialize)]
