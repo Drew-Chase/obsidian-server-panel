178f78101bfe3dbac544963434e6b61a13f03435
Drew Chase (Work) <dcmanproductions@gmail.com>
2024-09-23 16:22:54 -0400

    Refactor backup module structure and add file hash table
    Restructure the backup module code by moving `BackupItem` and related functions to separate files. Added a new table `file_hash_table` to track file metadata and hashes. Also, included a function to ensure the creation of the backups directory if it doesn't exist.

--- src-actix/backups/src/lib.rs
+++ src-actix/backups/src/lib.rs
@@ -1,180 +1,16 @@
+pub mod backup_item;
+pub mod hashed_file;
+
 use log::{error, info};
+use serde::{Deserialize, Serialize};
+use sha2::{Digest, Sha256};
 use sqlite::{State, Statement};
+use std::env;
+use std::error::Error;
+use std::fs::File;
+use std::io::{BufReader, Read};
 use std::path::{Path, PathBuf};
 use std::time::SystemTime;
-
-pub enum BackupItemType {
-	AUTO,
-	MANUAL
-}
-
-pub struct BackupItem {
-	id: u32,
-	name: String,
-	path: PathBuf,
-	r#type: BackupItemType,
-	timestamp: SystemTime,
-	size: u64,
-	server: u32,
-}
-
-impl BackupItem {
-	pub fn get_list_of_backups() -> Vec<Self>
-	{
-		let mut result: Vec<Self> = vec![];
-		let conn = match create_connection() {
-			Ok(c) => c,
-			Err(_) => {
-				return result;
-			}
-		};
-		let mut stmt = match conn.prepare("select * from `backups`") {
-			Ok(s) => s,
-			Err(e) => {
-				error!("Unable to prepare select statement for the `get_list_of_backups` function of the backups class: {}", e);
-				return result;
-			}
-		};
-
-		while let State::Row = stmt
-			.next()
-			.map_err(|e|
-				{
-					error!("Unable to get next row from the database: {}", e);
-					return State::Done;
-				})
-			.unwrap()
-		{
-			if let Some(item) = Self::from_statement(&stmt) {
-				result.push(item);
-			}
-		}
-
-		result
-	}
-	pub fn get_list_of_backups_from_server(server_id: u32) -> Vec<Self>
-	{
-		let mut result: Vec<Self> = vec![];
-		let conn = match create_connection() {
-			Ok(c) => c,
-			Err(_) => {
-				return result;
-			}
-		};
-		let mut stmt = match conn.prepare("select * from `backups` where server = ?") {
-			Ok(s) => s,
-			Err(e) => {
-				error!("Unable to prepare select statement for the `get_list_of_backups` function of the backups class: {}", e);
-				return result;
-			}
-		};
-
-		match stmt.bind((1, server_id as i64)) {
-			Ok(_) => {},
-			Err(e) => {
-				error!("Unable to bind '{}' -> `server_id` in the `get_list_of_backups_from_server` function of the backups class: {}", server_id, e);
-				return result;
-			}
-		}
-
-		while let State::Row = stmt
-			.next()
-			.map_err(|e|
-				{
-					error!("Unable to get next row from the database: {}", e);
-					return State::Done;
-				})
-			.unwrap()
-		{
-			if let Some(item) = Self::from_statement(&stmt) {
-				result.push(item);
-			}
-		}
-
-		result
-	}
-	pub fn from_id(id: u32) -> Option<Self> {
-		let conn = match create_connection() {
-			Ok(c) => c,
-			Err(_) => {
-				return None;
-			}
-		};
-		let mut stmt = match conn.prepare("select * from `backups` where 'id' = ? LIMIT 1") {
-			Ok(s) => s,
-			Err(e) => {
-				error!("Unable to prepare select statement for the `from_id` function of the backups class: {}", e);
-				return None;
-			}
-		};
-		match stmt.bind((1, id as i64)) {
-			Ok(_) => {},
-			Err(e) => {
-				error!("Unable to bind '{}' -> `id` in the `from_id` function of the backups class: {}", id, e);
-				return None;
-			}
-		}
-		match stmt.next() {
-			Ok(_) => {},
-			Err(e) => {
-				error!("Failed to get result of select query in the `from_id` function of the backups class: {}", e);
-				return None;
-			}
-		};
-
-		Self::from_statement(&stmt)
-	}
-
-	fn from_statement(stmt: &Statement) -> Option<Self>
-	{
-		Some(
-			BackupItem {
-				id: stmt.read::<i64, _>("id").map_err(|e| {
-					error!("Unable to parse the column `id` from the backups table in the `from_id` function: {}", e);
-					return None::<Self>;
-				}).ok()? as u32,
-				path: Path::new(&(stmt.read::<String, _>("path").map_err(|e| {
-					error!("Unable to parse the column `id` from the backups table in the `from_id` function: {}", e);
-					return None::<Self>;
-				}).ok()?)).to_path_buf(),
-
-				name: stmt.read::<String, _>("name").map_err(|e| {
-					error!("Unable to parse the column `name` from the backups table in the `from_id` function: {}", e);
-					return None::<Self>;
-				}).ok()?,
-				r#type: match stmt.read::<String, _>("type").map_err(|e| {
-					error!("Unable to parse the column `type` from the backups table in the `from_id` function: {}", e);
-					return None::<Self>;
-				}).ok()?.as_str() {
-					"AUTO" => BackupItemType::AUTO,
-					"MANUAL" => BackupItemType::MANUAL,
-					_ => {
-						error!("Unknown type value in the `from_id` function");
-						return None::<Self>;
-					}
-				},
-				timestamp: SystemTime::UNIX_EPOCH + std::time::Duration::from_secs(
-					stmt.read::<String, _>("timestamp").map_err(|e| {
-						error!("Unable to parse the column `timestamp` from the backups table in the `from_id` function: {}", e);
-						return None::<Self>;
-					}).ok()?.parse::<u64>().map_err(|e| {
-						error!("Unable to convert timestamp string to u64 in the `from_id` function: {}", e);
-						return None::<Self>;
-					}).ok()?
-				),
-				size: stmt.read::<i64, _>("size").map_err(|e| {
-					error!("Unable to parse the column `size` from the backups table in the `from_id` function: {}", e);
-					return None::<Self>;
-				}).ok()? as u64,
-				server: stmt.read::<i64, _>("server").map_err(|e| {
-					error!("Unable to parse the column `server` from the backups table in the `from_id` function: {}", e);
-					return None::<Self>;
-				}).ok()? as u32,
-			}
-		)
-	}
-}
-
 
 pub fn initialize() {
 	info!("Initializing backups database");
@@ -185,23 +21,48 @@ pub fn initialize() {
 			id INTEGER PRIMARY KEY AUTOINCREMENT,
 			name TEXT NOT NULL,
 			path TEXT NOT NULL,
-			type TEXT NOT NULL,
+			type INTEGER NOT NULL,
+			method INTEGER NOT NULL,
 			timestamp TEXT NOT NULL,
 			size INTEGER NOT NULL,
 			server INTEGER NOT NULL
-		)
+		);
+
+		CREATE TABLE IF NOT EXISTS file_hash_table (
+			path TEXT NOT NULL UNIQUE,
+			name TEXT NOT NULL,
+			hash TEXT NOT NULL,
+			timestamp TEXT NOT NULL,
+			size INTEGER NOT NULL,
+		);
 	",
 	) {
 		error!("Failed to create backups table: {}", e);
 	} else {
 		info!("Successfully created or verified the backups table.");
 	}
+	let backups_dir = get_backups_directory();
+	if !backups_dir.exists() {
+		std::fs::create_dir_all(backups_dir).expect("Unable to create backup directory.");
+	}
 }
-
 
 fn create_connection() -> Result<sqlite::Connection, sqlite::Error> {
 	sqlite::Connection::open("servers.db").map_err(|e| {
-		error!("Failed to open servers database connection for backups: {}", e);
+		error!(
+            "Failed to open servers database connection for backups: {}",
+            e
+        );
 		e
 	})
+}
+
+pub fn get_backups_directory() -> PathBuf {
+	Path::join(
+		env::current_exe()
+			.expect("Failed to get the current executable path")
+			.parent()
+			.expect("Failed to get the parent directory of the executable"),
+		Path::new("backups"),
+	)
 }
