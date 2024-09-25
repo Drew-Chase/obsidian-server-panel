819b26628e8a66594f66f0ecf48ecfef756e4bd5
Drew (Linux) <dcmanproductions@gmail.com>
2024-09-25 19:59:30 -0400

    Refactor index serving and implement asset endpoint
    Refactor the index.html serving logic to use include_dir for embedding static files. Added a new endpoint to serve assets from the embedded directory, enhancing the robustness of static file handling.

--- src-actix/main.rs
+++ src-actix/main.rs
@@ -1,5 +1,6 @@
 mod auth_middleware;
 mod authentication_endpoint;
+mod backups_endpoint;
 mod file_system_endpoint;
 mod java_endpoint;
 mod minecraft_endpoint;
@@ -7,131 +8,147 @@ mod server_endpoint;
 mod server_properties_endpoint;
 mod server_settings_endpoint;
 mod system_stats_endpoint;
-mod backups_endpoint;
 
-use actix_files::Files;
 use actix_files::NamedFile;
+use actix_files::{file_extension_to_mime, Files};
 use actix_web::error::ErrorInternalServerError;
-use actix_web::{error, middleware, web, App, HttpResponse, HttpServer, Responder};
+use actix_web::{error, get, middleware, web, App, HttpResponse, HttpServer, Responder};
+use include_dir::{include_dir, Dir};
 use log::{error, info};
 use serde_json::json;
 use std::sync::Mutex;
 use sysinfo::System;
 
-// Function to serve the index.html file
-async fn index() -> Result<impl Responder, actix_web::Error> {
-	match NamedFile::open_async("wwwroot/index.html").await {
-		Ok(file) => Ok(file),
-		Err(_) => Err(ErrorInternalServerError("Error serving index.html")),
-	}
-}
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-	std::env::set_var("RUST_LOG", "trace");
-	env_logger::init();
-	let port = 1420; // Port to listen on
+    std::env::set_var("RUST_LOG", "trace");
+    env_logger::init();
+    let port = 1420; // Port to listen on
 
-	match authentication::initialize() {
-		Ok(_) => info!("Authentication initialized"),
-		Err(e) => error!("Failed to initialize authentication: {}", e),
-	}
+    match authentication::initialize() {
+        Ok(_) => info!("Authentication initialized"),
+        Err(e) => error!("Failed to initialize authentication: {}", e),
+    }
 
-	servers::server_db::initialize();
-	backups::initialize();
+    servers::server_db::initialize();
+    backups::initialize();
 
-	let sys = web::Data::new(Mutex::new(System::new_all()));
+    let sys = web::Data::new(Mutex::new(System::new_all()));
 
-	let server = HttpServer::new(move || {
-		App::new()
-			.wrap(middleware::Logger::default())
-			.wrap(auth_middleware::AuthMiddleware)
-			.app_data(
-				web::JsonConfig::default()
-					.limit(4096)
-					.error_handler(|err, _req| {
-						let error = json!({ "error": format!("{}", err) });
-						error::InternalError::from_response(
-							err,
-							HttpResponse::BadRequest().json(error),
-						)
-							.into()
-					}),
-			)
-			// Handle API routes here
-			.service(
-				web::scope("api")
-					.service(
-						web::scope("auth")
-							.service(authentication_endpoint::create_user)
-							.service(authentication_endpoint::login)
-							.service(authentication_endpoint::login_with_token)
-							.service(authentication_endpoint::generate_access_token)
-							.service(authentication_endpoint::get_access_tokens)
-							.service(authentication_endpoint::check_if_access_token_exists)
-							.service(authentication_endpoint::get_users),
-					)
-					.service(
-						web::scope("minecraft")
-							.service(minecraft_endpoint::get_minecraft_versions)
-							.service(minecraft_endpoint::get_latest_release)
-							.service(minecraft_endpoint::get_latest_snapshot)
-							.service(minecraft_endpoint::get_version_by_id),
-					)
-					.service(web::scope("java").service(java_endpoint::get_java_versions))
-					.service(
-						web::scope("system")
-							.service(system_stats_endpoint::get_system_info)
-							.service(system_stats_endpoint::get_system_usage)
-							.service(system_stats_endpoint::get_storage_info)
-							.app_data(sys.clone()),
-					)
-					.service(
-						web::scope("server")
-							.service(
-								web::scope("{id}")
-									.service(
-										web::scope("properties")
-											.service(
-												server_properties_endpoint::get_server_properties,
-											)
-											.service(
-												server_properties_endpoint::set_server_property,
-											),
-									)
-									.service(
-										web::scope("settings")
-											.service(server_settings_endpoint::get_server_settings)
-											.service(server_settings_endpoint::set_java_arguments)
-											.service(
-												server_settings_endpoint::set_minecraft_arguments,
-											)
-											.service(server_settings_endpoint::set_memory_max)
-											.service(server_settings_endpoint::set_memory_min)
-											.service(server_settings_endpoint::set_executable)
-											.service(server_settings_endpoint::set_name),
-									)
-									.service(
-										web::scope("files")
-											.service(file_system_endpoint::get_server_files),
-									)
-									.service(server_endpoint::get_server_by_id)
-									.service(
-										web::scope("backups")
-											.service(backups_endpoint::get_backups)
-									)
-							)
-							.service(server_endpoint::get_servers)
-							.service(server_endpoint::create_server),
-					),
-			)
-			// Serve static files from the wwwroot directory
-			.service(Files::new("/", "wwwroot").index_file("index.html"))
-			// Handle all other routes by serving the index.html file
-			.default_service(web::route().to(index))
-	})
-		.workers(4)
-		.bind(format!("0.0.0.0:{port}", port = port))?
-		.run();
-	info!("Starting server at http://127.0.0.1:{port}...");
-	server.await
+    let server = HttpServer::new(move || {
+        App::new()
+            .wrap(middleware::Logger::default())
+            .wrap(auth_middleware::AuthMiddleware)
+            .app_data(
+                web::JsonConfig::default()
+                    .limit(4096)
+                    .error_handler(|err, _req| {
+                        let error = json!({ "error": format!("{}", err) });
+                        error::InternalError::from_response(
+                            err,
+                            HttpResponse::BadRequest().json(error),
+                        )
+                        .into()
+                    }),
+            )
+            // Handle API routes here
+            .service(
+                web::scope("api")
+                    .service(
+                        web::scope("auth")
+                            .service(authentication_endpoint::create_user)
+                            .service(authentication_endpoint::login)
+                            .service(authentication_endpoint::login_with_token)
+                            .service(authentication_endpoint::generate_access_token)
+                            .service(authentication_endpoint::get_access_tokens)
+                            .service(authentication_endpoint::check_if_access_token_exists)
+                            .service(authentication_endpoint::get_users),
+                    )
+                    .service(
+                        web::scope("minecraft")
+                            .service(minecraft_endpoint::get_minecraft_versions)
+                            .service(minecraft_endpoint::get_latest_release)
+                            .service(minecraft_endpoint::get_latest_snapshot)
+                            .service(minecraft_endpoint::get_version_by_id),
+                    )
+                    .service(web::scope("java").service(java_endpoint::get_java_versions))
+                    .service(
+                        web::scope("system")
+                            .service(system_stats_endpoint::get_system_info)
+                            .service(system_stats_endpoint::get_system_usage)
+                            .service(system_stats_endpoint::get_storage_info)
+                            .app_data(sys.clone()),
+                    )
+                    .service(
+                        web::scope("server")
+                            .service(
+                                web::scope("{id}")
+                                    .service(
+                                        web::scope("properties")
+                                            .service(
+                                                server_properties_endpoint::get_server_properties,
+                                            )
+                                            .service(
+                                                server_properties_endpoint::set_server_property,
+                                            ),
+                                    )
+                                    .service(
+                                        web::scope("settings")
+                                            .service(server_settings_endpoint::get_server_settings)
+                                            .service(server_settings_endpoint::set_java_arguments)
+                                            .service(
+                                                server_settings_endpoint::set_minecraft_arguments,
+                                            )
+                                            .service(server_settings_endpoint::set_memory_max)
+                                            .service(server_settings_endpoint::set_memory_min)
+                                            .service(server_settings_endpoint::set_executable)
+                                            .service(server_settings_endpoint::set_name),
+                                    )
+                                    .service(
+                                        web::scope("files")
+                                            .service(file_system_endpoint::get_server_files),
+                                    )
+                                    .service(server_endpoint::get_server_by_id)
+                                    .service(
+                                        web::scope("backups")
+                                            .service(backups_endpoint::get_backups),
+                                    ),
+                            )
+                            .service(server_endpoint::get_servers)
+                            .service(server_endpoint::create_server),
+                    ),
+            )
+            .service(web::scope("assets/{file}").service(assets))
+            // Handle all other routes by serving the index.html file
+            .default_service(web::route().to(index))
+    })
+    .workers(4)
+    .bind(format!("0.0.0.0:{port}", port = port))?
+    .run();
+    info!("Starting server at http://127.0.0.1:{port}...");
+    server.await
+}
+
+// Include the wwwroot directory from the OUT_DIR
+static WWWROOT: Dir = include_dir!("dist/wwwroot");
+
+// Function to serve the index.html file
+async fn index() -> Result<impl Responder, actix_web::Error> {
+    if let Some(file) = WWWROOT.get_file("index.html") {
+        let body = file.contents();
+        return Ok(HttpResponse::Ok().content_type("text/html").body(body));
+    }
+    Err(ErrorInternalServerError("Failed to find index.html"))
+}
+#[get("")]
+async fn assets(file: web::Path<String>) -> impl Responder {
+    if let Some(file) = WWWROOT.get_file(format!("assets/{}", file.as_str())) {
+        let body = file.contents();
+        return Ok(HttpResponse::Ok()
+            .content_type(file_extension_to_mime(
+                file.path().extension().unwrap().to_str().unwrap(),
+            ))
+            .body(body));
+    }
+    Err(ErrorInternalServerError(format!("Failed to find {}", file)))
 }
