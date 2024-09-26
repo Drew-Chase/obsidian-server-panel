c0d06f6c404077cecfbcdc4874f1a9d370f84b95
Drew Chase (Work) <dcmanproductions@gmail.com>
2024-09-26 16:01:15 -0400

    Remove redundant npm install command in build script.
    The `install` command was unnecessary and has been removed to streamline the build process. This change simplifies the environment setup and reduces potential points of failure.

--- src-actix/build.rs
+++ src-actix/build.rs
@@ -5,7 +5,6 @@ fn main() {
 	NpmEnv::default()
 		.with_node_env(&NodeEnv::Production)
 		.init_env()
-		.install(None)
 		.run("build frontend")
 		.exec()
 		.expect("Failed to execute npm run 'build frontend'");
