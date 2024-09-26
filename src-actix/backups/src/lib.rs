765ac724b951db6655d11987f6574d333c4cdd69
Drew (Linux) <dcmanproductions@gmail.com>
2024-09-26 18:49:06 -0400

    Refactor code indentation for consistency
    Adjusted indentation levels throughout the `initialize` and other functions in `src/lib.rs` for better code readability and consistency. This change improves the maintainability of the code without affecting its functionality.

--- src-actix/backups/src/lib.rs
+++ src-actix/backups/src/lib.rs
@@ -6,10 +6,10 @@ use std::env;
 use std::path::{Path, PathBuf};
 
 pub fn initialize() {
-	info!("Initializing backups database");
-	let conn = create_connection().expect("Failed to connect to database");
-	if let Err(e) = conn.execute(
-		"
+    info!("Initializing backups database");
+    let conn = create_connection().expect("Failed to connect to database");
+    if let Err(e) = conn.execute(
+        "
 				CREATE TABLE IF NOT EXISTS backups
 				(
 				    id        INTEGER          NOT NULL PRIMARY KEY AUTOINCREMENT,
@@ -27,33 +27,27 @@ pub fn initialize() {
 				    timestamp DATETIME DEFAULT CURRENT_TIMESTAMP
 				);
 	",
-	) {
-		error!("Failed to create backups table: {}", e);
-	} else {
-		info!("Successfully created or verified the backups table.");
-	}
-	let backups_dir = get_backups_directory();
-	if !backups_dir.exists() {
-		std::fs::create_dir_all(backups_dir).expect("Unable to create backup directory.");
-	}
+    ) {
+        error!("Failed to create backups table: {}", e);
+    } else {
+        info!("Successfully created or verified the backups table.");
+    }
+    let backups_dir = get_backups_directory();
+    if !backups_dir.exists() {
+        std::fs::create_dir_all(backups_dir).expect("Unable to create backup directory.");
+    }
 }
 
 fn create_connection() -> Result<sqlite::Connection, sqlite::Error> {
-	sqlite::Connection::open("servers.db").map_err(|e| {
-		error!(
+    sqlite::Connection::open("servers.db").map_err(|e| {
+        error!(
             "Failed to open servers database connection for backups: {}",
             e
         );
-		e
-	})
+        e
+    })
 }
 
 pub fn get_backups_directory() -> PathBuf {
-	Path::join(
-		env::current_exe()
-			.expect("Failed to get the current executable path")
-			.parent()
-			.expect("Failed to get the parent directory of the executable"),
-		Path::new("backups"),
-	)
+    Path::new("backups").to_path_buf()
 }
