7e543f3c8f5fe0c505febe310012ba7c2477871c
Drew Chase (Work) <dcmanproductions@gmail.com>
2024-09-27 12:38:48 -0400

    Add backup_db and hashed_backup_item modules
    Introduced two new modules, backup_db and hashed_backup_item, to the codebase. These modules aim to better structure the backup-related functionalities and hashed item operations. Additionally, some code formatting was corrected to improve readability.

--- src-actix/backups/src/lib.rs
+++ src-actix/backups/src/lib.rs
@@ -1,15 +1,16 @@
 pub mod backup_item;
 pub mod hashed_file;
+mod backup_db;
+mod hashed_backup_item;
 
 use log::{error, info};
-use std::env;
 use std::path::{Path, PathBuf};
 
 pub fn initialize() {
-    info!("Initializing backups database");
-    let conn = create_connection().expect("Failed to connect to database");
-    if let Err(e) = conn.execute(
-        "
+	info!("Initializing backups database");
+	let conn = create_connection().expect("Failed to connect to database");
+	if let Err(e) = conn.execute(
+		"
 				CREATE TABLE IF NOT EXISTS backups
 				(
 				    id        INTEGER          NOT NULL PRIMARY KEY AUTOINCREMENT,
@@ -27,27 +28,27 @@ pub fn initialize() {
 				    timestamp DATETIME DEFAULT CURRENT_TIMESTAMP
 				);
 	",
-    ) {
-        error!("Failed to create backups table: {}", e);
-    } else {
-        info!("Successfully created or verified the backups table.");
-    }
-    let backups_dir = get_backups_directory();
-    if !backups_dir.exists() {
-        std::fs::create_dir_all(backups_dir).expect("Unable to create backup directory.");
-    }
+	) {
+		error!("Failed to create backups table: {}", e);
+	} else {
+		info!("Successfully created or verified the backups table.");
+	}
+	let backups_dir = get_backups_directory();
+	if !backups_dir.exists() {
+		std::fs::create_dir_all(backups_dir).expect("Unable to create backup directory.");
+	}
 }
 
 fn create_connection() -> Result<sqlite::Connection, sqlite::Error> {
-    sqlite::Connection::open("servers.db").map_err(|e| {
-        error!(
+	sqlite::Connection::open("servers.db").map_err(|e| {
+		error!(
             "Failed to open servers database connection for backups: {}",
             e
         );
-        e
-    })
+		e
+	})
 }
 
 pub fn get_backups_directory() -> PathBuf {
-    Path::new("backups").to_path_buf()
+	Path::new("backups").to_path_buf()
 }
