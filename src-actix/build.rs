4bb6aed279a18fdabbd215d99df5cbc06d626201
Drew Chase (Work) <dcmanproductions@gmail.com>
2024-09-27 12:38:19 -0400

    Add directory creation to build script
    Ensure the target/dev-env directory exists before running npm commands. This change prevents build failures caused by missing directories.

--- src-actix/build.rs
+++ src-actix/build.rs
@@ -1,6 +1,8 @@
 use npm_rs::{NodeEnv, NpmEnv};
+use std::fs;
 
 fn main() {
+	fs::create_dir_all("target/dev-env").expect("failed to create target directory");
 	// Run `npm run build frontend`
 	NpmEnv::default()
 		.with_node_env(&NodeEnv::Production)
