fb735639a981e35f579188f5a129ed7482caef73
Drew Chase (Work) <dcmanproductions@gmail.com>
2024-09-30 10:33:00 -0400

    Revise file hashing and change detection logic
    Revised the `hash_file` function to directly return a hex string, eliminating the need for a separate function to convert the hash to a string. Enhanced the `changed` method to use file metadata for more accurate change detection, including handling large files efficiently by assuming changes based on the modified timestamp for files over 1GB.

--- src-actix/backups/src/hashed_file.rs
+++ src-actix/backups/src/hashed_file.rs
@@ -1,78 +1,96 @@
 use crate::file_hash_db;
+use log::info;
 use serde_derive::{Deserialize, Serialize};
 use sha2::{Digest, Sha256};
 use std::error::Error;
-use std::fmt::Write;
 use std::fs::File;
 use std::io::{BufReader, Read};
 use std::path::{Path, PathBuf};
-use std::time::SystemTime;
+use std::time::{Duration, SystemTime};
 
 #[derive(Serialize, Deserialize, Clone, Debug)]
 pub struct HashedFile {
-    pub path: PathBuf,
-    pub hash: String,
-    pub timestamp: SystemTime,
+	pub path: PathBuf,
+	pub hash: String,
+	pub timestamp: SystemTime,
 }
 impl HashedFile {
-    pub fn from_path(path: impl AsRef<Path>) -> Result<Self, Box<dyn Error>> {
-        let path = path.as_ref().to_path_buf();
-        if !path.exists() {
-            return Err("File does not exist".into());
-        }
-        if !path.is_file() {
-            return Err("Path is not a file".into());
-        }
-        if let Some(file) = file_hash_db::get(&path) {
-            return Ok(file);
-        }
+	pub fn from_path(path: impl AsRef<Path>) -> Result<Self, Box<dyn Error>> {
+		let path = path.as_ref().to_path_buf();
+		if !path.exists() {
+			return Err("File does not exist".into());
+		}
+		if !path.is_file() {
+			return Err("Path is not a file".into());
+		}
+		if let Some(file) = file_hash_db::get(&path) {
+			return Ok(file);
+		}
 
-        let hash = hash_file(&path)?;
-        let timestamp = SystemTime::now();
-        Ok(Self {
-            path,
-            hash: hash_to_string(&hash),
-            timestamp,
-        })
-    }
-    pub fn changed(&self) -> Result<bool, Box<dyn Error>> {
-        if let Some(file) = file_hash_db::get(&self.path) {
-            let updated_hash = hash_to_string(&hash_file(&self.path)?);
-            let current_hash = &*file.hash;
-            let changed = updated_hash != current_hash;
-            Ok(changed)
-        } else {
-            Ok(true)
-        }
-    }
+		let hash = hash_file(&path)?;
+		let timestamp = SystemTime::now();
+		Ok(Self {
+			path,
+			hash,
+			timestamp,
+		})
+	}
+	pub fn changed(&self) -> Result<bool, Box<dyn Error>> {
+		if let Some(file) = file_hash_db::get(&self.path) {
+			let metadata = std::fs::metadata(&self.path)?;
+			let modified = metadata.modified()?;
 
-    pub fn cache(&self) -> Result<(), Box<dyn Error>> {
-        if file_hash_db::exists(&self.path)? {
-            file_hash_db::update(&self.path, &self.hash)?;
-        } else {
-            file_hash_db::insert(&self.path, &self.hash)?;
-        }
-        Ok(())
-    }
+			// If the file has been modified since the last time we checked
+			if normalize_time(modified) > normalize_time(file.timestamp) {
+				info!(
+				"File has been modified: {:?} ({} > {})",
+				self.path,
+				normalize_time(modified),
+				normalize_time(file.timestamp),
+			);
+				// If the file is too large, we'll just assume that the modified
+				// time means that the file has changed.
+				// This is a bit of a hack, but it's better than trying to hash a huge file
+				// current implementation is limited to 1GB files
+				if metadata.len() > 1 << 30 {
+					return Ok(true);
+				}
+				let updated_hash = &hash_file(&self.path)?;
+				let current_hash = &*file.hash;
+				let changed = updated_hash != current_hash;
+				return Ok(changed);
+			}
+		} else {
+			return Ok(true);
+		}
+		Ok(false)
+	}
+
+	pub fn cache(&self) -> Result<(), Box<dyn Error>> {
+		if file_hash_db::exists(&self.path)? {
+			file_hash_db::update(&self.path, &self.hash)?;
+		} else {
+			file_hash_db::insert(&self.path, &self.hash)?;
+		}
+		Ok(())
+	}
 }
 
-fn hash_file(path: impl AsRef<Path>) -> Result<Vec<u8>, Box<dyn Error>> {
-    let file = File::open(path.as_ref())?;
-    let mut reader = BufReader::new(file);
-    let mut hasher = Sha256::new();
-    let mut buffer = [0; 1024];
-    loop {
-        let bytes_read = reader.read(&mut buffer)?;
-        if bytes_read == 0 {
-            break;
-        }
-        hasher.update(&buffer[..bytes_read]);
-    }
-    Ok(hasher.finalize().to_vec())
+fn hash_file(path: impl AsRef<Path>) -> Result<String, Box<dyn Error>> {
+	let file = File::open(path.as_ref())?;
+	let mut reader = BufReader::new(file);
+	let mut hasher = Sha256::new();
+	let mut buffer = [0; 1024];
+	loop {
+		let bytes_read = reader.read(&mut buffer)?;
+		if bytes_read == 0 {
+			break;
+		}
+		hasher.update(&buffer[..bytes_read]);
+	}
+	Ok(hex::encode(hasher.finalize()))
 }
-fn hash_to_string(hash: &[u8]) -> String {
-    hash.iter().fold(String::new(), |mut acc, byte| {
-        write!(acc, "{:02x}", byte).unwrap();
-        acc
-    })
+
+fn normalize_time(t: SystemTime) -> u64 {
+	t.duration_since(SystemTime::UNIX_EPOCH).unwrap().as_secs()
 }
