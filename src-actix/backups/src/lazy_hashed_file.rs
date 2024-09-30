30629d546be5c6aa74f134d364b074a5459c7f76
Drew Chase (Work) <dcmanproductions@gmail.com>
2024-09-30 10:32:39 -0400

    Add utility methods to LazyHashedFile struct
    Introduces new methods including `is_empty`, `as_vec`, `to_vec`, and `len` for the `LazyHashedFile` struct. These methods enhance functionality by providing additional ways to interact with `items`. The existing methods' indentation has also been corrected for consistency.

--- src-actix/backups/src/lazy_hashed_file.rs
+++ src-actix/backups/src/lazy_hashed_file.rs
@@ -3,36 +3,53 @@ use log::error;
 use std::path::Path;
 
 pub struct LazyHashedFile {
-    pub items: Vec<HashedFile>,
+	items: Vec<HashedFile>,
 }
 
 impl Default for LazyHashedFile {
-    fn default() -> Self {
-        Self::new()
-    }
+	fn default() -> Self {
+		Self::new()
+	}
 }
 
 impl LazyHashedFile {
-    pub fn new() -> Self {
-        LazyHashedFile { items: Vec::new() }
-    }
+	pub fn new() -> Self {
+		LazyHashedFile { items: Vec::new() }
+	}
 
-    pub fn add(&mut self, item: HashedFile) {
-        self.items.push(item);
-    }
+	pub fn add(&mut self, item: HashedFile) {
+		self.items.push(item);
+	}
 
-    pub fn get(&self, path: &Path) -> Option<&HashedFile> {
-        self.items.iter().find(|item| item.path == path)
-    }
+	pub fn get(&self, path: &Path) -> Option<&HashedFile> {
+		self.items.iter().find(|item| item.path == path)
+	}
 
-    pub fn flush(&self) {
-        for item in self.items.iter() {
-            match item.cache() {
-                Ok(_) => {}
-                Err(e) => {
-                    error!("Failed to cache file hash for '{:?}': {}", item.path, e);
-                }
-            }
-        }
-    }
+	pub fn is_empty(&self) -> bool {
+		self.items.is_empty()
+	}
+
+	pub fn as_vec(&self) -> &Vec<HashedFile> {
+		&self.items
+	}
+
+	pub fn to_vec(&self) -> Vec<HashedFile> {
+		self.items.clone()
+	}
+
+	pub fn len(&self) -> usize {
+		self.items.len()
+	}
+
+
+	pub fn flush(&self) {
+		for item in self.items.iter() {
+			match item.cache() {
+				Ok(_) => {}
+				Err(e) => {
+					error!("Failed to cache file hash for '{:?}': {}", item.path, e);
+				}
+			}
+		}
+	}
 }
