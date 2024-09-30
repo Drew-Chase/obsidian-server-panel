8299d4057eb4532b0935f6fe6d755bdb7754b1d8
Drew Chase (Work) <dcmanproductions@gmail.com>
2024-09-30 15:54:54 -0400

    Refactor create_manual_backup to accept backup type
    Updated the `create_manual_backup` endpoint to include a backup type in the path parameters. This change allows users to specify the type of backup (e.g., incremental or full) when initiating a manual backup, improving the flexibility and functionality of the endpoint.

--- src-actix/backups_endpoint.rs
+++ src-actix/backups_endpoint.rs
@@ -1,3 +1,4 @@
+use actix_web::web::method;
 use actix_web::{get, post, web, HttpMessage, HttpRequest, HttpResponse, Responder};
 use authentication::data::User;
 use backups::backup_item::{BackupCreationMethod, BackupItem, BackupType};
@@ -10,55 +11,57 @@ use std::path::Path;
 
 #[get("/")]
 pub async fn get_backups(id: web::Path<String>, req: HttpRequest) -> impl Responder {
-    if let Some(_) = req.extensions().get::<User>() {
-        let id_number: u32 = match decode(id.as_str()) {
-            Ok(id_number) => id_number[0] as u32,
-            Err(_) => return HttpResponse::BadRequest().json(json!({"error":"Invalid ID"})),
-        };
+	if req.extensions().get::<User>().is_some() {
+		let id_number: u32 = match decode(id.as_str()) {
+			Ok(id_number) => id_number[0] as u32,
+			Err(_) => return HttpResponse::BadRequest().json(json!({"error":"Invalid ID"})),
+		};
 
-        return HttpResponse::Ok().json(json!(BackupItem::from_server(id_number)
+		return HttpResponse::Ok().json(json!(BackupItem::from_server(id_number)
             .iter()
             .map(|e| { e.clone().hash() })
             .collect::<Vec<HashedBackupItem>>()));
-    }
+	}
 
-    HttpResponse::Unauthorized().json(json!({"error":"Unauthorized"}))
+	HttpResponse::Unauthorized().json(json!({"error":"Unauthorized"}))
 }
 
-#[post("/create")]
-pub async fn create_manual_backup(id: web::Path<String>, req: HttpRequest) -> impl Responder {
-    if let Some(user) = req.extensions().get::<User>() {
-        let id_number: u32 = match decode(id.as_str()) {
-            Ok(id_number) => id_number[0] as u32,
-            Err(_) => return HttpResponse::BadRequest().json(json!({"error":"Invalid ID"})),
-        };
+#[post("/create/{method}")]
+pub async fn create_manual_backup(path: web::Path<(String, BackupType)>, req: HttpRequest) -> impl Responder {
+	let (id, method) = path.into_inner();
 
-        let server = match server_db::get_owned_server_by_id(id_number, user.id) {
-            Some(s) => s,
-            None => {
-                let msg = format!("Server with id: {} not found", id_number);
-                error!("{}", msg);
-                return HttpResponse::BadRequest().json(json!({"error":msg}));
-            }
-        };
+	if let Some(user) = req.extensions().get::<User>() {
+		let id_number: u32 = match decode(id.as_str()) {
+			Ok(id_number) => id_number[0] as u32,
+			Err(_) => return HttpResponse::BadRequest().json(json!({"error":"Invalid ID"})),
+		};
 
-        if let Some(server_directory) = server.directory {
-            let item = match BackupItem::create_backup(
-                id_number,
-                Path::new(&server_directory),
-                BackupCreationMethod::MANUAL,
-                BackupType::Incremental,
-            ) {
-                Ok(b) => b.hash(),
-                Err(e) => {
-                    let msg = format!("Failed to create backup: {}", e);
-                    error!("{}", msg);
-                    return HttpResponse::BadRequest().json(json!({"error":msg}));
-                }
-            };
-            return HttpResponse::Ok().json(json!(item));
-        }
-    }
+		let server = match server_db::get_owned_server_by_id(id_number, user.id) {
+			Some(s) => s,
+			None => {
+				let msg = format!("Server with id: {} not found", id_number);
+				error!("{}", msg);
+				return HttpResponse::BadRequest().json(json!({"error":msg}));
+			}
+		};
 
-    HttpResponse::Unauthorized().json(json!({"error":"Unauthorized"}))
+		if let Some(server_directory) = server.directory {
+			let item = match BackupItem::create_backup(
+				id_number,
+				Path::new(&server_directory),
+				BackupCreationMethod::MANUAL,
+				method,
+			) {
+				Ok(b) => b.hash(),
+				Err(e) => {
+					let msg = format!("Failed to create backup: {}", e);
+					error!("{}", msg);
+					return HttpResponse::BadRequest().json(json!({"error":msg}));
+				}
+			};
+			return HttpResponse::Ok().json(json!(item));
+		}
+	}
+
+	HttpResponse::Unauthorized().json(json!({"error":"Unauthorized"}))
 }
