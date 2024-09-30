8607ac88bbfc493b40f790911ebe11f152e69ab4
Drew Chase (Work) <dcmanproductions@gmail.com>
2024-09-30 10:31:37 -0400

    Create additional directory in build script
    Add command to create the "target/wwwroot" directory in the build script. This ensures the necessary folder structure is in place for the build process to run smoothly.

--- src-actix/build.rs
+++ src-actix/build.rs
@@ -3,6 +3,7 @@ use std::fs;
 
 fn main() {
 	fs::create_dir_all("target/dev-env").expect("failed to create target directory");
+	fs::create_dir_all("target/wwwroot").expect("failed to create wwwroot directory");
 	// Run `npm run build frontend`
 	NpmEnv::default()
 		.with_node_env(&NodeEnv::Production)
