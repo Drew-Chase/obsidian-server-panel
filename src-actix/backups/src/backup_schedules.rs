ac6d20a24591a145edb9b7d4e7b87021ed1a9538
Drew Chase (Work) <dcmanproductions@gmail.com>
2024-10-02 12:57:53 -0400

    Refactor backup schedules to use structured objects
    Replaced inline SQL-based backup schedule initialization with a structured `BackupSchedule` object. Implemented methods for schedule creation, execution, and updating next execution time to improve maintainability and extendibility of the backup scheduling logic.

--- src-actix/backups/src/backup_schedules.rs
+++ src-actix/backups/src/backup_schedules.rs
@@ -1,25 +1,43 @@
-use crate::{create_connection, get_backups_directory};
-use log::{debug, error, info};
+use std::error::Error;
+use crate::backup_item::{BackupItem, BackupType};
+use crate::backup_schedule_db;
+use std::time::{Duration, SystemTime, UNIX_EPOCH};
+use tokio::time::{interval, Elapsed};
 
-pub fn initialize() {
-	debug!("Initializing backup schedule table");
-	let conn = create_connection().expect("Failed to connect to database");
-	if let Err(e) = conn.execute(
-		"
-				CREATE TABLE IF NOT EXISTS backups
-				(
-				    id        INTEGER          NOT NULL PRIMARY KEY AUTOINCREMENT,
-				    path      TEXT             NOT NULL UNIQUE,
-				    type      TINYINT          NOT NULL,
-				    method    TINYINT          NOT NULL,
-				    timestamp DATETIME DEFAULT CURRENT_TIMESTAMP,
-				    size      UNSIGNED BIG INT NOT NULL,
-				    server    INTEGER          NOT NULL
-				);
-	",
-	) {
-		error!("Failed to create backups table: {}", e);
-	} else {
-		info!("Successfully created or verified the backup schedule table.");
+pub struct BackupSchedule {
+	pub id: u32,
+	pub server: u32,
+	pub backup_type: BackupType,
+	pub interval: u32,
+	pub exec_if_empty: bool,
+	pub exec_if_offline: bool,
+	pub last_exec: Option<SystemTime>,
+	pub next_exec: Option<SystemTime>,
+}
+impl BackupSchedule {
+	pub fn new(id: u32, server: u32, backup_type: BackupType, interval: u32, exec_if_empty: bool, exec_if_offline: bool) -> Self {
+		Self {
+			id,
+			server,
+			backup_type,
+			interval,
+			exec_if_empty,
+			exec_if_offline,
+			last_exec: None,
+			next_exec: None,
+		}
+	}    pub fn update_next_exec(&mut self) {
+		let now = SystemTime::now();
+		self.next_exec = Some(if let Some(last_exec) = self.last_exec {
+			last_exec + Duration::from_secs(self.interval as u64 * 60)
+		} else {
+			now + Duration::from_secs(self.interval as u64 * 60)
+		});
+	}
+
+	pub async fn execute(&mut self) {
+		BackupItem::create_backup()
+		self.last_exec = Some(SystemTime::now());
+		self.update_next_exec();
 	}
 }
