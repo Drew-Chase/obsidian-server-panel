112ebb5fda20924813c3ee1cddc2013b36025fcf
Drew (Linux) <dcmanproductions@gmail.com>
2024-10-02 21:29:27 -0400

    Add Java and OS version structs for serialization
    Introduced new structs to represent Java and OS versions using Serde for serialization and deserialization. These structures include `Version`, `Manifest`, `Availability`, `JavaVersionData`, `JavaVersions`, `OSVersions`, `RuntimeVersion`, and `OS` to encapsulate details for various runtime environments.

---
+++ src-actix/java/src/data.rs
@@ -0,0 +1,96 @@
+use serde_derive::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize, Clone)]
+pub struct Version {
+    pub name: String,
+    pub released: String,
+}
+
+#[derive(Serialize, Deserialize, Clone)]
+pub struct Manifest {
+    pub sha1: String,
+    pub size: i64,
+    pub url: String,
+}
+
+#[derive(Serialize, Deserialize, Copy, Clone)]
+pub struct Availability {
+    pub group: i64,
+    pub progress: i64,
+}
+
+#[derive(Serialize, Deserialize, Clone)]
+pub struct JavaVersionData {
+    pub availability: Availability,
+    #[serde(rename = "manifest")]
+    pub manifest: Manifest,
+    #[serde(rename = "version")]
+    pub version: Version,
+}
+
+#[derive(Serialize, Deserialize, Clone)]
+pub struct JavaVersions {
+    #[serde(rename = "java-runtime-alpha")]
+    pub alpha: Vec<JavaVersionData>,
+    #[serde(rename = "java-runtime-beta")]
+    pub beta: Vec<JavaVersionData>,
+    #[serde(rename = "java-runtime-delta")]
+    pub delta: Vec<JavaVersionData>,
+    #[serde(rename = "java-runtime-gamma")]
+    pub gamma: Vec<JavaVersionData>,
+    #[serde(rename = "java-runtime-gamma-snapshot")]
+    pub gamma_snapshot: Vec<JavaVersionData>,
+    #[serde(rename = "jre-legacy")]
+    pub legacy: Vec<JavaVersionData>,
+}
+
+#[derive(Serialize, Deserialize, Clone)]
+pub struct OSVersions {
+    pub linux: JavaVersions,
+    #[serde(rename = "linux-i386")]
+    pub linux_i386: JavaVersions,
+    #[serde(rename = "mac-os")]
+    pub macos: JavaVersions,
+    #[serde(rename = "mac-os-arm64")]
+    pub macos_arm64: JavaVersions,
+    #[serde(rename = "windows-arm64")]
+    pub windows_arm64: JavaVersions,
+    #[serde(rename = "windows-x64")]
+    pub windows_x64: JavaVersions,
+    #[serde(rename = "windows-x86")]
+    pub windows_x86: JavaVersions,
+}
+
+#[derive(Serialize, Deserialize)]
+pub enum RuntimeVersion {
+    #[serde(rename = "alpha")]
+    Alpha,
+    #[serde(rename = "beta")]
+    Beta,
+    #[serde(rename = "delta")]
+    Delta,
+    #[serde(rename = "gamma")]
+    Gamma,
+    #[serde(rename = "gamma-snapshot")]
+    GammaSnapshot,
+    #[serde(rename = "legacy")]
+    Legacy,
+}
+
+#[derive(Serialize, Deserialize)]
+pub enum OS {
+    #[serde(rename = "linux")]
+    Linux,
+    #[serde(rename = "linux-i386")]
+    LinuxI386,
+    #[serde(rename = "mac-os")]
+    MacOS,
+    #[serde(rename = "mac-os-arm64")]
+    MacOSArm64,
+    #[serde(rename = "windows-arm64")]
+    WindowsArm64,
+    #[serde(rename = "windows-x64")]
+    WindowsX64,
+    #[serde(rename = "windows-x86")]
+    WindowsX86,
+}
