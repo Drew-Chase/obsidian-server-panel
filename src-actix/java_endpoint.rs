959e57cc7e849347b017651261a0086b74146e1c
Drew (Linux) <dcmanproductions@gmail.com>
2024-10-02 21:29:10 -0400

    Add endpoints for OS and runtime versions
    Refactor existing `get_java_versions` function for clarity. Add new endpoints to handle OS-specific versions and runtime-specific versions, enhancing API flexibility and functionality.

--- src-actix/java_endpoint.rs
+++ src-actix/java_endpoint.rs
@@ -1,11 +1,40 @@
 use actix_web::{get, HttpResponse, Responder};
-use minecraft::minecraft_java;
 use serde_json::json;
 
 #[get("/versions")]
 pub async fn get_java_versions() -> impl Responder {
-	match minecraft_java::get_java_versions().await {
-		Ok(versions) => HttpResponse::Ok().json(versions),
-		Err(e) => HttpResponse::BadRequest().json(json!({"error": e})),
-	}
+    match java::versions::list().await {
+        Ok(versions) => HttpResponse::Ok().json(versions),
+        Err(e) => HttpResponse::BadRequest().json(json!({"error": e})),
+    }
+}
+
+#[get("/versions/{os}")]
+pub async fn get_os_version(os: actix_web::web::Path<java::data::OS>) -> impl Responder {
+    let all = match java::versions::list().await {
+        Ok(versions) => versions,
+        Err(e) => return HttpResponse::BadRequest().json(json!({"error": e})),
+    };
+    match java::versions::get_os_version(os.into_inner(), &all) {
+        Some(versions) => HttpResponse::Ok().json(versions),
+        None => HttpResponse::BadRequest().json(json!({"error": "OS not found"})),
+    }
+}
+#[get("/versions/{os}/{runtime}")]
+pub async fn get_runtime_version(
+    os: actix_web::web::Path<java::data::OS>,
+    runtime: actix_web::web::Path<java::data::RuntimeVersion>,
+) -> impl Responder {
+    let all = match java::versions::list().await {
+        Ok(versions) => versions,
+        Err(e) => return HttpResponse::BadRequest().json(json!({"error": e})),
+    };
+    let os = match java::versions::get_os_version(os.into_inner(), &all) {
+        Some(versions) => versions,
+        None => return HttpResponse::BadRequest().json(json!({"error": "OS not found"})),
+    };
+    match java::versions::get_runtime_version(&os, &runtime.into_inner()) {
+        Some(version) => HttpResponse::Ok().json(version),
+        None => HttpResponse::BadRequest().json(json!({"error": "Runtime not found"})),
+    }
 }
