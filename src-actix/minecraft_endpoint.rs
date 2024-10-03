77f4a00f3b6aae839afc4ea8b8b3a6c6d0c4efa3
Drew (Linux) <dcmanproductions@gmail.com>
2024-10-02 21:28:56 -0400

    Add new endpoints for Minecraft versions and Java version
    Revised the existing endpoints to follow a consistent URL pattern, and introduced new endpoints to fetch specific lists of snapshots and releases. Additionally, added an endpoint to retrieve the Java version associated with a specific Minecraft version.

--- src-actix/minecraft_endpoint.rs
+++ src-actix/minecraft_endpoint.rs
@@ -1,35 +1,74 @@
+use actix_web::web::Path;
 use actix_web::{get, HttpResponse, Responder};
 use minecraft::minecraft_version;
 use serde_json::json;
 
 #[get("/versions")]
 pub async fn get_minecraft_versions() -> impl Responder {
-	match minecraft_version::get_versions().await {
-		Ok(versions) => HttpResponse::Ok().json(versions),
-		Err(e) => HttpResponse::BadRequest().json(json!({"error": e})),
-	}
+    match minecraft_version::get_versions().await {
+        Ok(versions) => HttpResponse::Ok().json(versions),
+        Err(e) => HttpResponse::BadRequest().json(json!({"error": e})),
+    }
 }
 
-#[get("/latest-release")]
+#[get("/version/releases/latest")]
 pub async fn get_latest_release() -> impl Responder {
-	match minecraft_version::get_latest_release().await {
-		Ok(version) => HttpResponse::Ok().json(version),
-		Err(e) => HttpResponse::BadRequest().json(json!({"error": e})),
-	}
+    match minecraft_version::get_latest_release().await {
+        Ok(version) => HttpResponse::Ok().json(version),
+        Err(e) => HttpResponse::BadRequest().json(json!({"error": e})),
+    }
 }
 
-#[get("/latest-snapshot")]
+#[get("/version/snapshots/latest")]
 pub async fn get_latest_snapshot() -> impl Responder {
-	match minecraft_version::get_latest_snapshot().await {
-		Ok(version) => HttpResponse::Ok().json(version),
-		Err(e) => HttpResponse::BadRequest().json(json!({"error": e})),
-	}
+    match minecraft_version::get_latest_snapshot().await {
+        Ok(version) => HttpResponse::Ok().json(version),
+        Err(e) => HttpResponse::BadRequest().json(json!({"error": e})),
+    }
+}
+#[get("/versions/snapshots")]
+pub async fn get_snapshots() -> impl Responder {
+    match minecraft_version::get_versions().await {
+        Ok(versions) => {
+            let snapshots: Vec<minecraft::minecraft_version::MinecraftVersionResponse> = versions
+                .into_iter()
+                .filter(|version| {
+                    version.version_type == minecraft::minecraft_version::VersionType::Snapshot
+                })
+                .collect();
+            HttpResponse::Ok().json(snapshots)
+        }
+        Err(e) => HttpResponse::BadRequest().json(json!({"error": e})),
+    }
+}
+
+#[get("/versions/releases")]
+pub async fn get_releases() -> impl Responder {
+    match minecraft_version::get_versions().await {
+        Ok(versions) => {
+            let releases: Vec<minecraft::minecraft_version::MinecraftVersionResponse> = versions
+                .into_iter()
+                .filter(|version| {
+                    version.version_type == minecraft::minecraft_version::VersionType::Release
+                })
+                .collect();
+            HttpResponse::Ok().json(releases)
+        }
+        Err(e) => HttpResponse::BadRequest().json(json!({"error": e})),
+    }
 }
 
 #[get("/version/{version}")]
 pub async fn get_version_by_id(version: actix_web::web::Path<String>) -> impl Responder {
-	match minecraft_version::get_version_by_id(&version, None).await {
-		Ok(version) => HttpResponse::Ok().json(version),
-		Err(e) => HttpResponse::BadRequest().json(json!({"error": e})),
-	}
+    match minecraft_version::get_version_by_id(&version, None).await {
+        Ok(version) => HttpResponse::Ok().json(version),
+        Err(e) => HttpResponse::BadRequest().json(json!({"error": e})),
+    }
+}
+#[get("/{version}/java")]
+pub async fn get_java_version_by_minecraft_version(version: Path<String>) -> impl Responder {
+    match minecraft_version::get_version_runtime(&version).await {
+        Ok(version) => HttpResponse::Ok().json(version),
+        Err(e) => HttpResponse::BadRequest().json(json!({"error": format!("{}", e)})),
+    }
 }
