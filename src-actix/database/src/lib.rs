7c94eb80a62e51f66299c5cd83f63e0d60b5dda3
Drew (Linux) <dcmanproductions@gmail.com>
2024-10-05 13:39:42 -0400

    Add SQLite database connection function
    This commit introduces `create_appdb_connection` which establishes a connection to the application's SQLite database. It includes error handling and logs any issues encountered while opening the database file or setting the journal mode to WAL.

---
+++ src-actix/database/src/lib.rs
@@ -0,0 +1,39 @@
+use log::error;
+
+
+/// Establishes a connection to the app's SQLite database.
+///
+/// # Returns
+///
+/// * `Ok(Connection)` - A successful connection to the SQLite database.
+/// * `Err(Error)` - An error that occurred while attempting to open the database or set the journal mode.
+///
+/// # Errors
+///
+/// This function will return an error if:
+///
+/// * The database file `app.db` could not be opened.
+/// * The `PRAGMA journal_mode = WAL` statement could not be executed.
+///
+/// The error is logged with the `log` crate's `error!` macro.
+pub fn create_appdb_connection() -> Result<sqlite::Connection, sqlite::Error> {
+    match sqlite::Connection::open("app.db").map_err(|e| {
+        error!(
+            "Failed to open servers database connection for backups: {}",
+            e
+        );
+        e
+    }) {
+        Ok(conn) => {
+            // allows multiple connections to the database
+            match conn.execute("PRAGMA journal_mode = WAL;") {
+                Ok(_) => {}
+                Err(e) => {
+                    error!("Failed to set journal mode to WAL: {}", e);
+                }
+            }
+            Ok(conn)
+        }
+        Err(e) => Err(e),
+    }
+}
