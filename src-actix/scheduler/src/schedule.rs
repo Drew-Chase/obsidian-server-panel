4e14f142044409e8586836ca5bca2a6fcccfefe6
Drew Chase (Work) <dcmanproductions@gmail.com>
2024-10-09 13:05:46 -0400

    Fix duration handling and improve end_time updates
    Updated Schedule struct to properly handle duration initialization and end_time updates. Replaced `Duration::new()` with the correct duration parameter and provided clear method names for updating end_time. Improved code readability and consistency with proper indentation.

--- src-actix/scheduler/src/schedule.rs
+++ src-actix/scheduler/src/schedule.rs
@@ -2,47 +2,47 @@ use crate::duration::{Duration, SystemTimeTrait};
 use std::time::SystemTime;
 
 pub struct Schedule {
-    pub id: u64,
-    pub duration: Duration,
-    pub active: bool,
-    reoccurring: bool,
-    end_time: SystemTime,
-    action: fn(),
+	pub id: u64,
+	pub duration: Duration,
+	pub active: bool,
+	reoccurring: bool,
+	end_time: SystemTime,
+	action: fn(),
 }
 
 impl Schedule {
-    pub fn new(id: u64, duration: Duration, reoccurring:bool, action: fn()) -> Schedule {
-        Schedule {
-            id,
-            duration: Duration::new(),
-            active: true,
-            reoccurring,
-            end_time: SystemTime::now(),
-            action,
-        }
-    }
+	pub fn new(id: u64, duration: Duration, reoccurring: bool, action: fn()) -> Schedule {
+		Schedule {
+			id,
+			duration,
+			active: true,
+			reoccurring,
+			end_time: duration.add_duration_to_now(),
+			action,
+		}
+	}
 
-    pub fn get_end_time(&self) -> SystemTime {
-        self.end_time
-    }
-    
-    pub fn activate(&mut self) {
-        self.active = true;
-        self.end_time = self.duration.to_system_time();
-    }
-    
-    pub fn deactivate(&mut self) {
-        self.active = false;
-    }
-    
-    
-    pub fn tick(&mut self) {
-        if self.active && SystemTime::now() >= self.end_time {
-            (self.action)();
-            self.active = self.reoccurring;
-            if self.active {
-                self.end_time = self.duration.to_system_time();
-            }
-        }
-    }
+	pub fn get_end_time(&self) -> SystemTime {
+		self.end_time
+	}
+
+	pub fn activate(&mut self) {
+		self.active = true;
+		self.end_time = self.duration.add_duration_to_now();
+	}
+
+	pub fn deactivate(&mut self) {
+		self.active = false;
+	}
+
+
+	pub fn tick(&mut self) {
+		if self.active && SystemTime::now() >= self.end_time {
+			(self.action)();
+			self.active = self.reoccurring;
+			if self.active {
+				self.end_time = self.duration.add_duration_to_now();
+			}
+		}
+	}
 }
