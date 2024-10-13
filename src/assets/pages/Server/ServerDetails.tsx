c755835bb665c3bd3695077f098d9503eec3054b
Drew (Linux) <dcmanproductions@gmail.com>
2024-10-12 20:05:35 -0400

    Add dynamic server stats to ServerDetails component
    Integrated state hooks to dynamically update CPU, memory, and storage usage as well as online player count in the ServerDetails component. Used Conversions utility for displaying formatted data values. Enhanced StatCard displays with valueDisplay and maxValueDisplay props for better readability.

--- src/assets/pages/Server/ServerDetails.tsx
+++ src/assets/pages/Server/ServerDetails.tsx
@@ -10,9 +10,21 @@ import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faUser} from "@fortawesome/free-solid-svg-icons";
 import MinecraftVersionsList from "../../components/Server/MinecraftVersionsList.tsx";
 import LoaderVersionsList from "../../components/Server/LoaderVersionsList.tsx";
+import Conversions from "../../ts/Conversions.ts";
+import {useState} from "react";
 
 export default function ServerDetails()
 {
+    // @ts-ignore
+    const [cpuUsage, setCpuUsage] = useState(0);
+    // @ts-ignore
+    const [memoryUsage, setMemoryUsage] = useState(0);
+    // @ts-ignore
+    const [totalMemory, setTotalMemory] = useState(0);
+    // @ts-ignore
+    const [storageUsage, setStorageUsage] = useState(0);
+    // @ts-ignore
+    const [onlinePlayers, setOnlinePlayers] = useState(0);
     setTitle("Server Details");
     return (
         <div className={"flex flex-col gap-8"}>
@@ -22,25 +34,32 @@ export default function ServerDetails()
             <div className={"flex flex-row w-full justify-between"}>
                 <StatCard
                     title={"CPU Usage"}
-                    value={`40%`}
+                    value={cpuUsage}
+                    valueDisplay={`${cpuUsage}%`}
                     maxValue={100}
+                    maxValueDisplay={"100%"}
                     icon={<CPU size={24}/>}
                 />
                 <StatCard
                     title={"Memory Usage"}
-                    value={`28 GB`}
-                    maxValue={64}
+                    value={memoryUsage}
+                    valueDisplay={`${Conversions.bytesToSize(memoryUsage)}`}
+                    maxValue={totalMemory}
+                    maxValueDisplay={Conversions.bytesToSize(totalMemory)}
                     icon={<RAM size={24}/>}
                 />
                 <StatCard
                     title={"Storage Usage"}
-                    value={`800 GB`}
+                    value={storageUsage}
+                    valueDisplay={`${Conversions.bytesToSize(storageUsage)}`}
                     maxValue={1000}
+                    maxValueDisplay={"1000 GB"}
                     icon={<Storage size={24}/>}
                 />
                 <StatCard
                     title={"Online Players"}
-                    value={`4 Players`}
+                    value={onlinePlayers}
+                    valueDisplay={`${onlinePlayers} Players`}
                     maxValue={20}
                     icon={<FontAwesomeIcon icon={faUser}/>}
                 />
