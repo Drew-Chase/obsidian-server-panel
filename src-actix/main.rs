535386f0c12d62ac86ba36b29e2aec845ec4fd88
Drew (Linux) <dcmanproductions@gmail.com>
2024-10-14 17:44:55 -0400

    Add Vite proxy and configuration-based routing
    Introduce conditional routing based on development or production configuration. Implement a Vite proxy to forward requests to the Vite server in development mode and handle larger payloads efficiently.

--- src-actix/main.rs
+++ src-actix/main.rs
@@ -9,10 +9,12 @@ mod server_endpoint;
 mod server_properties_endpoint;
 mod server_settings_endpoint;
 mod system_stats_endpoint;
-use actix_files::file_extension_to_mime;
-use actix_web::error::ErrorInternalServerError;
 use actix_web::http::header;
-use actix_web::{error, get, middleware, web, App, HttpResponse, HttpServer, Responder};
+use actix_web::{
+    error, get, middleware, web, App, Error, HttpRequest, HttpResponse, HttpServer, Responder,
+};
+use awc::Client;
+use futures_util::stream::StreamExt;
 use include_dir::{include_dir, Dir};
 use log::{debug, error, info};
 use network_utility::{close_all_ports, open_port};
@@ -40,9 +42,17 @@ async fn main() -> std::io::Result<()> {
     // Spawn a thread to refresh the upnp port every 5 minutes
     open_port!(port, "Obsidian Minecraft Server Manager");
 
+    let config = if cfg!(debug_assertions) {
+        "development"
+    } else {
+        "production"
+    };
+
+    info!("Starting {} server at http://127.0.0.1:{}", config, port);
+
     let sys = web::Data::new(Mutex::new(System::new_all()));
     let server = HttpServer::new(move || {
-        App::new()
+        let app = App::new()
             .wrap(middleware::Logger::default())
             .wrap(auth_middleware::AuthMiddleware)
             .wrap_fn(|req, srv| {
@@ -139,10 +149,18 @@ async fn main() -> std::io::Result<()> {
                             .service(server_endpoint::create_server)
                             .service(server_endpoint::get_supported_loaders),
                     ),
-            )
-            .service(web::scope("assets/{file}").service(assets))
-            // Handle all other routes by serving the index.html file
-            .default_service(web::route().to(index))
+            );
+        // Add conditional routing based on the config
+        if config == "development" {
+            app.default_service(web::route().to(proxy_to_vite))
+                .service(web::resource("/assets/{file:.*}").route(web::get().to(proxy_to_vite)))
+                .service(
+                    web::resource("/node_modules/{file:.*}").route(web::get().to(proxy_to_vite)),
+                )
+        } else {
+            app.default_service(web::route().to(index))
+                .service(web::resource("/assets/{file:.*}").route(web::get().to(index)))
+        }
     })
     .workers(4)
     .bind(format!("0.0.0.0:{port}", port = port))?
@@ -162,26 +180,114 @@ async fn main() -> std::io::Result<()> {
     stop_result
 }
 
-// Include the wwwroot directory from the OUT_DIR
-static WWWROOT: Dir = include_dir!("target/wwwroot");
+/// The maximum payload size allowed for forwarding requests and responses.
+///
+/// This constant defines the maximum size (in bytes) for the request and response payloads
+/// when proxying. Any payload exceeding this size will result in an error.
+///
+/// Currently, it is set to 1 GB.
+const MAX_PAYLOAD_SIZE: usize = 1024 * 1024 * 1024; // 1 GB
 
-// Function to serve the index.html file
-async fn index() -> Result<impl Responder, actix_web::Error> {
+/// Static directory including all files under `target/wwwroot`.
+///
+/// This static directory is used to embed files into the binary at compile time.
+/// The `WWWROOT` directory will be used to serve static files such as `index.html`.
+static WWWROOT: Dir = include_dir!("target/wwwroot");
+/// Handles the request for the index.html file.
+///
+/// This function serves the `index.html` file from the embedded directory
+/// if it exists, and returns an internal server error if the file is not found.
+///
+/// # Arguments
+///
+/// * `_req` - The HTTP request object.
+///
+/// # Returns
+///
+/// An `impl Responder` which can either be a successful HTTP response containing
+/// the `index.html` file, or an internal server error.
+async fn index(_req: HttpRequest) -> Result<impl Responder, Error> {
     if let Some(file) = WWWROOT.get_file("index.html") {
         let body = file.contents();
         return Ok(HttpResponse::Ok().content_type("text/html").body(body));
     }
-    Err(ErrorInternalServerError("Failed to find index.html"))
+    Err(error::ErrorInternalServerError("Failed to find index.html"))
 }
-#[get("")]
-async fn assets(file: web::Path<String>) -> impl Responder {
-    if let Some(file) = WWWROOT.get_file(format!("assets/{}", file.as_str())) {
-        let body = file.contents();
-        return Ok(HttpResponse::Ok()
-            .content_type(file_extension_to_mime(
-                file.path().extension().unwrap().to_str().unwrap(),
-            ))
-            .body(body));
+
+/// Proxies requests to the Vite development server.
+///
+/// This function forwards incoming requests to a local Vite server running on port 3000.
+/// It buffers the entire request payload and response payload to avoid partial transfers.
+/// Requests and responses larger than the maximum payload size will result in an error.
+///
+/// # Arguments
+///
+/// * `req` - The HTTP request object.
+/// * `payload` - The request payload.
+///
+/// # Returns
+///
+/// An `HttpResponse` which contains the response from the Vite server,
+/// or an error response in case of failure.
+async fn proxy_to_vite(req: HttpRequest, mut payload: web::Payload) -> Result<HttpResponse, Error> {
+    let client = Client::new();
+    let forward_url = format!("http://localhost:3000{}", req.uri());
+
+    // Buffer the entire payload
+    let mut body_bytes = web::BytesMut::new();
+    while let Some(chunk) = payload.next().await {
+        let chunk = chunk?;
+        if (body_bytes.len() + chunk.len()) > MAX_PAYLOAD_SIZE {
+            return Err(actix_web::error::ErrorPayloadTooLarge("Payload overflow"));
+        }
+        body_bytes.extend_from_slice(&chunk);
     }
-    Err(ErrorInternalServerError(format!("Failed to find {}", file)))
+
+    let mut forwarded_resp = client
+        .request_from(forward_url.as_str(), req.head())
+        .no_decompress()
+        .send_body(body_bytes)
+        .await
+        .map_err(|err| {
+            actix_web::error::ErrorInternalServerError(format!(
+                "Failed to forward request: {}",
+                err
+            ))
+        })?;
+
+    // Buffer the entire response body
+    let mut resp_body_bytes = web::BytesMut::new();
+    while let Some(chunk) = forwarded_resp.next().await {
+        let chunk = chunk?;
+        if (resp_body_bytes.len() + chunk.len()) > MAX_PAYLOAD_SIZE {
+            return Err(actix_web::error::ErrorPayloadTooLarge(
+                "Response payload overflow",
+            ));
+        }
+        resp_body_bytes.extend_from_slice(&chunk);
+    }
+
+    // Build the response
+    let mut res = HttpResponse::build(forwarded_resp.status());
+
+    // Copy headers
+    for (header_name, header_value) in forwarded_resp.headers().iter() {
+        res.insert_header((header_name.clone(), header_value.clone()));
+    }
+
+    Ok(res.body(resp_body_bytes))
+}
+
+/// Handles requests to check the server status.
+///
+/// This endpoint responds to GET requests with a JSON object indicating
+/// that the server is running correctly. It can be used for health checks
+/// or monitoring server status.
+///
+/// # Returns
+///
+/// A JSON object with a `status` field set to "ok".
+#[get("/")]
+async fn status() -> impl Responder {
+    HttpResponse::Ok().json(json!({ "status": "ok" }))
 }
