91b67dd7ee2f2991dc472804652f7a733798b833
Drew (Linux) <dcmanproductions@gmail.com>
2024-10-14 17:45:18 -0400

    Remove obsolete build scripts
    Removed old JavaScript build and deployment scripts from the project. These scripts are no longer needed and have been entirely deleted to clean up the codebase.

--- scripts/package.js
+++
@@ -1,8 +0,0 @@
-import {buildBackend, buildFrontend, cleanup, deploy, incrementVersion} from "./app.js";
-
-console.log("Packaging...");
-cleanup();
-buildFrontend();
-buildBackend();
-await deploy();
-cleanup();
--- scripts/publish.js
+++
@@ -1,9 +0,0 @@
-import {buildBackend, buildFrontend, cleanup, deploy, incrementVersion} from "./app.js";
-
-console.log("Publishing...");
-cleanup();
-incrementVersion();
-buildFrontend();
-buildBackend();
-await deploy();
-cleanup();
