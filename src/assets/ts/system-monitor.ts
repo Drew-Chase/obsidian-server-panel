537643901291f887d27a601bbc0a983d7a5cba68
Drew (Linux) <dcmanproductions@gmail.com>
2024-10-14 21:32:38 -0400

    Switch to WebSocket for real-time system monitoring
    Replaced jQuery-based polling with WebSocket for continuous and real-time system usage data updates. This improves efficiency and ensures immediate data communication between the client and server.

--- src/assets/ts/system-monitor.ts
+++ src/assets/ts/system-monitor.ts
@@ -1,6 +1,3 @@
-import $ from "jquery";
-import {api_domain} from "../../main.tsx";
-
 export interface SystemMonitorResponse
 {
     cores: number[];
@@ -31,25 +28,35 @@ export const SystemMonitorResponseDefault: SystemMonitorResponse = {
 
 export default class SystemMonitor
 {
-    private interval: number = 0;
     public current_data: SystemMonitorResponse = SystemMonitorResponseDefault;
+    private socket: WebSocket | null = null;
 
-    async ping(): Promise<SystemMonitorResponse>
+    startMonitoring(action: (data: SystemMonitorResponse) => void): void
     {
-        return this.current_data = await $.get(`${api_domain}/system/usage`);
-    }
-
-    startMonitoring(update_interval: number, action: (data: SystemMonitorResponse) => void): void
-    {
-        this.interval = setInterval(async () =>
+        this.socket = new WebSocket("/api/system/usage/ws");
+        this.socket.addEventListener("open", () =>
         {
-            action(await this.ping());
-        }, update_interval);
+            console.log("Connected to system monitor websocket");
+        });
+        this.socket.addEventListener("message", (event) =>
+        {
+            const data = JSON.parse(event.data);
+            action(data);
+        });
+        this.socket.addEventListener("close", () =>
+        {
+            console.log("Disconnected from system monitor websocket");
+        });
+        this.socket.addEventListener("error", (event) =>
+        {
+            console.error("Error on system monitor websocket", event);
+        });
     }
 
     stopMonitoring(): void
     {
-        clearInterval(this.interval);
+        this.socket?.close();
     }
+
 
 }
