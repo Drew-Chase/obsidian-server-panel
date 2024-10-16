dca7bd351ced28c5b0581aabe51706c727311932
Drew Chase (Work) <dcmanproductions@gmail.com>
2024-10-16 11:12:13 -0400

    Add aria-label and textValue to Listbox items
    Added an aria-label to the Listbox for accessibility and included textValue properties to ListboxItem for descriptive purposes. These changes improve screen reader compatibility and provide better context to users.

--- src/assets/components/Server/DiscoverMods.tsx
+++ src/assets/components/Server/DiscoverMods.tsx
@@ -48,12 +48,13 @@ export default function DiscoverMods()
             </div>
             <div className={"flex flex-col gap-4 mt-4 rounded-lg bg-neutral-800 p-4 overflow-y-auto max-h-[calc(100dvh_-_240px)] h-screen"}>
                 <Listbox
+                    aria-label="List of discovered mods"
                     itemClasses={{
-                        base: "data-[hover]:bg-neutral-600",
+                        base: "data-[hover]:bg-neutral-600"
                     }}
                 >
                     {Array.from({length: 10}).map((_, i) => (
-                        <ListboxItem key={i}>
+                        <ListboxItem key={i} textValue={"Sodium - The fastest and most compatible rendering optimization mod for Minecraft. Now available for both NeoForge and Fabric!"}>
                             <DiscoverModItem icon={icon} name={"Sodium"} author={"jellysquid3"} description={"The fastest and most compatible rendering optimization mod for Minecraft. Now available for both NeoForge and Fabric!"}/>
                         </ListboxItem>
                     ))}
