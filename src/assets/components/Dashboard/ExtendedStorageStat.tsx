01f1965ca8f490a3c498af22a38a70b0b6d3c162
Drew Chase (Work) <dcmanproductions@gmail.com>
2024-10-16 11:13:38 -0400

    Add aria labels and toast notification for a11y
    Enhance accessibility by adding ARIA labels to interactive elements in the ExtendedStorageStat component. Introduce toast notifications to confirm CSV export action.

--- src/assets/components/Dashboard/ExtendedStorageStat.tsx
+++ src/assets/components/Dashboard/ExtendedStorageStat.tsx
@@ -1,23 +1,26 @@
 import {Button, CircularProgress, Listbox, ListboxItem, Tooltip} from "@nextui-org/react";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {faArrowDown, faCircle} from "@fortawesome/free-solid-svg-icons";
+import {faArrowDown, faCircle, faCloudDownload} from "@fortawesome/free-solid-svg-icons";
+import {toast} from "sonner";
 
 export default function ExtendedStorageStat()
 {
     return (
-        <div className={"flex flex-col bg-neutral-600 rounded-3xl shadow-lg p-8 max-w-md w-full mx-2 h-lg"}>
+        <div className={"flex flex-col bg-neutral-600 rounded-3xl shadow-lg p-8 max-w-md w-full mx-2 h-lg"} aria-label="Extended Storage Statistic">
             <div className={"flex flex-row w-full items-center"}>
                 <p className={"text-lg font-semibold"}>Storage Usage</p>
-                <Tooltip content={"Export a spreadsheet csv with a breakdown of your storage consumption."}>
+                <Tooltip content={"Export a spreadsheet csv with a breakdown of your storage consumption."} aria-label="Export Tooltip">
                     <Button
                         endContent={<FontAwesomeIcon icon={faArrowDown}/>}
                         className={"ml-auto"}
+                        aria-label="Export to CSV"
+                        onClick={()=>toast("Exported CSV file.", {description: "A CSV file has been exported with a breakdown of your storage consumption.", icon: <FontAwesomeIcon icon={faCloudDownload}/>})}
                     >
                         Export
                     </Button>
                 </Tooltip>
             </div>
-            <div className={"relative w-[300px] h-[300px] mx-auto my-4"}>
+            <div className={"relative w-[300px] h-[300px] mx-auto my-4"} aria-label="Storage Usage Circular Progress">
                 <p className={"absolute text-4xl font-semibold top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/6"}>800GB</p>
                 <div className={"-rotate-90 -translate-x-[165px] translate-y-[165px]"}>
                     <CircularProgress
@@ -30,6 +33,7 @@ export default function ExtendedStorageStat()
                             track: "stroke-transparent",
                             indicator: "stroke-[#00C2FF]"
                         }}
+                        aria-label="Blue progress indicator"
                     />
                     <CircularProgress
                         value={60}
@@ -41,6 +45,7 @@ export default function ExtendedStorageStat()
                             track: "stroke-transparent",
                             indicator: "stroke-[#0E43FB]"
                         }}
+                        aria-label="Dark blue progress indicator"
                     />
                     <CircularProgress
                         value={80}
@@ -51,16 +56,18 @@ export default function ExtendedStorageStat()
                             svg: "w-[300px] h-[300px]",
                             track: "stroke-transparent"
                         }}
+                        aria-label="Primary progress indicator"
                     />
                 </div>
             </div>
-            <Listbox className={"mt-4"}>
+            <Listbox className={"mt-4"} aria-label="Storage Usage Listbox">
                 <ListboxItem
                     key={"primary"}
                     startContent={
                         <FontAwesomeIcon icon={faCircle} width={8} color={"#CB3CFF"}/>
                     }
                     endContent={<p className={"text-tiny opacity-70"}> 400GB - 50%</p>}
+                    aria-label="SMP Server storage usage"
                 >
                     <span className={"text-neutral-400"}>SMP Server</span>
                 </ListboxItem>
@@ -70,8 +77,8 @@ export default function ExtendedStorageStat()
                         <FontAwesomeIcon icon={faCircle} width={8} color={"#0E43FB"}/>
                     }
                     endContent={<p className={"text-tiny opacity-70"}>400GB - 50%</p>}
+                    aria-label="All The Mods 6 storage usage"
                 >
-
                     <span className={"text-neutral-400"}>All The Mods 6</span>
                 </ListboxItem>
                 <ListboxItem
@@ -80,6 +87,7 @@ export default function ExtendedStorageStat()
                         <FontAwesomeIcon icon={faCircle} width={8} color={"#00C2FF"}/>
                     }
                     endContent={<p className={"text-tiny opacity-70"}>400GB - 50%</p>}
+                    aria-label="Other storage usage"
                 >
                     <span className={"text-neutral-400"}>Other</span>
                 </ListboxItem>
