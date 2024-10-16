b4e8f84f0bbd858cda660af3a585abd5f06f4e45
Drew Chase (Work) <dcmanproductions@gmail.com>
2024-10-16 11:13:49 -0400

    Add EditorModal to CrashReportsStat component
    Integrated EditorModal to open on click of a crash report item. Introduced useState to manage the modal's open state and enhanced accessibility features for listbox items.

--- src/assets/components/Dashboard/CrashReportsStat.tsx
+++ src/assets/components/Dashboard/CrashReportsStat.tsx
@@ -1,5 +1,7 @@
 import {Avatar, Listbox, ListboxItem, ScrollShadow} from "@nextui-org/react";
 import testImage from "../../images/demo/test-server.png";
+import {useState} from "react";
+import EditorModal from "../EditorModal.tsx";
 
 interface CrashReportsStatProps
 {
@@ -8,35 +10,42 @@ interface CrashReportsStatProps
 
 export default function CrashReportsStat(props: CrashReportsStatProps)
 {
+    const [isEditorOpen, setIsEditorOpen] = useState(false);
     return (
-        <div className={"flex flex-col bg-neutral-600 rounded-3xl shadow-lg p-8 max-w-md w-full max-h-[400px] h-dvh overflow-y-auto"}>
-            <p className={"text-lg font-semibold"}>Crash Reports</p>
+        <>
+            <EditorModal isOpen={isEditorOpen} onClose={() => setIsEditorOpen(false)}/>
+            <div className={"flex flex-col bg-neutral-600 rounded-3xl shadow-lg p-8 max-w-md w-full max-h-[400px] h-dvh overflow-y-auto"}>
+                <p className={"text-lg font-semibold"}>Crash Reports</p>
 
-            <div className={"flex flex-row w-full text-tiny my-4"}>
-                <p className={"mr-auto"}>Description / Mod / Server</p>
-                <p>Timestamp</p>
-            </div>
-            <ScrollShadow className={"max-h-[300px] h-[400px] overflow-y-auto"}>
-                <Listbox>
-                    {Array.from({length: 10}, (_, i) => (
-                        <ListboxItem
-                            key={i}
-                            startContent={
-                                <div className={"rounded-md bg-neutral-800 p-1"}>
-                                    <Avatar src={testImage}/>
+                <div className={"flex flex-row w-full text-tiny my-4"}>
+                    <p className={"mr-auto"}>Description / Mod / Server</p>
+                    <p>Timestamp</p>
+                </div>
+                <ScrollShadow className={"max-h-[300px] h-[400px] overflow-y-auto"}>
+                    <Listbox aria-label={"A list of all recent crash reports"}>
+                        {Array.from({length: 10}, (_, i) => (
+                            <ListboxItem
+                                key={i}
+                                startContent={
+                                    <div className={"rounded-md bg-neutral-800 p-1"}>
+                                        <Avatar src={testImage}/>
+                                    </div>
+                                }
+                                description={props.serverName || "SMP Server"}
+                                endContent={<p className={"text-tiny text-nowrap"}>2:36:50 PM</p>}
+                                textValue={"Ticking entity"}
+                                aria-label={"Ticking entity"}
+                                onClick={() => setIsEditorOpen(true)}
+                            >
+                                <div className={"flex flex-row items-center gap-2"}>
+                                    <p className={"max-w-[90px] truncate"}>Ticking entity</p>
+                                    <p className={"max-w-[90px] truncate opacity-50 font-light text-tiny"}>carpet-tis-addition-v1.62.0-mc1.20.4.jar</p>
                                 </div>
-                            }
-                            description={props.serverName || "SMP Server"}
-                            endContent={<p className={"text-tiny text-nowrap"}>2:36:50 PM</p>}
-                        >
-                            <div className={"flex flex-row items-center gap-2"}>
-                                <p className={"max-w-[90px] truncate"}>Ticking entity</p>
-                                <p className={"max-w-[90px] truncate opacity-50 font-light text-tiny"}>carpet-tis-addition-v1.62.0-mc1.20.4.jar</p>
-                            </div>
-                        </ListboxItem>
-                    ))}
-                </Listbox>
-            </ScrollShadow>
-        </div>
+                            </ListboxItem>
+                        ))}
+                    </Listbox>
+                </ScrollShadow>
+            </div>
+        </>
     );
 }
