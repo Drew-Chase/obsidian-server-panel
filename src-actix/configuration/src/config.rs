a9cdf2285e8138fe54833edbc52c73c89bd5524f
Drew Chase (Work) <dcmanproductions@gmail.com>
2024-10-16 11:07:38 -0400

    Remove file handling functions from Config
    The file handling methods from `Config` struct have been removed. This change simplifies the configuration struct and its responsibilities, delegating file reading and writing to other parts of the codebase.

--- src-actix/configuration/src/config.rs
+++ src-actix/configuration/src/config.rs
@@ -1,5 +1,4 @@
 use serde_derive::{Deserialize, Serialize};
-use std::path::Path;
 
 #[derive(Deserialize, Serialize, Debug, Clone)]
 pub struct Config {
@@ -9,17 +8,4 @@ pub struct Config {
     pub servers_directory: String,
     pub backups_directory: String,
     pub java_install_directory: String,
-}
-
-impl Config {
-    pub fn from_file(file: impl AsRef<Path>) -> Result<Config, Box<dyn std::error::Error>> {
-        let contents = std::fs::read_to_string(file)?;
-        let config: Config = serde_json::from_str(&contents)?;
-        Ok(config)
-    }
-    pub fn to_file(&self, file: impl AsRef<Path>) -> Result<(), Box<dyn std::error::Error>> {
-        let contents = serde_json::to_string_pretty(self)?;
-        std::fs::write(file, contents)?;
-        Ok(())
-    }
 }
