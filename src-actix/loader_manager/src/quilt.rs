0585f288d2a23686ca1dbdc03620834fb637a263
Drew Chase (Work) <dcmanproductions@gmail.com>
2024-10-17 16:35:48 -0400

    Add quilt version management functions
    Implemented stub functions for version management in quilt.rs including versions(), download(), and install(). These functions currently use `todo!` macros and need further implementation to handle actual functionality.

---
+++ src-actix/loader_manager/src/quilt.rs
@@ -0,0 +1,9 @@
+pub fn versions() -> Vec<String> {
+	todo!("return all versions of quilt")
+}
+pub fn download(version: &str) -> Result<(), Box<dyn std::error::Error>> {
+	todo!("download quilt version {}", version)
+}
+pub fn install(version: &str) -> Result<(), Box<dyn std::error::Error>> {
+	todo!("install quilt version {}", version)
+}
