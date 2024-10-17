14817ddf1acb73c897a06953e142c7d7fa373c7f
Drew Chase (Work) <dcmanproductions@gmail.com>
2024-10-17 15:27:46 -0400

    Add modloader detection for instances
    Introduce a new `Modloader` enum and update the `Instance` interface to include `modloader`. Implement detection logic in the `browse` function to set `modloader` based on either `categories` or `game_versions` depending on the platform.

--- src/assets/ts/instances.ts
+++ src/assets/ts/instances.ts
@@ -22,6 +22,7 @@ export interface Instance
     game_versions: string[];
     categories: string[];
     project_url: string;
+    modloader: Modloader;
 }
 
 export interface InstalledInstance extends Instance
@@ -36,6 +37,16 @@ export enum Platforms
     MODRINTH = "Modrinth",
     CURSEFORGE = "Curseforge",
     ATLAUNCHER = "AtLauncher",
+}
+
+export enum Modloader
+{
+    FABRIC = "Fabric",
+    FORGE = "Forge",
+    NEOFORGE = "NeoForge",
+    QUILT = "Quilt",
+    RIFT = "Rift",
+    UNKNOWN = "Unknown"
 }
 
 export enum SortOptions
@@ -60,13 +71,25 @@ export default class Instances
     static async browse(options: BrowseOptions, signal: AbortSignal): Promise<InstanceSearchResult>
     {
         return fetch(`/api/instances/discover?search=${options.search}&sort=${options.sort}&platform=${options.platform}&limit=${options.limit}&offset=${options.offset}`, {
-            signal:signal
+            signal: signal
         })
             .then(response => response.json())
             .then((result: InstanceSearchResult) =>
             {
                 result.hits.forEach(hit =>
                 {
+                    switch (hit.platform)
+                    {
+                        case Platforms.MODRINTH:
+                            hit.modloader = Object.values(Modloader).find(modloader => hit.categories.map(i => i.toLowerCase()).includes(modloader.toLowerCase())) || Modloader.UNKNOWN;
+                            hit.categories = hit.categories.filter(category => category.toLowerCase() !== hit.modloader.toLowerCase());
+                            break;
+                        case Platforms.CURSEFORGE:
+                            hit.modloader = Object.values(Modloader).find(modloader => hit.game_versions.map(i => i.toLowerCase()).includes(modloader.toLowerCase())) || Modloader.UNKNOWN;
+                            hit.game_versions = hit.game_versions.filter(version => version.toLowerCase() !== hit.modloader.toLowerCase());
+                            break;
+
+                    }
                     if (hit.last_updated) hit.last_updated = new Date(hit.last_updated);
                     if (hit.published) hit.published = new Date(hit.published);
                 });
