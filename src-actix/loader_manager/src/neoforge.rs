168b6510d3ca897ab658e82c34c2266a5ae22701
Drew Chase (Work) <dcmanproductions@gmail.com>
2024-10-17 16:35:40 -0400

    Add neoforge.rs with placeholder functions
    Introduce `versions`, `download`, and `install` functions in neoforge.rs for managing Neoforge versions. These functions currently contain placeholders (todo!) and need proper implementation.

---
+++ src-actix/loader_manager/src/neoforge.rs
@@ -0,0 +1,9 @@
+pub fn versions() -> Vec<String> {
+	todo!("return all versions of neoforge")
+}
+pub fn download(version: &str) -> Result<(), Box<dyn std::error::Error>> {
+	todo!("download neoforge version {}", version)
+}
+pub fn install(version: &str) -> Result<(), Box<dyn std::error::Error>> {
+	todo!("install neoforge version {}", version)
+}
