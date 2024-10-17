f8b734a663393290ce9b47f96dc210874234aa23
Drew Chase (Work) <dcmanproductions@gmail.com>
2024-10-17 14:29:32 -0400

    Add dynamic modpack fetching to DiscoverModpacks
    Implemented useEffect to dynamically fetch and update the list of modpacks based on search, platform, and sorting criteria. Added loading spinner and improved component interactions to enhance user experience.

--- src/assets/pages/DiscoverModpacks.tsx
+++ src/assets/pages/DiscoverModpacks.tsx
@@ -1,68 +1,109 @@
-import {Input, Listbox, ListboxItem, Select, SelectItem} from "@nextui-org/react";
+import {Input, Listbox, ListboxItem, Select, SelectItem, Spinner, Tooltip} from "@nextui-org/react";
 import MagnifyGlass from "../images/MagnifyGlass.svg.tsx";
 import DiscoverModItem from "../components/Server/DiscoverModItem.tsx";
-import icon from "../images/demo/test-mod-icon.webp";
 import {setTitle} from "../../main.tsx";
+import Instances, {Instance, Platforms, SortOptions} from "../ts/instances.ts";
+import {useEffect, useState} from "react";
 
 export default function DiscoverModpacks()
 {
+    const [instances, setInstances] = useState<Instance[]>([]);
+    const [search, setSearch] = useState<string>("");
+    const [platform, setPlatform] = useState<Platforms>(Platforms.ALL);
+    const [sort, setSort] = useState<SortOptions>(SortOptions.RELEVANT);
+    const [loading, setLoading] = useState<boolean>(true);
+    let abortController = new AbortController();
+
+
+    useEffect(() =>
+    {
+        setLoading(true);
+        abortController.abort();
+        abortController = new AbortController();
+        Instances.browse({
+            platform: platform,
+            search: search,
+            sort: sort,
+            limit: 10,
+            offset: 0
+        }, abortController.signal)
+            .then(i => i.hits)
+            .then(setInstances)
+            .finally(() => setLoading(false));
+    }, [search, platform, sort]);
+
     setTitle("Discover Modpacks");
     return (
-
         <div className={"flex flex-col bg-neutral-600 rounded-2xl shadow-lg p-8 grow w-full mx-2 gap-4"}>
             <div className={"flex flex-col"}>
                 <div className={"flex flex-row items-center gap-4"}>
                     <h2 className={"text-2xl font-semibold text-nowrap"}>Discover Modpacks</h2>
-                    <Input
-                        label={"Search"}
-                        placeholder={"Search for mods"}
-                        startContent={<MagnifyGlass/>}
-                        className={"w-full"}
-                        classNames={{
-                            inputWrapper: "bg-neutral-700"
-                        }}
-                    />
-                    <Select
-                        label={"Source"}
-                        disallowEmptySelection
-                        defaultSelectedKeys={["all"]}
-                        className={"w-[400px]"}
-                        classNames={{
-                            trigger: "bg-neutral-700"
-                        }}
-                    >
-                        <SelectItem key={"all"}>All</SelectItem>
-                        <SelectItem key={"modrinth"}>Modrinth</SelectItem>
-                        <SelectItem key={"curseforge"}>CurseForge</SelectItem>
-                    </Select>
-                    <Select
-                        label={"Sort By"}
-                        disallowEmptySelection
-                        defaultSelectedKeys={["relevance"]}
-                        className={"w-[400px]"}
-                        classNames={{
-                            trigger: "bg-neutral-700"
-                        }}
-                    >
-                        <SelectItem key={"relevance"}>Relevance</SelectItem>
-                        <SelectItem key={"downloads"}>Download count</SelectItem>
-                        <SelectItem key={"published"}>Published Date</SelectItem>
-                        <SelectItem key={"updated"}>Updated Date</SelectItem>
-                    </Select>
+                    <div className={"-mb-4 flex flex-row items-center gap-4 w-full"}>
+                        <Input
+                            label={"Search"}
+                            placeholder={"Search for mods"}
+                            startContent={<MagnifyGlass/>}
+                            className={"w-full"}
+                            classNames={{
+                                inputWrapper: "bg-neutral-700 data-[focus]:!bg-neutral-800 data-[hover]:!bg-neutral-800",
+                            }}
+                            value={search}
+                            onValueChange={setSearch}
+                            description={"Search for mods by name, author, or category"}
+                        />
+                        <Select
+                            label={"Source"}
+                            disallowEmptySelection
+                            defaultSelectedKeys={[platform]}
+                            disabledKeys={[Platforms.ATLAUNCHER]}
+                            className={"w-[400px]"}
+                            classNames={{
+                                trigger: "bg-neutral-700 data-[hover]:!bg-neutral-800"
+                            }}
+                            onSelectionChange={(key) => setPlatform([...key][0] as string as Platforms)}
+                            value={platform}
+                            description={"AtLauncher is not supported yet"}
+                        >
+                            {Object.values(Platforms).map(platform => (
+                                <SelectItem key={platform}>{platform}</SelectItem>
+                            ))}
+                        </Select>
+                        <Tooltip content={"Sort options only work for Modrinth"} closeDelay={0}>
+                            <Select
+                                label={"Sort By"}
+                                disallowEmptySelection
+                                defaultSelectedKeys={[sort]}
+                                className={"w-[400px]"}
+                                classNames={{
+                                    trigger: "bg-neutral-700 data-[hover]:!bg-neutral-800"
+                                }}
+                                onSelectionChange={(key) => setSort([...key][0] as string as SortOptions)}
+                                value={sort}
+                                description={"Sort options only work for Modrinth"}
+                            >
+                                {Object.values(SortOptions).map(sort => (
+                                    <SelectItem key={sort}>{sort}</SelectItem>
+                                ))}
+                            </Select>
+                        </Tooltip>
+                    </div>
                 </div>
                 <div className={"flex flex-col gap-4 mt-4 rounded-lg bg-neutral-800 p-4 overflow-y-auto max-h-[calc(100dvh_-_200px)] h-screen"}>
-                    <Listbox
-                        aria-label="Discover Modpacks List"
-                        itemClasses={{
-                            base: "data-[hover]:bg-neutral-600"
-                        }}
-                    >
-                        {Array.from({length: 10}).map((_, i) => (
-                            <ListboxItem key={i} textValue={`Mod Item ${i + 1}`}>
-                                <DiscoverModItem icon={icon} name={"Sodium"} author={"jellysquid3"} description={"The fastest and most compatible rendering optimization mod for Minecraft. Now available for both NeoForge and Fabric!"}/>
-                            </ListboxItem>
-                        ))}
-                    </Listbox>
+                    {loading ? <Spinner color={"primary"} className={"mx-auto"}/> : (
+                        <Listbox
+                            aria-label="Discover Modpacks List"
+                            itemClasses={{
+                                base: "data-[hover]:bg-neutral-600"
+                            }}
+                            emptyContent={"No mods found"}
+                        >
+                            {instances.map((instance, i) => (
+                                <ListboxItem key={i} textValue={instance.name}>
+                                    <DiscoverModItem {...instance}/>
+                                </ListboxItem>
+                            ))}
+                        </Listbox>
+                    )}
                 </div>
             </div>
         </div>
