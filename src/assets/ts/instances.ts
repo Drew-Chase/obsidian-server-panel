b4ae57f3ad7d6f595947a1483495a76d7981d21c
Drew Chase (Work) <dcmanproductions@gmail.com>
2024-10-17 14:30:18 -0400

    Add advanced search result structure and fetch implementation
    This commit introduces a new `InstanceSearchResult` type to encapsulate search results, including pagination details. Additionally, it modifies the `browse` method to use the Fetch API for more granular error handling and extensibility, and ensures proper date parsing for `last_updated` and `published` fields.

--- src/assets/ts/instances.ts
+++ src/assets/ts/instances.ts
@@ -1,3 +1,10 @@
+type InstanceSearchResult = {
+    hits: Instance[];
+    total_hits: number;
+    limit: number;
+    offset: number;
+}
+
 export interface Instance
 {
     id: string;
@@ -6,28 +13,38 @@ export interface Instance
     description: string;
     downloads: number;
     likes: number;
-    lastUpdated: Date;
-    published: Date;
+    last_updated?: Date;
+    published?: Date;
     platform: Platforms;
+    icon: string;
+    gallery: string[];
+    versions: string[];
+    game_versions: string[];
+    categories: string[];
+    project_url: string;
 }
 
-export interface InstalledInstance extends Instance{
+export interface InstalledInstance extends Instance
+{
     servers: string[];
     owner: string;
 }
 
 export enum Platforms
 {
-    MODRINTH = "modrinth",
-    CURSEFORGE = "curseforge",
-    ATLAUNCHER = "at-launcher",
+    ALL = "All",
+    MODRINTH = "Modrinth",
+    CURSEFORGE = "Curseforge",
+    ATLAUNCHER = "AtLauncher",
 }
 
 export enum SortOptions
 {
-    RELEVANT = "relevant",
-    POPULAR = "popular",
-    NEWEST = "newest",
+    RELEVANT = "Relevance",
+    POPULAR = "Downloads",
+    NEWEST = "Newest",
+    FOLLOWS = "Follows",
+    UPDATED = "Updated",
 }
 
 type BrowseOptions = {
@@ -40,8 +57,20 @@ type BrowseOptions = {
 
 export default class Instances
 {
-    static async browse(options: BrowseOptions): Promise<Instance[]>
+    static async browse(options: BrowseOptions, signal: AbortSignal): Promise<InstanceSearchResult>
     {
-        return $.get("/api/");
+        return fetch(`/api/instances/discover?search=${options.search}&sort=${options.sort}&platform=${options.platform}&limit=${options.limit}&offset=${options.offset}`, {
+            signal:signal
+        })
+            .then(response => response.json())
+            .then((result: InstanceSearchResult) =>
+            {
+                result.hits.forEach(hit =>
+                {
+                    if (hit.last_updated) hit.last_updated = new Date(hit.last_updated);
+                    if (hit.published) hit.published = new Date(hit.published);
+                });
+                return result;
+            });
     }
 }
