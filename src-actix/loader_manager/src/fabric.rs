7f8a317ac1c33c98cd2295e1ddda51541560e559
Drew Chase (Work) <dcmanproductions@gmail.com>
2024-10-17 16:35:19 -0400

    Add fabric.rs with placeholder functions
    Introduced placeholder functions for listing, downloading, and installing fabric versions in the newly added fabric.rs file. These functions currently contain todo! macros to be implemented in the future.

---
+++ src-actix/loader_manager/src/fabric.rs
@@ -0,0 +1,9 @@
+pub fn versions() -> Vec<String> {
+    todo!("listing all fabric versions")
+}
+pub fn download(version: &str) -> Result<(), Box<dyn std::error::Error>> {
+    todo!("downloading fabric version {}", version)
+}
+pub fn install(version: &str) -> Result<(), Box<dyn std::error::Error>> {
+    todo!("installing fabric version {}", version)
+}
