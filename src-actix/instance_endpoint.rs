dad937cd7649c5fa16946ef086fbbe4b2372c392
Drew Chase (Work) <dcmanproductions@gmail.com>
2024-10-17 11:45:55 -0400

    Update modpack search to use instance_manager
    Refactor `discover_modpacks` to use `instance_manager` instead of `minecraft_modding_platforms`. Enhanced error response to include the error message in JSON format.

--- src-actix/instance_endpoint.rs
+++ src-actix/instance_endpoint.rs
@@ -1,12 +1,14 @@
 use actix_web::{get, web, HttpResponse, Responder};
-use minecraft_modding_platforms::modpacks::BrowseOptions;
+use instance_manager::modpacks::BrowseOptions;
+use serde_json::json;
 
 #[get("discover")]
 pub async fn discover_modpacks(query: web::Query<BrowseOptions>) -> impl Responder {
     let options = query.into_inner();
-    let results = minecraft_modding_platforms::modpacks::search_modpacks(options).await;
+    let results = instance_manager::modpacks::search_modpacks(options).await;
     match results {
         Ok(data) => HttpResponse::Ok().json(data),
-        Err(_) => HttpResponse::BadRequest().json("Failed to search modpacks")
+        Err(e) => HttpResponse::BadRequest()
+            .json(json!({"message": "Failed to fetch modpacks", "error":format!("{}", e)})),
     }
 }
