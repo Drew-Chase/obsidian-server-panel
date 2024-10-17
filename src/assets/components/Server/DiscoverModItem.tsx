7e972b59601a5a64ad7187cae1d013fdf2a5e421
Drew Chase (Work) <dcmanproductions@gmail.com>
2024-10-17 14:29:57 -0400

    Update DiscoverModItem to use Instance interface
    Replaced ModItemProps with Instance interface for better type integration. Added Tooltip for download chip and unified number formatting using the Conversions module. Adjusted link button to dynamically reflect the project URL and platform.

--- src/assets/components/Server/DiscoverModItem.tsx
+++ src/assets/components/Server/DiscoverModItem.tsx
@@ -1,37 +1,36 @@
-import {Avatar, Button, Chip, Link} from "@nextui-org/react";
+import {Avatar, Button, Chip, Link, Tooltip} from "@nextui-org/react";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faCalendar, faDownload, faHeart} from "@fortawesome/free-solid-svg-icons";
+import {Instance} from "../../ts/instances.ts";
+import Conversions from "../../ts/conversions.ts";
 
-interface ModItemProps
-{
-    icon?: string;
-    name: string;
-    description?: string;
-    author?: string;
-}
 
-export default function DiscoverModItem(props: ModItemProps)
+export default function DiscoverModItem(instance: Instance)
 {
     return (
         <div className={"flex flex-col shrink-0 p-4 justify-between"}>
             <div className={"flex flex-row shrink-0"}>
-                {props.icon && <Avatar src={props.icon} radius={"sm"} size={"lg"} className={"mr-4"}/>}
+                {instance.icon && <Avatar src={instance.icon} radius={"sm"} size={"lg"} className={"mr-4"}/>}
                 <div className={"flex flex-col items-start"}>
                     <div className={"flex flex-row items-center"}>
-                        <p className={"text-large font-bold mr-2"}>{props.name}</p>
-                        <p className={"text-tiny opacity-70"}>By {props.author}</p>
+                        <p className={"text-large font-bold mr-2"}>{instance.name}</p>
+                        <p className={"text-tiny opacity-70"}>By {instance.author}</p>
                     </div>
-                    <p className={"opacity-70"}>{props.description}</p>
+                    <p className={"opacity-70"}>{instance.description}</p>
                 </div>
             </div>
             <div className={"flex flex-row mt-4 gap-3 items-center"}>
                 <div className={"flex flex-row gap-3 mr-auto"}>
-                    <Chip radius={"sm"} color={"default"} variant={"flat"}><FontAwesomeIcon icon={faDownload} className={"mr-3"}/> 20.44M</Chip>
-                    <Chip radius={"sm"} color={"default"} variant={"flat"}><FontAwesomeIcon icon={faHeart} className={"mr-3 text-red-600"}/>16.6K</Chip>
-                    <Chip radius={"sm"} color={"default"} variant={"flat"}><FontAwesomeIcon icon={faCalendar} className={"mr-3"}/>4 hours ago</Chip>
+                    <Tooltip content={`${instance.downloads.toLocaleString("en-us")} Downloads`} closeDelay={0}>
+                        <Chip radius={"sm"} color={"default"} variant={"flat"}><FontAwesomeIcon icon={faDownload} className={"mr-3"}/> {Conversions.formatLargeNumber(instance.downloads, 1)}</Chip>
+                    </Tooltip>
+                    <Chip radius={"sm"} color={"default"} variant={"flat"}><FontAwesomeIcon icon={faHeart} className={"mr-3 text-red-600"}/>{Conversions.formatLargeNumber(instance.likes, 1)}</Chip>
+                    {instance.last_updated && (
+                        <Chip radius={"sm"} color={"default"} variant={"flat"}><FontAwesomeIcon icon={faCalendar} className={"mr-3"}/>{Conversions.formatTimeClosestRelative(instance.last_updated)}</Chip>
+                    )}
                 </div>
                 <Button color={"primary"} variant={"flat"} startContent={<FontAwesomeIcon icon={faDownload} className={"mr-1"}/>}>Install</Button>
-                <Button href={"https://modrinth.com/mod/sodium"} as={Link} variant={"flat"} showAnchorIcon isExternal>View on Modrinth</Button>
+                <Button href={instance.project_url} as={Link} variant={"flat"} showAnchorIcon isExternal target={"_blank"}>View on {instance.platform}</Button>
             </div>
         </div>
     );
