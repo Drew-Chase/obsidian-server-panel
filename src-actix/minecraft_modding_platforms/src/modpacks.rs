15d844a29523d40ab2dda8da62a87102c9680fcd
Drew (Linux) <dcmanproductions@gmail.com>
2024-10-16 22:28:15 -0400

    Add modpack search functionality
    Introduce structures and functions to handle modpack searching across multiple platforms. This includes defining the modpack and search result structures, and implementing async search functions for each platform.

---
+++ src-actix/minecraft_modding_platforms/src/modpacks.rs
@@ -0,0 +1,125 @@
+use crate::Platform;
+use chrono::{DateTime, Utc};
+use rayon::prelude::*;
+use serde::{Deserialize, Serialize};
+use std::fmt::Display;
+use std::ops::AddAssign;
+
+#[derive(Serialize, Deserialize)]
+pub struct ModpackSearchResults {
+    pub hits: Vec<Modpack>,
+    pub offset: i64,
+    pub limit: i64,
+    pub total_hits: i64,
+}
+#[derive(Serialize, Deserialize)]
+pub struct Modpack {
+    pub(crate) id: String,
+    pub(crate) name: String,
+    pub(crate) author: String,
+    pub(crate) description: String,
+    pub(crate) icon: Option<String>,
+    pub(crate) gallery: Option<Vec<String>>,
+    pub(crate) versions: Option<Vec<String>>,
+    pub(crate) game_versions: Option<Vec<String>>,
+    pub(crate) downloads: u32,
+    pub(crate) likes: Option<u32>,
+    pub(crate) last_updated: Option<DateTime<Utc>>,
+    pub(crate) published: Option<DateTime<Utc>>,
+    pub(crate) platform: Platform,
+    pub(crate) categories: Option<Vec<String>>,
+    pub(crate) project_url: Option<String>,
+}
+
+#[derive(Deserialize, Clone)]
+pub enum SortOptions {
+    Relevance,
+    Downloads,
+    Follows,
+    Newest,
+    Updated,
+}
+
+impl Display for SortOptions {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let str = match self {
+            SortOptions::Relevance => "relevance".to_string(),
+            SortOptions::Downloads => "downloads".to_string(),
+            SortOptions::Follows => "follows".to_string(),
+            SortOptions::Newest => "newest".to_string(),
+            SortOptions::Updated => "updated".to_string(),
+        };
+        write!(f, "{}", str)
+    }
+}
+
+#[derive(Deserialize, Clone)]
+pub struct BrowseOptions {
+    pub search: String,
+    pub sort: SortOptions,
+    pub platform: Platform,
+    pub limit: u32,
+    pub offset: u32,
+}
+
+impl ModpackSearchResults {
+    pub fn merge(&mut self, other: ModpackSearchResults) {
+        self.hits.extend(other.hits);
+        self.offset = other.offset;
+        self.limit = other.limit;
+        self.total_hits = other.total_hits;
+    }
+}
+
+impl AddAssign for ModpackSearchResults {
+    fn add_assign(&mut self, other: Self) {
+        self.merge(other);
+    }
+}
+
+pub async fn search_modpacks(
+    options: BrowseOptions,
+) -> Result<ModpackSearchResults, Box<dyn std::error::Error>> {
+    match options.platform {
+        Platform::All => {
+            let mut results = ModpackSearchResults {
+                hits: vec![],
+                offset: 0,
+                limit: 0,
+                total_hits: 0,
+            };
+
+            let searches = vec![
+                crate::modrinth::ModrinthPackSearchResults::search(options.clone()),
+                crate::curseforge::CurseForgePackSearchResults::search(options.clone()),
+                crate::atlauncher::ATLauncherPackSearchResults::search(),
+            ];
+
+            let search_results: Result<Vec<_>, _> = searches
+                .into_par_iter()
+                .map(|search| async { search.await?.to_modpack_results() })
+                .collect::<Vec<_>>()
+                .await
+                .into_iter()
+                .collect();
+
+            for result in search_results? {
+                results += result;
+            }
+
+            Ok(results)
+        }
+        Platform::Curseforge => Ok(crate::curseforge::CurseForgePackSearchResults::search(
+            options.clone(),
+        )
+        .await?
+        .to_modpack_results()),
+
+        Platform::Modrinth => Ok(crate::modrinth::ModrinthPackSearchResults::search(options)
+            .await?
+            .to_modpack_results()),
+        Platform::AtLauncher => Ok(crate::atlauncher::ATLauncherPackSearchResults::search()
+            .await?
+            .to_modpack_results()),
+    }
+}
