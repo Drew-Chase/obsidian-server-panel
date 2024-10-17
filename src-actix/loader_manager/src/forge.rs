faf18a1693a625fccc5fdfe0fb943d02061e0543
Drew Chase (Work) <dcmanproductions@gmail.com>
2024-10-17 16:35:30 -0400

    Add initial forge management functions
    This commit introduces the `versions`, `download`, and `install` functions in the `forge.rs` file. These functions are placeholders for managing different versions of forge in the `loader_manager` module. The actual implementation is marked as TODO.

---
+++ src-actix/loader_manager/src/forge.rs
@@ -0,0 +1,9 @@
+pub fn versions() -> Vec<String> {
+    todo!("return all versions of forge")
+}
+pub fn download(version: &str) -> Result<(), Box<dyn std::error::Error>> {
+    todo!("download forge version {}", version)
+}
+pub fn install(version: &str) -> Result<(), Box<dyn std::error::Error>> {
+	todo!("install forge version {}", version)
+}
