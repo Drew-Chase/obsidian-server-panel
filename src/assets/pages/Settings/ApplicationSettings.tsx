e3d06ec3b282dd6bd3ad15f9e173fdb0f794ce97
Drew (Linux) <dcmanproductions@gmail.com>
2024-10-19 16:29:03 -0400

    Add Java settings section to Application Settings
    This commit introduces a new Java settings section to the Application Settings page by importing and incorporating the `JavaSettings` component. Additionally, the existing layout's structure has been adjusted to accommodate the new section, enhancing the overall settings organization.

--- src/assets/pages/Settings/ApplicationSettings.tsx
+++ src/assets/pages/Settings/ApplicationSettings.tsx
@@ -1,11 +1,15 @@
 import {setTitle} from "../../../main.tsx";
+import JavaSettings from "../../components/Settings/JavaSettings.tsx";
 
 export default function ApplicationSettings()
 {
     setTitle("Application Settings");
     return (
-        <div className={"flex flex-col bg-neutral-600 rounded-3xl shadow-lg p-8 w-full mx-2 overflow-y-auto"}>
-            <p className={"text-lg font-semibold mr-auto mb-8"}>Application Settings</p>
+        <div className={"flex flex-col gap-4"}>
+            <div className={"flex flex-col bg-neutral-600 rounded-3xl shadow-lg p-8 w-full mx-2 overflow-y-auto"}>
+                <p className={"text-lg font-semibold mr-auto mb-8"}>Application Settings</p>
+            </div>
+            <JavaSettings/>
         </div>
     );
 }
