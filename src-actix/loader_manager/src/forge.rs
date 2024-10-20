44428ae8fd898e88f1181db5363d8c6915ac7df3
Drew (Linux) <dcmanproductions@gmail.com>
2024-10-19 21:42:24 -0400

    Refactor install function to be async with added parameters
    Changed the `install` function to asynchronous and added a directory parameter. This allows for non-blocking execution and better flexibility in installation paths.

--- src-actix/loader_manager/src/forge.rs
+++ src-actix/loader_manager/src/forge.rs
@@ -1,9 +1,11 @@
+use std::path::Path;
+
 pub fn versions() -> Vec<String> {
     todo!("return all versions of forge")
 }
 pub fn download(version: &str) -> Result<(), Box<dyn std::error::Error>> {
     todo!("download forge version {}", version)
 }
-pub fn install(version: &str) -> Result<(), Box<dyn std::error::Error>> {
-	todo!("install forge version {}", version)
+pub async fn install(mc_version: &str, dir: impl AsRef<Path>) -> Result<String, Box<dyn std::error::Error>> {
+	todo!("install forge version {}", mc_version)
 }
