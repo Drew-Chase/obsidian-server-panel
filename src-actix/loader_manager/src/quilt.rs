03dd738827daa19b366bd8d62152679a216aa799
Drew (Linux) <dcmanproductions@gmail.com>
2024-10-19 21:42:50 -0400

    Refactor `install` function signature in quilt.rs
    Update `install` to be asynchronous and accept a directory path. This enhances flexibility and prepares for future implementation of installation logic.

--- src-actix/loader_manager/src/quilt.rs
+++ src-actix/loader_manager/src/quilt.rs
@@ -1,9 +1,11 @@
+use std::path::Path;
+
 pub fn versions() -> Vec<String> {
 	todo!("return all versions of quilt")
 }
 pub fn download(version: &str) -> Result<(), Box<dyn std::error::Error>> {
 	todo!("download quilt version {}", version)
 }
-pub fn install(version: &str) -> Result<(), Box<dyn std::error::Error>> {
-	todo!("install quilt version {}", version)
+pub async fn install(mc_version: &str, dir: impl AsRef<Path>) -> Result<String, Box<dyn std::error::Error>> {
+	todo!("install quilt version {}", mc_version)
 }
