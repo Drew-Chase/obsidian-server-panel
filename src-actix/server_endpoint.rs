55947d884ff8ec0bf678afc084e4de48b5ea9693
Drew (Linux) <dcmanproductions@gmail.com>
2024-10-19 21:43:29 -0400

    Refactor server handling endpoints with improved comments
    Enhanced readability by adding detailed comments to explain each step in the server handling endpoints. This includes authentication checks, server retrieval, creation, and loader management processes ensuring clarity for future maintenance and updates.

--- src-actix/server_endpoint.rs
+++ src-actix/server_endpoint.rs
@@ -10,9 +10,12 @@ use servers::server_db;
 use servers::server_db::{BasicHashedServer, HashedServer, Server};
 use std::path::Path;
 
+/// Retrieves servers owned by the authenticated user
 #[get("")]
 pub async fn get_servers(req: HttpRequest) -> impl Responder {
+    // Check if a user is authenticated from the request extensions
     if let Some(user) = req.extensions().get::<User>() {
+        // Fetch servers by the user's ID
         let servers = match server_db::get_servers_by_owner(user.id) {
             Ok(s) => s,
             Err(e) => {
@@ -21,25 +24,32 @@ pub async fn get_servers(req: HttpRequest) -> impl Responder {
             }
         };
 
+        // Convert the servers to BasicHashedServer format for response
         let servers: Vec<BasicHashedServer> = servers
             .iter()
             .map(|s| BasicHashedServer::from_server(s.clone()))
             .collect();
 
+        // Return the list of servers as JSON response
         return HttpResponse::Ok().json(servers);
     }
 
+    // Return Unauthorized if the user is not authenticated
     HttpResponse::Unauthorized().json(json!({"error":"Unauthorized"}))
 }
 
+/// Retrieves a specific server by its ID, ensuring the server is owned by the authenticated user
 #[get("")]
 pub async fn get_server_by_id(id: web::Path<String>, req: HttpRequest) -> impl Responder {
+    // Check if a user is authenticated from the request extensions
     if let Some(user) = req.extensions().get::<User>() {
+        // Decode the given ID
         let id_number: u32 = match decode(id.as_str()) {
             Ok(id_number) => id_number[0] as u32,
             Err(_) => return HttpResponse::BadRequest().json(json!({"error":"Invalid ID"})),
         };
 
+        // Fetch the server by the ID and user's ID
         let server = match server_db::get_owned_server_by_id(id_number, user.id) {
             Some(s) => s,
             None => {
@@ -48,9 +58,11 @@ pub async fn get_server_by_id(id: web::Path<String>, req: HttpRequest) -> impl R
                 return HttpResponse::BadRequest().json(json!({"error":msg}));
             }
         };
+        // Return the server details as JSON response
         return HttpResponse::Ok().json(HashedServer::from_server(server));
     }
 
+    // Return Unauthorized if the user is not authenticated
     HttpResponse::Unauthorized().json(json!({"error":"Unauthorized"}))
 }
 
@@ -66,12 +78,16 @@ struct CreateServerRequest {
     loader: u8,
     loader_version: Option<String>,
 }
+
+/// Creates a new server for the authenticated user
 #[post("")]
 pub async fn create_server(
     req: HttpRequest,
     body: web::Json<CreateServerRequest>,
 ) -> impl Responder {
+    // Check if a user is authenticated from the request extensions
     if let Some(user) = req.extensions().get::<User>() {
+        // Create a new server in the database
         let server: Server = match server_db::create_server(body.name.as_str(), user.id) {
             Ok(s) => s,
             Err(e) => {
@@ -80,6 +96,7 @@ pub async fn create_server(
             }
         };
 
+        // Create a directory for the server
         let dir = match create_server_directory(server.id) {
             Ok(d) => d,
             Err(e) => {
@@ -88,6 +105,7 @@ pub async fn create_server(
             }
         };
 
+        // Set the server's directory in the database
         match server_db::set_server_directory(server.id, dir.to_str().unwrap()) {
             Ok(_) => (),
             Err(e) => {
@@ -96,48 +114,42 @@ pub async fn create_server(
             }
         }
 
+        // Set the Minecraft version for the server
         if let Err(e) = server_db::set_minecraft_version(server.id, &body.minecraft_version) {
             error!("{}", e);
             return HttpResponse::BadRequest().json(json!({"error":e}));
         }
+
+        // Set the server's loader if provided
         if let Some(loader_version) = &body.loader_version {
             if let Err(e) = server_db::set_loader(server.id, body.loader, loader_version.as_str()) {
                 error!("{}", e);
                 return HttpResponse::BadRequest().json(json!({"error":e}));
             }
         }
-        match body.loader {
-            0 => {
-                match minecraft::minecraft_version::download_server_jar(
-                    &body.minecraft_version,
-                    &dir,
-                )
-                .await
-                {
-                    Ok(_) => {
-                        if let Err(e) = server_db::set_server_executable(server.id, "server.jar") {
-                            error!("{}", e);
-                            return HttpResponse::BadRequest().json(json!({"error":e}));
-                        }
-                    }
-                    Err(e) => {
-                        error!("{}", e);
-                        return HttpResponse::BadRequest().json(
-                            json!({"error":format!("Failed to download the server jar: {}", e)}),
-                        );
-                    }
-                }
-            },
-            1=>todo!("Implement the Fabric loader"),
-            2=>todo!("Implement the Forge loader"),
-            3=>todo!("Implement the Spigot loader"),
-            4=>todo!("Implement the Paper loader"),
-            5=>todo!("Implement the BungeeCord loader"),
-            6=>todo!("Implement the Quilt loader"),
-            7=>todo!("Implement the Mohist loader"),
-            _ => (),
+
+        // Install the server loader if it's not VANILLA
+        let loader = loader_manager::from_u8(body.loader)
+            .ok_or("Invalid loader")
+            .unwrap();
+        let loader_version = body.loader_version.clone();
+        if loader != loader_manager::Loaders::VANILLA {
+            let executable = loader_manager::install_loader(
+                loader,
+                &body.minecraft_version,
+                &dir,
+                loader_version,
+            )
+            .await
+            .unwrap();
+            // Set the server executable in the database
+            if let Err(e) = server_db::set_server_executable(server.id, &executable) {
+                error!("{}", e);
+                return HttpResponse::BadRequest().json(json!({"error":e}));
+            }
         }
 
+        // Create and set server properties
         let mut properties: Properties =
             match Properties::new(&Path::join(&dir, Path::new("server.properties"))) {
                 Ok(p) => p,
@@ -153,6 +165,7 @@ pub async fn create_server(
         properties.set("hardcore", &body.hardcore.to_string());
         properties.set("max-players", &body.max_players.to_string());
 
+        // Save the properties to the file
         match properties.write() {
             Ok(_) => (),
             Err(e) => {
@@ -162,14 +175,72 @@ pub async fn create_server(
             }
         }
 
+        // Return the created server details as JSON response
         return HttpResponse::Ok().json(HashedServer::from_server(server));
     }
 
+    // Return Unauthorized if the user is not authenticated
     HttpResponse::Unauthorized().json(json!({"error":"Unauthorized"}))
 }
 
-
+/// Retrieves all supported server loaders
 #[get("/supported_loaders")]
-pub async fn get_supported_loaders()->impl Responder{
+pub async fn get_supported_loaders() -> impl Responder {
+    // Return the list of all supported loaders as JSON response
     HttpResponse::Ok().json(servers::supported_loaders::SupportedLoaders::all())
+}
+
+/// Installs a specified loader for a server, ensuring the server is owned by the authenticated user
+#[post("/install_loader/{version}/{loader}/{loader_version}")]
+pub async fn install_loader(
+    id: web::Path<String>,
+    version: web::Path<String>,
+    loader: web::Path<u8>,
+    loader_version: web::Path<String>,
+    req: HttpRequest,
+) -> impl Responder {
+    // Check if a user is authenticated from the request extensions
+    if let Some(user) = req.extensions().get::<User>() {
+        // Decode the given ID
+        let id_number: u32 = match decode(id.as_str()) {
+            Ok(id_number) => id_number[0] as u32,
+            Err(_) => return HttpResponse::BadRequest().json(json!({"error":"Invalid ID"})),
+        };
+
+        // Fetch the server by the ID and user's ID
+        let server = match server_db::get_owned_server_by_id(id_number, user.id) {
+            Some(s) => s,
+            None => {
+                let msg = format!("Server with id: {} not found", id_number);
+                error!("{}", msg);
+                return HttpResponse::BadRequest().json(json!({"error":msg}));
+            }
+        };
+
+        // Install the specified loader if it's not VANILLA
+        let loader = loader_manager::from_u8(*loader)
+            .ok_or("Invalid loader")
+            .unwrap();
+        if loader != loader_manager::Loaders::VANILLA {
+            let executable = loader_manager::install_loader(
+                loader,
+                &version,
+                &server.clone().directory.unwrap(),
+                Some(loader_version.as_ref()),
+            )
+            .await
+            .unwrap();
+            // Set the server executable in the database
+            if let Err(e) = server_db::set_server_executable(id_number, &executable) {
+                error!("{}", e);
+                return HttpResponse::BadRequest().json(json!({"error":e}));
+            }
+        }
+
+        // Return the updated server details as JSON response
+        return HttpResponse::Ok().json(HashedServer::from_server(server));
+    }
+
+    // Return a bad request if there's an unexpected error
+    HttpResponse::BadRequest().json(json!({"message":"Unexpected error has occurred"}))
 }
