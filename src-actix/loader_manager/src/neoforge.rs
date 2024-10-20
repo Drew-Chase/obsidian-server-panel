56a44ff565915fbc9394d1e481052b891916030a
Drew (Linux) <dcmanproductions@gmail.com>
2024-10-19 21:42:41 -0400

    Refactor `install` function to be async and accept directory
    The `install` function now accepts a `mc_version` and a directory path as arguments. It has been updated to be asynchronous (`async`) to handle potentially long-running installation processes more effectively. Also, added the `std::path::Path` import to manage the directory path parameter.

--- src-actix/loader_manager/src/neoforge.rs
+++ src-actix/loader_manager/src/neoforge.rs
@@ -1,9 +1,11 @@
+use std::path::Path;
+
 pub fn versions() -> Vec<String> {
 	todo!("return all versions of neoforge")
 }
 pub fn download(version: &str) -> Result<(), Box<dyn std::error::Error>> {
 	todo!("download neoforge version {}", version)
 }
-pub fn install(version: &str) -> Result<(), Box<dyn std::error::Error>> {
-	todo!("install neoforge version {}", version)
+pub async fn install(mc_version: &str, dir: impl AsRef<Path>) -> Result<String, Box<dyn std::error::Error>> {
+	todo!("install neoforge version {}", mc_version)
 }
