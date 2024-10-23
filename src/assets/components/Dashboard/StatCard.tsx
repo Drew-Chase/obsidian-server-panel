647ce0bc1edc08694a85fa3477635a2ca521c9e5
Drew Chase (Work) <dcmanproductions@gmail.com>
2024-10-23 10:43:51 -0400

    Enhance StatCard component responsiveness
    Updated the StatCard container to improve its responsiveness by adjusting its minimum width and flexibility. This change ensures better adaptability across different screen sizes.

--- src/assets/components/Dashboard/StatCard.tsx
+++ src/assets/components/Dashboard/StatCard.tsx
@@ -14,7 +14,7 @@ interface StatCardProps
 export default function StatCard(props: StatCardProps)
 {
     return (
-        <div className={"flex flex-col bg-neutral-600 rounded-2xl shadow-lg p-8 max-w-md w-full mx-2"}>
+        <div className={"flex flex-col bg-neutral-600 rounded-2xl shadow-lg p-8 max-w-md min-w-sm grow shrink"}>
             <div className={"flex flex-row w-full items-center mb-3"}>
                 <span className={"text-primary"}>{props.icon}</span>
                 <p className={"text-tiny font-medium ml-2 mr-auto"}>{props.title}</p>
