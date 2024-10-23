e055efe342a8281cd1b772d6a10fe8edda6930d4
Drew Chase (Work) <dcmanproductions@gmail.com>
2024-10-23 16:27:05 -0400

    Enhance LoaderSettings with dynamic version fetching
    Refactor LoaderSettings component to dynamically fetch and display versions for different loaders based on the selected Minecraft version. Introduce state management for loader type and conditional disabling of tabs based on version constraints.

--- src/assets/components/Dashboard/CreateServer/LoaderSettings.tsx
+++ src/assets/components/Dashboard/CreateServer/LoaderSettings.tsx
@@ -1,37 +1,76 @@
 import {AutocompleteItem, Button, Tab, Tabs, Tooltip} from "@nextui-org/react";
 import OAutocomplete from "../../Extends/OAutocomplete.tsx";
+import {useEffect, useState} from "react";
+import {getFabricVersions, getForgeVersions} from "../../../ts/loaders.ts";
 
-export default function LoaderSettings()
+export default function LoaderSettings({minecraft_version, snapshots}: { minecraft_version: string, snapshots: boolean })
 {
+
+    const [versions, setVersions] = useState<string[]>([]);
+    const [loader, setLoader] = useState<string>("Vanilla");
+    const [forgeOnly, setForgeOnly] = useState<boolean>(false);
+    useEffect(() =>
+    {
+        switch (loader)
+        {
+            case "Vanilla":
+                break;
+            case "Fabric":
+                getFabricVersions().then(setVersions);
+                break;
+            case "Forge":
+                getForgeVersions(minecraft_version).then(setVersions);
+                break;
+            case "NeoForge":
+                break;
+            case "Quilt":
+                break;
+        }
+        if (minecraft_version !== "")
+        {
+            const parts: number[] = minecraft_version.split(".").map((part) => parseInt(part));
+            setForgeOnly(parts.length >= 2 && parts[1] <= 14);
+        } else
+        {
+            setForgeOnly(false);
+        }
+
+    }, [minecraft_version, loader]);
+
+
     return (
-        <>
+        <div>
             <p>Select Loader</p>
-            <Tabs>
+            <Tabs onSelectionChange={key => setLoader(key as string)} isDisabled={minecraft_version == ""}>
                 <Tab title={"Vanilla"}></Tab>
-                <Tab title={"Fabric"}>
+                <Tab title={"Fabric"} key={"Fabric"} isDisabled={forgeOnly}>
                     <div className={"flex flex-row w-full gap-4 items-center"}>
                         <OAutocomplete
                             label={"Fabric Loader Version"}
                             placeholder={"Select a Fabric Loader version"}
                             className={"w-full"}
                         >
-                            <AutocompleteItem key={"1.20.4"}>1.20.4</AutocompleteItem>
+                            {versions.map((version) => (
+                                <AutocompleteItem key={version}>{version}</AutocompleteItem>
+                            ))}
                         </OAutocomplete>
                         <Tooltip content={"This is required for most fabric mods"}>
                             <Button>Install Fabric API</Button>
                         </Tooltip>
                     </div>
                 </Tab>
-                <Tab title={"Forge"}>
+                <Tab title={"Forge"} key={"Forge"} isDisabled={snapshots}>
                     <OAutocomplete
                         label={"Forge Version"}
                         placeholder={"Select a Forge version"}
                         className={"w-full"}
                     >
-                        <AutocompleteItem key={"1.20.4"}>1.20.4</AutocompleteItem>
+                        {versions.map((version) => (
+                            <AutocompleteItem key={version}>{version}</AutocompleteItem>
+                        ))}
                     </OAutocomplete>
                 </Tab>
-                <Tab title={"NeoForge"}>
+                <Tab title={"NeoForge"} key={"NeoForge"} isDisabled={snapshots || forgeOnly}>
                     <OAutocomplete
                         label={"NeoForge Version"}
                         placeholder={"Select a NeoForge version"}
@@ -40,7 +79,7 @@ export default function LoaderSettings()
                         <AutocompleteItem key={"1.20.4"}>1.20.4</AutocompleteItem>
                     </OAutocomplete>
                 </Tab>
-                <Tab title={"Quilt"}>
+                <Tab title={"Quilt"} key={"Quilt"} isDisabled={forgeOnly}>
                     <OAutocomplete
                         label={"Quilt Version"}
                         placeholder={"Select a Quilt version"}
@@ -50,6 +89,6 @@ export default function LoaderSettings()
                     </OAutocomplete>
                 </Tab>
             </Tabs>
-        </>
+        </div>
     );
 }
