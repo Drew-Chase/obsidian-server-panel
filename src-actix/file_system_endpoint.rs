88e0437524e9792a17b4931c5186e52a897552b4
Drew Chase (Work) <dcmanproductions@gmail.com>
2024-10-23 16:27:42 -0400

    Add file upload endpoint to file_system_endpoint.rs
    Introduced new structs for handling file metadata and form data using actix-multipart and serde. Added an endpoint `/upload` to handle file uploads with server validation and user authentication.

--- src-actix/file_system_endpoint.rs
+++ src-actix/file_system_endpoint.rs
@@ -1,7 +1,9 @@
+use actix_multipart::form::{json::Json as MPJson, tempfile::TempFile, MultipartForm};
 use actix_web::{post, web, HttpMessage, HttpRequest, HttpResponse, Responder};
 use authentication::data::User;
 use crypto::hashids::decode;
 use log::error;
+use serde::Deserialize;
 use serde_json::json;
 use servers::physical_server;
 
@@ -29,6 +31,50 @@ pub async fn get_server_files(
             user.id,
             body,
         ));
+    }
+
+    HttpResponse::Unauthorized().json(json!({"error": "Unauthorized"}))
+}
+
+#[derive(Debug, Deserialize)]
+struct UploadFileMetadata {
+    directory: String,
+    filename: String,
+}
+
+#[derive(Debug, MultipartForm)]
+struct UploadForm {
+    file: TempFile,
+    json: MPJson<UploadFileMetadata>,
+}
+
+#[post("/upload")]
+pub async fn upload_file_to_server(
+    id: web::Path<String>,
+    MultipartForm(mut payload): MultipartForm<UploadForm>,
+    req: HttpRequest,
+) -> impl Responder {
+    if let Some(user) = req.extensions().get::<User>() {
+        let id_number = match decode(&id) {
+            Ok(id) => id,
+            Err(_) => {
+                error!("Invalid id: {}", id);
+                return HttpResponse::BadRequest()
+                    .json(json!({"error": format!("Invalid id: {}", id)}));
+            }
+        };
+        if id_number.is_empty() {
+            return HttpResponse::BadRequest()
+                .json(json!({"error": format!("Invalid id: {}", id)}));
+        }
+        let id_number = id_number[0] as u32;
+
+        let server = servers::server_db::get_owned_server_by_id(id_number, user.id);
+        if let Some(server) = server {
+            
+        } else {
+            return HttpResponse::BadRequest().json(json!({"error": "Server not found"}));
+        }
     }
 
     HttpResponse::Unauthorized().json(json!({"error": "Unauthorized"}))
