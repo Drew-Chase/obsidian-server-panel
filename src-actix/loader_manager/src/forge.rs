a70dff2d28021859992038e534c362bef0d1912f
Drew Chase (Work) <dcmanproductions@gmail.com>
2024-10-23 16:27:23 -0400

    Add async versions function to fetch Forge versions
    Refactored the `versions` function to be asynchronous, fetching available Minecraft Forge versions from a remote JSON manifest. The new implementation filters the versions based on the provided Minecraft version and returns the matching Forge versions. This change enhances functionality by integrating real-time data fetching.

--- src-actix/loader_manager/src/forge.rs
+++ src-actix/loader_manager/src/forge.rs
@@ -1,11 +1,34 @@
+use std::error::Error;
 use std::path::Path;
 
-pub fn versions() -> Vec<String> {
-    todo!("return all versions of forge")
+const FORGE_VERSIONS_MANIFEST: &str =
+    "https://files.minecraftforge.net/net/minecraftforge/forge/maven-metadata.json";
+
+pub async fn versions(minecraft_version: impl AsRef<str>) -> Result<Vec<String>, Box<dyn Error>> {
+    let response = reqwest::get(FORGE_VERSIONS_MANIFEST).await?;
+    let json: serde_json::Value = response.json().await?;
+    let mc_versions: Vec<String> = json
+        .as_object()
+        .unwrap()
+        .keys()
+        .map(|s| s.to_string())
+        .collect::<Vec<String>>();
+
+    for mc_version in mc_versions {
+        if mc_version.contains(minecraft_version.as_ref()) {
+            let versions = json[&mc_version].as_array().unwrap();
+            return Ok(versions
+                .iter()
+                .map(|v| v.as_str().unwrap().to_string())
+                .collect::<Vec<String>>());
+        }
+    }
+
+    Ok(vec![])
 }
-pub fn download(version: &str) -> Result<(), Box<dyn std::error::Error>> {
+pub fn download(version: &str) -> Result<(), Box<dyn Error>> {
     todo!("download forge version {}", version)
 }
-pub async fn install(mc_version: &str, dir: impl AsRef<Path>) -> Result<String, Box<dyn std::error::Error>> {
-	todo!("install forge version {}", mc_version)
+pub async fn install(mc_version: &str, dir: impl AsRef<Path>) -> Result<String, Box<dyn Error>> {
+    todo!("install forge version {}", mc_version)
 }
