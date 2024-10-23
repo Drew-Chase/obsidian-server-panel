d205bbe74da8663145cd5743639019b16baee71e
Drew Chase (Work) <dcmanproductions@gmail.com>
2024-10-23 10:44:33 -0400

    Add modal for actions and responsive table adjustments
    Introduced a modal to handle actions when screen width is below 900px. Added responsive column hiding based on screen width for a better mobile experience. Enhanced table cell behavior to fit truncated content within smaller screens.

--- src/assets/components/Dashboard/InstancesList.tsx
+++ src/assets/components/Dashboard/InstancesList.tsx
@@ -1,12 +1,33 @@
-import {Button, Chip, Link, Table, TableBody, TableCell, TableColumn, TableHeader, TableRow, Tooltip} from "@nextui-org/react";
+import {Button, Chip, Link, Modal, ModalBody, ModalContent, ModalFooter, ModalHeader, Table, TableBody, TableCell, TableColumn, TableHeader, TableRow, Tooltip} from "@nextui-org/react";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {faAward, faBook, faCirclePlus, faCopy, faHardDrive, faLayerGroup, faPlus} from "@fortawesome/free-solid-svg-icons";
+import {faAward, faBook, faCirclePlus, faCopy, faEllipsis, faHardDrive, faLayerGroup, faPlus} from "@fortawesome/free-solid-svg-icons";
 import DownloadFile from "../../images/DownloadFile.svg.tsx";
+import {useScreenSize} from "../../providers/ScreenSizeProvider.tsx";
+import {useState} from "react";
 
 export default function InstancesList()
 {
+    const {width} = useScreenSize();
+    const [isActionModalOpen, setIsActionModalOpen] = useState(false);
     return (
         <div className={"flex flex-col bg-neutral-600 rounded-3xl shadow-lg p-8 w-full mx-2 h-[calc(100dvh_-_330px)] min-h-[300px] overflow-y-auto grow relative"}>
+            <Modal size={"2xl"} isOpen={isActionModalOpen} onClose={() => setIsActionModalOpen(false)}>
+                <ModalContent>
+                    {onClose => (<>
+                        <ModalHeader>Actions</ModalHeader>
+                        <ModalBody>
+                            <div className={"flex flex-col w-full gap-4"}>
+                                <Button className={"h-16 text-neutral-400 data-[hover]:text-foreground"} aria-label={"Create Server"}> Create Server </Button>
+                                <Button className={"h-16 data-[hover]:text-foreground text-neutral-400"} aria-label={"Duplicate Instance"}> Duplicate Instance </Button>
+                                <Button className={"h-16 text-neutral-400 data-[hover]:text-foreground"} aria-label={"Download Instance"}> Download Instance </Button>
+                            </div>
+                        </ModalBody>
+                        <ModalFooter>
+                            <Button onClick={onClose}>Cancel</Button>
+                        </ModalFooter>
+                    </>)}
+                </ModalContent>
+            </Modal>
             <div className={"flex flex-row w-full items-center"}>
                 <p className={"text-lg font-semibold"}>Instances</p>
 
@@ -34,17 +55,17 @@ export default function InstancesList()
                     thead: "bg-neutral-700/50 backdrop-blur-lg"
                 }}
                 checkboxesProps={{
-                    className: "w-0"
+                    className: "w-6"
                 }}
                 isStriped
                 removeWrapper
             >
                 <TableHeader>
                     <TableColumn>Name</TableColumn>
-                    <TableColumn><FontAwesomeIcon icon={faAward} className={"mr-1"}/> Loader</TableColumn>
-                    <TableColumn><FontAwesomeIcon icon={faBook} className={"mr-1"}/> Minecraft Version</TableColumn>
-                    <TableColumn><FontAwesomeIcon icon={faLayerGroup} className={"mr-1"}/> Source</TableColumn>
-                    <TableColumn><FontAwesomeIcon icon={faHardDrive} className={"mr-1"}/> Servers</TableColumn>
+                    <TableColumn hidden={width < 900}><FontAwesomeIcon icon={faAward} className={"mr-1"}/> Loader</TableColumn>
+                    <TableColumn hidden={width < 1020}><FontAwesomeIcon icon={faBook} className={"mr-1"}/> Minecraft Version</TableColumn>
+                    <TableColumn hidden={width < 900}><FontAwesomeIcon icon={faLayerGroup} className={"mr-1"}/> Source</TableColumn>
+                    <TableColumn hidden={width < 1120}><FontAwesomeIcon icon={faHardDrive} className={"mr-1"}/> Servers</TableColumn>
                     <TableColumn className={"w-0"}>Actions</TableColumn>
                 </TableHeader>
                 <TableBody>
@@ -52,23 +73,35 @@ export default function InstancesList()
                     {
                         return (
                             <TableRow key={index}>
-                                <TableCell>All The Mods 6</TableCell>
-                                <TableCell><Link>Forge</Link></TableCell>
-                                <TableCell><Link>1.20.4</Link></TableCell>
-                                <TableCell><Link>Modrinth</Link></TableCell>
-                                <TableCell><Chip color={"success"} variant={"flat"}>4</Chip></TableCell>
+                                <TableCell><span className={"max-w-[100px] truncate"}>All The Mods 6</span></TableCell>
+                                <TableCell hidden={width < 900}><Link>Forge</Link></TableCell>
+                                <TableCell hidden={width < 1020}><Link>1.20.4</Link></TableCell>
+                                <TableCell hidden={width < 900}><Link>Modrinth</Link></TableCell>
+                                <TableCell hidden={width < 1120}><Chip color={"success"} variant={"flat"}>4</Chip></TableCell>
                                 <TableCell>
-                                    <div className={"flex flex-row items-center"}>
-                                        <Tooltip content={"Create Server"}>
-                                            <Button variant={"light"} className={"min-w-0 w-2 text-neutral-400 data-[hover]:text-foreground"} aria-label={"Create Server"}> <FontAwesomeIcon icon={faCirclePlus}/> </Button>
-                                        </Tooltip>
-                                        <Tooltip content={`Duplicate Instance`}>
-                                            <Button variant={"light"} className={"min-w-0 w-2 data-[hover]:text-foreground text-neutral-400"} aria-label={"Duplicate Instance"}> <FontAwesomeIcon icon={faCopy}/> </Button>
-                                        </Tooltip>
-                                        <Tooltip content={"Download Instance"}>
-                                            <Button variant={"light"} className={"min-w-0 w-2 text-neutral-400 data-[hover]:text-foreground"} aria-label={"Download Instance"}> <DownloadFile/> </Button>
-                                        </Tooltip>
-                                    </div>
+                                    {(width < 900) ?
+                                        (
+                                            <Button
+                                                radius={"full"}
+                                                className={"min-w-0 w-11 h-11 text-neutral-400 data-[hover]:text-foreground"}
+                                                aria-label={"Open Actions"}
+                                                onClick={() => setIsActionModalOpen(true)}
+                                            >
+                                                <FontAwesomeIcon icon={faEllipsis}/>
+                                            </Button>
+                                        ) : (
+                                            <div className={"flex flex-row items-center"}>
+                                                <Tooltip content={"Create Server"}>
+                                                    <Button variant={"light"} className={"min-w-0 w-2 text-neutral-400 data-[hover]:text-foreground"} aria-label={"Create Server"}> <FontAwesomeIcon icon={faCirclePlus}/> </Button>
+                                                </Tooltip>
+                                                <Tooltip content={`Duplicate Instance`}>
+                                                    <Button variant={"light"} className={"min-w-0 w-2 data-[hover]:text-foreground text-neutral-400"} aria-label={"Duplicate Instance"}> <FontAwesomeIcon icon={faCopy}/> </Button>
+                                                </Tooltip>
+                                                <Tooltip content={"Download Instance"}>
+                                                    <Button variant={"light"} className={"min-w-0 w-2 text-neutral-400 data-[hover]:text-foreground"} aria-label={"Download Instance"}> <DownloadFile/> </Button>
+                                                </Tooltip>
+                                            </div>
+                                        )}
                                 </TableCell>
                             </TableRow>
                         );
