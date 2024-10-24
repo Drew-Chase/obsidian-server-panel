1548378547e79275096eb41ab627964b14f420e5
Drew (Linux) <dcmanproductions@gmail.com>
2024-10-24 19:54:01 -0400

    Refactor loader enum management
    Consolidated loader enum definitions into a dedicated module, `supported_loaders`. This change simplifies loader enum usage and improves code maintainability by centralizing loader-related functionality.

--- src-actix/loader_manager/src/lib.rs
+++ src-actix/loader_manager/src/lib.rs
@@ -2,90 +2,10 @@ pub mod fabric;
 pub mod forge;
 pub mod neoforge;
 pub mod quilt;
+pub mod supported_loaders;
 
-use serde::{Deserialize, Serialize};
-use std::fmt::Display;
+use crate::supported_loaders::Loader;
 use std::path::Path;
-use std::str::FromStr;
-
-/// Represents the various loaders available in the system.
-#[derive(Debug, Clone, Copy, PartialEq, Deserialize, Serialize)]
-pub enum Loaders {
-    VANILLA,
-    FORGE,
-    FABRIC,
-    NEOFORGE,
-    QUILT,
-}
-
-impl Display for Loaders {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        match self {
-            Loaders::VANILLA => write!(f, "Vanilla"),
-            Loaders::FORGE => write!(f, "Forge"),
-            Loaders::FABRIC => write!(f, "Fabric"),
-            Loaders::NEOFORGE => write!(f, "NeoForge"),
-            Loaders::QUILT => write!(f, "Quilt"),
-        }
-    }
-}
-
-impl FromStr for Loaders {
-    type Err = ();
-
-    /// Parses a string into a Loaders enum.
-    ///
-    /// # Examples
-    ///
-    /// ```no-error
-    /// let loader: Loaders = "forge".parse().unwrap();
-    /// ```
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        match s.to_lowercase().as_str() {
-            "vanilla" => Ok(Loaders::VANILLA),
-            "forge" => Ok(Loaders::FORGE),
-            "fabric" => Ok(Loaders::FABRIC),
-            "neoforge" => Ok(Loaders::NEOFORGE),
-            "quilt" => Ok(Loaders::QUILT),
-            _ => Err(()),
-        }
-    }
-}
-
-pub trait FromU8 {
-    fn from_u8(value: u8) -> Option<Self>
-    where
-        Self: Sized;
-}
-
-impl FromU8 for Loaders {
-    fn from_u8(value: u8) -> Option<Self> {
-        match value {
-            0 => Some(Loaders::VANILLA),
-            1 => Some(Loaders::FORGE),
-            2 => Some(Loaders::FABRIC),
-            3 => Some(Loaders::NEOFORGE),
-            4 => Some(Loaders::QUILT),
-            _ => None,
-        }
-    }
-}
-
-pub trait AsU8 {
-    fn as_u8(&self) -> u8;
-}
-
-impl AsU8 for Loaders {
-    fn as_u8(&self) -> u8 {
-        match self {
-            Loaders::VANILLA => 0,
-            Loaders::FORGE => 1,
-            Loaders::FABRIC => 2,
-            Loaders::NEOFORGE => 3,
-            Loaders::QUILT => 4,
-        }
-    }
-}
 
 /// Installs the specified loader.
 ///
@@ -100,16 +20,16 @@ impl AsU8 for Loaders {
 ///
 /// A Result containing the installation result as a String or an error.
 pub async fn install_loader(
-    loader: Loaders,
+    loader: Loader,
     mc_version: &str,
     dir: impl AsRef<Path>,
     loader_version: Option<impl AsRef<str>>,
 ) -> Result<String, Box<dyn std::error::Error>> {
     match loader {
-        Loaders::FORGE => forge::install(mc_version, dir).await,
-        Loaders::FABRIC => fabric::install(mc_version, loader_version.unwrap().as_ref(), dir).await,
-        Loaders::NEOFORGE => neoforge::install(mc_version, dir).await,
-        Loaders::QUILT => quilt::install(mc_version, dir).await,
+        Loader::Forge => forge::install(mc_version, dir).await,
+        Loader::Fabric => fabric::install(mc_version, loader_version.unwrap().as_ref(), dir).await,
+        Loader::NeoForge => neoforge::install(mc_version, dir).await,
+        Loader::Quilt => quilt::install(mc_version, dir).await,
         _ => Err("Loader not supported".into()),
     }
 }
