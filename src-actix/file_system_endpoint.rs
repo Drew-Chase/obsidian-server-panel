859b894b3edebc4d6d019ac25a372437e236063f
Drew (Linux) <dcmanproductions@gmail.com>
2024-10-23 21:09:31 -0400

    Add file upload functionality to server endpoint
    Implemented logic to handle file uploads, including reading from a temporary file and writing to the server directory. Included error handling for file creation and read/write operations to ensure robustness. Added debug logs for better traceability of the file paths and server directory.

--- src-actix/file_system_endpoint.rs
+++ src-actix/file_system_endpoint.rs
@@ -2,10 +2,13 @@ use actix_multipart::form::{json::Json as MPJson, tempfile::TempFile, MultipartF
 use actix_web::{post, web, HttpMessage, HttpRequest, HttpResponse, Responder};
 use authentication::data::User;
 use crypto::hashids::decode;
-use log::error;
+use log::{debug, error, info};
 use serde::Deserialize;
 use serde_json::json;
 use servers::physical_server;
+use std::fs::File;
+use std::io::{Read, Write};
+use std::path::{Path, PathBuf};
 
 #[post("")]
 pub async fn get_server_files(
@@ -54,6 +57,8 @@ pub async fn upload_file_to_server(
     MultipartForm(mut payload): MultipartForm<UploadForm>,
     req: HttpRequest,
 ) -> impl Responder {
+    let directory = payload.json.directory.clone();
+    let filename = payload.json.filename.clone();
     if let Some(user) = req.extensions().get::<User>() {
         let id_number = match decode(&id) {
             Ok(id) => id,
@@ -71,10 +76,39 @@ pub async fn upload_file_to_server(
 
         let server = servers::server_db::get_owned_server_by_id(id_number, user.id);
         if let Some(server) = server {
-            
+            if let Some(server_dir) = server.directory {
+                let path = PathBuf::from(format!("{}{}{}", server_dir, directory, filename));
+                debug!("Uploading file to: {:?}", path);
+                debug!(
+                    "Server Directory: {:?}, Directory: {:?}, Filename: {:?}",
+                    server_dir, directory, filename
+                );
+
+                let mut file = match File::create(&path) {
+                    Ok(file) => file,
+                    Err(e) => {
+                        error!("Error creating file: {:?}", e);
+                        return HttpResponse::InternalServerError()
+                            .json(json!({"error": "Error creating file"}));
+                    }
+                };
+                let temp_file = payload.file.file.as_file_mut();
+                let mut temp_bytes: Vec<u8> = Vec::new();
+                if let Err(e) = temp_file.read_to_end(&mut temp_bytes) {
+                    error!("Error reading file: {:?}", e);
+                    return HttpResponse::InternalServerError()
+                        .json(json!({"error": "Error reading file"}));
+                }
+                if let Err(e) = file.write_all(&*temp_bytes) {
+                    error!("Error writing file: {:?}", e);
+                    return HttpResponse::InternalServerError()
+                        .json(json!({"error": "Error writing file"}));
+                }
+            }
         } else {
             return HttpResponse::BadRequest().json(json!({"error": "Server not found"}));
         }
+        return HttpResponse::Ok().json(json!({"success": "File uploaded"}));
     }
 
     HttpResponse::Unauthorized().json(json!({"error": "Unauthorized"}))
