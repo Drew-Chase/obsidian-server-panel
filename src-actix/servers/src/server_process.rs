4f6627fe3e331de2665d612603add0b96a2a3ec2
Drew (Linux) <dcmanproductions@gmail.com>
2024-10-24 19:55:37 -0400

    Add process stdio piping and async stdin writing
    Implemented `pipe_stdio_from_pid` for redirecting stdout to a callback and `write_to_stdin` for writing to stdin asynchronously. Enhanced process control by supporting non-blocking I/O operations for stdin and stdout. This allows better integration for async I/O processing with external processes.

--- src-actix/servers/src/server_process.rs
+++ src-actix/servers/src/server_process.rs
@@ -1,9 +1,16 @@
 use crate::server_db::Server;
 use lazy_static::lazy_static;
+use nix::fcntl::{fcntl, FcntlArg, OFlag};
+use nix::unistd::Pid;
 use serde_derive::{Deserialize, Serialize};
 use std::error::Error;
 use std::fmt::Display;
+use std::os::unix::io::FromRawFd;
 use std::str::FromStr;
+use std::sync::Arc;
+use tokio::fs::File;
+use tokio::io::{AsyncBufReadExt, AsyncWriteExt, BufReader};
+use tokio::sync::Mutex;
 
 #[derive(Debug, Serialize, Deserialize, Clone)]
 pub enum ServerStatus {
@@ -87,5 +94,63 @@ impl ServerProcess {
 
     pub fn kill_server(&mut self) -> Result<(), Box<dyn Error>> {
         todo!("Implement the kill_server function")
+    }
+
+    pub fn pipe_stdio_from_pid<F>(pid: u32, callback: F) -> Result<(), Box<dyn Error>>
+    where
+        F: Fn(String) + Send + Sync + 'static,
+    {
+        let pid = Pid::from_raw(pid as i32);
+
+        // Open the /proc/<pid>/fd/1 file for stdout of the process
+        let path = format!("./{}", pid);
+        let stdout_fd = nix::fcntl::open(
+            path.as_str(), // Convert `String` to `&str`
+            OFlag::O_RDONLY,
+            nix::sys::stat::Mode::empty(),
+        )?;
+
+        // Make the file descriptor non-blocking
+        fcntl(stdout_fd, FcntlArg::F_SETFL(OFlag::O_NONBLOCK))?;
+
+        let async_file = unsafe { File::from_raw_fd(stdout_fd) };
+        let reader = BufReader::new(async_file);
+
+        let callback = Arc::new(Mutex::new(callback));
+
+        tokio::spawn(async move {
+            let mut lines = reader.lines();
+            while let Ok(Some(line)) = lines.next_line().await {
+                let callback = Arc::clone(&callback);
+                let line = line.to_string();
+                tokio::spawn(async move {
+                    let callback = callback.lock().await;
+                    (callback)(line);
+                });
+            }
+        });
+
+        Ok(())
+    }
+    pub async fn write_to_stdin(pid: u32, data: String) -> Result<(), Box<dyn Error>> {
+        let pid = Pid::from_raw(pid as i32);
+
+        // Open the /proc/<pid>/fd/0 file for stdin of the process
+        let path = format!("./{}", pid);
+        let stdin_fd = nix::fcntl::open(
+            path.as_str(), // Convert `String` to `&str`
+            OFlag::O_WRONLY,
+            nix::sys::stat::Mode::empty(),
+        )?;
+
+        // Make the file descriptor non-blocking
+        fcntl(stdin_fd, FcntlArg::F_SETFL(OFlag::O_NONBLOCK))?;
+
+        let async_file = unsafe { File::from_raw_fd(stdin_fd) };
+        let mut writer = tokio::io::BufWriter::new(async_file);
+
+        writer.write_all(data.as_bytes()).await?;
+
+        Ok(())
     }
 }
