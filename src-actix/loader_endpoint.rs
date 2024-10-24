b13238cc2c062db29c71b64514706ed27d114e60
Drew (Linux) <dcmanproductions@gmail.com>
2024-10-24 19:52:56 -0400

    Refactor loader endpoint using unified Loader enum
    Unified the loader management by switching to a common Loader enum. Simplified code by removing redundant namespaces and adjusting function calls to the new Loader types. This change improves readability and maintainability of the loader endpoints.

--- src-actix/loader_endpoint.rs
+++ src-actix/loader_endpoint.rs
@@ -1,4 +1,5 @@
 use actix_web::{get, web, HttpResponse, Responder};
+use loader_manager::supported_loaders::Loader;
 use log::error;
 use serde_json::json;
 use std::str::FromStr;
@@ -7,13 +8,13 @@ use std::str::FromStr;
 #[get("/supported_loaders")]
 pub async fn get_supported_loaders() -> impl Responder {
     // Return the list of all supported loaders as JSON response
-    HttpResponse::Ok().json(servers::supported_loaders::SupportedLoaders::all())
+    HttpResponse::Ok().json(Loader::all())
 }
 #[get("/{loader_id}/{minecraft_version}")]
 pub async fn get_loader_by_id(params: web::Path<(String, String)>) -> impl Responder {
     let (loader_id, minecraft_version) = params.into_inner();
     // Fetch the loader by the given ID
-    let loader = match servers::supported_loaders::SupportedLoaders::from_str(loader_id.as_str()) {
+    let loader = match Loader::from_str(loader_id.as_str()) {
         Ok(loader) => loader,
         Err(e) => {
             let msg = format!("Loader with id: {} not found", loader_id);
@@ -22,20 +23,16 @@ pub async fn get_loader_by_id(params: web::Path<(String, String)>) -> impl Respo
         }
     };
     match loader {
-        servers::supported_loaders::SupportedLoaders::FABRIC => {
-            match loader_manager::fabric::versions().await {
-                Ok(versions) => HttpResponse::Ok().json(json!(versions)),
-                Err(_) => HttpResponse::InternalServerError()
-                    .json(json!({"message":"Failed to fetch versions"})),
-            }
-        }
-        servers::supported_loaders::SupportedLoaders::FORGE => {
-            match loader_manager::forge::versions(minecraft_version).await {
-                Ok(versions) => HttpResponse::Ok().json(json!(versions)),
-                Err(_) => HttpResponse::InternalServerError()
-                    .json(json!({"message":"Failed to fetch versions"})),
-            }
-        }
+        Loader::Fabric => match loader_manager::fabric::versions().await {
+            Ok(versions) => HttpResponse::Ok().json(json!(versions)),
+            Err(_) => HttpResponse::InternalServerError()
+                .json(json!({"message":"Failed to fetch versions"})),
+        },
+        Loader::Forge => match loader_manager::forge::versions(minecraft_version).await {
+            Ok(versions) => HttpResponse::Ok().json(json!(versions)),
+            Err(_) => HttpResponse::InternalServerError()
+                .json(json!({"message":"Failed to fetch versions"})),
+        },
 
         _ => HttpResponse::Ok().json(json!({"message":"Loader not supported"})),
     }
