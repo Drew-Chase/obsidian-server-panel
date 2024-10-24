37911eba3215d10ed288ccfb4081ff6240449bd2
Drew Chase (Work) <dcmanproductions@gmail.com>
2024-10-24 16:00:03 -0400

    Add OPopover component
    Implemented a new OPopover component that wraps the Popover component from "@nextui-org/react" and passes all props to it. This addition enhances reusability and simplifies Popover integration within the project.

---
+++ src/assets/components/Extends/OPopover.tsx
@@ -0,0 +1,11 @@
+import {Popover, PopoverProps} from "@nextui-org/react";
+
+export default function OPopover(props: PopoverProps)
+{
+    return (
+        <Popover
+            {...props}
+        >
+        </Popover>
+    );
+}
