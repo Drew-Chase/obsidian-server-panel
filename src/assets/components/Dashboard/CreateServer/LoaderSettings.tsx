a71e78b0c62cb97dde5e8c3ae96956c28539195b
Drew (Linux) <dcmanproductions@gmail.com>
2024-10-26 19:58:44 -0400

    Refactor loader settings component
    Simplify loader version retrieval and dynamic tab rendering. Replaced individual loader version fetch calls with a generic function and dynamically generate tabs based on supported loaders. This reduces code complexity and improves maintainability.

--- src/assets/components/Dashboard/CreateServer/LoaderSettings.tsx
+++ src/assets/components/Dashboard/CreateServer/LoaderSettings.tsx
@@ -1,44 +1,20 @@
 import {AutocompleteItem, Button, Tab, Tabs, Tooltip} from "@nextui-org/react";
 import OAutocomplete from "../../Extends/OAutocomplete.tsx";
 import {useEffect, useState} from "react";
-import {getFabricVersions, getForgeVersions} from "../../../ts/loaders.ts";
+import {getLoaderVersions, getSupportedLoaders} from "../../../ts/loaders.ts";
 
 
 export default function LoaderSettings({minecraft_version, snapshots, onLoaderChange}: { minecraft_version: string, snapshots: boolean, onLoaderChange: (loader: string, version: string) => void })
 {
-
     const [versions, setVersions] = useState<string[]>([]);
     const [loader, setLoader] = useState<string>("Vanilla");
     const [loaderVersion, setLoaderVersion] = useState<string>("");
-    const [forgeOnly, setForgeOnly] = useState<boolean>(false);
+    const [supportedLoaders, setSupportedLoaders] = useState<string[]>([]);
     useEffect(() =>
     {
-        switch (loader)
-        {
-            case "Vanilla":
-                setVersions([]);
-                setLoaderVersion("");
-                break;
-            case "Fabric":
-                getFabricVersions().then(setVersions);
-                break;
-            case "Forge":
-                getForgeVersions(minecraft_version).then(setVersions);
-                break;
-            case "NeoForge":
-                break;
-            case "Quilt":
-                break;
-        }
-        if (minecraft_version !== "")
-        {
-            const parts: number[] = minecraft_version.split(".").map((part) => parseInt(part));
-            setForgeOnly(parts.length >= 2 && parts[1] <= 14);
-        } else
-        {
-            setForgeOnly(false);
-        }
-
+        getSupportedLoaders(minecraft_version || "all", snapshots).then(setSupportedLoaders);
+        if (loader && minecraft_version)
+            getLoaderVersions(loader, minecraft_version).then(setVersions);
     }, [minecraft_version, loader]);
 
     useEffect(() =>
@@ -46,61 +22,32 @@ export default function LoaderSettings({minecraft_version, snapshots, onLoaderCh
         onLoaderChange(loader, loaderVersion);
     }, [loader, loaderVersion]);
 
-
     return (
         <div>
             <p>Select Loader</p>
             <Tabs onSelectionChange={key => setLoader(key as string)} isDisabled={minecraft_version == ""}>
-                <Tab title={"Vanilla"} key={"Vanilla"}></Tab>
-                <Tab title={"Fabric"} key={"Fabric"} isDisabled={forgeOnly}>
-                    <div className={"flex flex-row w-full gap-4 items-center"}>
-                        <OAutocomplete
-                            label={"Fabric Loader Version"}
-                            placeholder={"Select a Fabric Loader version"}
-                            className={"w-full"}
-                            onSelectionChange={(key) => setLoaderVersion(key as string)}
-                        >
-                            {versions.map((version) => (
-                                <AutocompleteItem key={version}>{version}</AutocompleteItem>
-                            ))}
-                        </OAutocomplete>
-                        <Tooltip content={"This is required for most fabric mods"}>
-                            <Button>Install Fabric API</Button>
-                        </Tooltip>
-                    </div>
-                </Tab>
-                <Tab title={"Forge"} key={"Forge"} isDisabled={snapshots}>
-                    <OAutocomplete
-                        label={"Forge Version"}
-                        placeholder={"Select a Forge version"}
-                        className={"w-full"}
-                        onSelectionChange={(key) => setLoaderVersion(key as string)}
-                    >
-                        {versions.map((version) => (
-                            <AutocompleteItem key={version}>{version}</AutocompleteItem>
-                        ))}
-                    </OAutocomplete>
-                </Tab>
-                <Tab title={"NeoForge"} key={"NeoForge"} isDisabled={snapshots || forgeOnly}>
-                    <OAutocomplete
-                        label={"NeoForge Version"}
-                        placeholder={"Select a NeoForge version"}
-                        className={"w-full"}
-                        onSelectionChange={(key) => setLoaderVersion(key as string)}
-                    >
-                        <AutocompleteItem key={"1.20.4"}>1.20.4</AutocompleteItem>
-                    </OAutocomplete>
-                </Tab>
-                <Tab title={"Quilt"} key={"Quilt"} isDisabled={forgeOnly}>
-                    <OAutocomplete
-                        label={"Quilt Version"}
-                        placeholder={"Select a Quilt version"}
-                        className={"w-full"}
-                        onSelectionChange={(key) => setLoaderVersion(key as string)}
-                    >
-                        <AutocompleteItem key={"1.20.4"}>1.20.4</AutocompleteItem>
-                    </OAutocomplete>
-                </Tab>
+                {supportedLoaders.map((supportedLoader) => (
+                    <Tab key={supportedLoader} value={supportedLoader} title={supportedLoader}>
+                        <div className={"flex flex-row w-full gap-4 items-center"} hidden={supportedLoader === "Vanilla"}>
+                            <OAutocomplete
+                                label={`${supportedLoader} Loader Version`}
+                                placeholder={`Select a ${supportedLoader} Loader version`}
+                                className={"w-full"}
+                                onSelectionChange={(key) => setLoaderVersion(key as string)}
+                            >
+                                {versions.map((version) => (
+                                    <AutocompleteItem key={version}>{version}</AutocompleteItem>
+                                ))}
+                            </OAutocomplete>
+                            {supportedLoader === "Fabric" &&
+                                <Tooltip content={"This is required for most fabric mods"}>
+                                    <Button>Install Fabric API</Button>
+                                </Tooltip>
+                            }
+                        </div>
+                    </Tab>
+
+                ))}
             </Tabs>
         </div>
     );
