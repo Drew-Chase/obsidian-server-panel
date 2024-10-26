6339c50be765cc17d604d182c31e9f2560510783
Drew (Linux) <dcmanproductions@gmail.com>
2024-10-26 19:57:26 -0400

    Refactor loader functions for dynamic version fetching
    Updated existing loader functions to be more dynamic by accepting loader type and Minecraft version as parameters. Removed redundant functions and introduced a new method to get supported loaders, incorporating snapshot support.

--- src/assets/ts/loaders.ts
+++ src/assets/ts/loaders.ts
@@ -1,10 +1,10 @@
 import $ from "jquery";
 
-export async function getFabricVersions(): Promise<string[]>
+export async function getLoaderVersions(loader: string, minecraft_version:string): Promise<string[]>
 {
-    return $.get("/api/loaders/FABRIC/0");
+    return $.get(`/api/loaders/${loader}/${minecraft_version}`);
 }
-export async function getForgeVersions(minecraft_version: String): Promise<string[]>
-{
-    return $.get(`/api/loaders/FORGE/${minecraft_version}`);
+
+export async function getSupportedLoaders(minecraft_version: string, is_snapshot:boolean): Promise<string[]>{
+    return $.get(`/api/loaders/supported_loaders/${minecraft_version}?snapshot=${is_snapshot}`);
 }
