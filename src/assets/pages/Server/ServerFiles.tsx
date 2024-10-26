15b04d2506696827be904d3b2860df23e310f0ab
Drew (Linux) <dcmanproductions@gmail.com>
2024-10-26 19:57:58 -0400

    Refactor ServerFiles UI and functionality.
    Replaced static file table with dynamic ServerFilesList component. Added breadcrumb navigation, file upload, create directory and file action features with tooltips. Integrated file fetching from the server based on navigation path.

--- src/assets/pages/Server/ServerFiles.tsx
+++ src/assets/pages/Server/ServerFiles.tsx
@@ -1,66 +1,88 @@
-import {Button, Chip, Table, TableBody, TableCell, TableColumn, TableHeader, TableRow} from "@nextui-org/react";
-import DownloadFile from "../../images/DownloadFile.svg.tsx";
+import {Badge, BreadcrumbItem, Breadcrumbs, Button, Divider, Tooltip} from "@nextui-org/react";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {faTrash} from "@fortawesome/free-solid-svg-icons";
+import {faFileArchive, faFileDownload, faFolderPlus, faTrashAlt, faUpload} from "@fortawesome/free-solid-svg-icons";
 import {setTitle} from "../../../main.tsx";
+import {useEffect, useState} from "react";
+import FileSystem, {FileItem} from "../../ts/file-system.ts";
+import {useSelectedServer} from "../../providers/SelectedServerProvider.tsx";
+import ServerFilesList from "../../components/Server/Files/ServerFilesList.tsx";
 
 export default function ServerFiles()
 {
+    const [files, setFiles] = useState<FileItem[]>([]);
+    const [path, setPath] = useState<string>("");
+    const {server} = useSelectedServer();
+    const [loading, setLoading] = useState(false);
+    const [selectedItems, setSelectedItems] = useState<FileItem[]>([]);
     setTitle("Server Files");
+
+    useEffect(() =>
+    {
+        setLoading(true);
+        if (server == null) return;
+        new FileSystem(server.id)
+            .files(path)
+            .then(setFiles)
+            .finally(() => setLoading(false));
+    }, [path, server]);
+
+    if (server == null) return null;
     return (
         <div className={"flex flex-col bg-neutral-600 rounded-2xl shadow-lg p-8 grow w-full mx-2 gap-4 max-h-[calc(100dvh_-_60px)]"}>
-            <div className={"flex flex-row"}>
-                <p className={"text-xl font-semibold mr-auto"}>Server Files</p>
-            </div>
-            <Table
-                isStriped
-                removeWrapper
-                isHeaderSticky
-                className={"h-full overflow-y-auto"}
-                color={"primary"}
-                classNames={{
-                    tr: "data-[odd]:bg-neutral-800 data-[hover]:bg-neutral-700",
-                    th: "bg-neutral-700/50 backdrop-blur-lg",
-                    thead: "bg-neutral-700/50 backdrop-blur-lg"
-                }}
-                checkboxesProps={{
-                    className: "w-10"
-                }}
-                selectionMode={"multiple"}
-                aria-label="Server Files Table"
-            >
-                <TableHeader>
-                    <TableColumn className={"w-full"}>Filename</TableColumn>
-                    <TableColumn>Size</TableColumn>
-                    <TableColumn>Type</TableColumn>
-                    <TableColumn>Actions</TableColumn>
-                </TableHeader>
-
-                <TableBody>
-                    {Array.from({length: 20}, () => (
-                        <TableRow>
-                            <TableCell>File.txt</TableCell>
-                            <TableCell>
-                                <div className={"flex flex-row min-w-[100px]"}>
-                                    <Chip variant={"flat"} color={"default"}>1.25KB</Chip>
-                                </div>
-                            </TableCell>
-                            <TableCell>
-                                <div className={"flex flex-row min-w-[100px]"}>
-                                    <Chip variant={"flat"} color={"default"}>File</Chip>
-                                </div>
-                            </TableCell>
-                            <TableCell>
-                                <div className={"flex flex-row"}>
-                                    <Button className={"min-w-0"} variant={"light"} aria-label="Download File"><DownloadFile/></Button>
-                                    <Button className={"min-w-0"} variant={"light"} color={"danger"} aria-label="Delete File"><FontAwesomeIcon icon={faTrash}/></Button>
-                                </div>
-                            </TableCell>
-                        </TableRow>
+            <div className={"flex flex-row gap-2"}>
+                <p className={"text-xl font-semibold mr-2"}>Server Files</p>
+                <Breadcrumbs variant={"solid"} classNames={{
+                    list: "bg-neutral-800"
+                }}>
+                    <BreadcrumbItem onClick={() => setPath("")}>Root</BreadcrumbItem>
+                    {path.split("/").slice(1).map((part, index) => (
+                        <BreadcrumbItem key={index} onClick={() =>
+                        {
+                            const parts = path.split("/");
+                            parts.splice(index + 1);
+                            setPath(parts.join("/"));
+                        }}>{part}</BreadcrumbItem>
                     ))}
-                </TableBody>
+                </Breadcrumbs>
+                <div className={"ml-auto flex flex-row gap-2"}>
+                    <Tooltip content={"Upload a file or directory."}>
+                        <Button className={"min-w-0"} aria-label="Upload a file or directory."><FontAwesomeIcon icon={faUpload}/></Button>
+                    </Tooltip>
+                    <Tooltip content={"Create a new directory."}>
+                        <Button className={"min-w-0"} aria-label="Create a new directory."><FontAwesomeIcon icon={faFolderPlus}/></Button>
+                    </Tooltip>
 
-            </Table>
+                    {selectedItems.length > 0 && (
+                        <div className={"flex flex-row gap-4"}>
+                            <Divider orientation={"vertical"}/>
+                            <Badge content={selectedItems.length} showOutline={false} color={"primary"}>
+                                <Tooltip content={`Archive and download ${selectedItems.length} files`}>
+                                    <Button className={"min-w-0"} aria-label="Archive and download files"><FontAwesomeIcon icon={faFileDownload}/></Button>
+                                </Tooltip>
+                            </Badge>
+                            <Badge content={selectedItems.length} showOutline={false} color={"primary"}>
+                                <Tooltip content={`Archive ${selectedItems.length} files`}>
+                                    <Button className={"min-w-0"} aria-label="Archive files"><FontAwesomeIcon icon={faFileArchive}/></Button>
+                                </Tooltip>
+                            </Badge>
+                            <Badge content={selectedItems.length} showOutline={false} color={"primary"}>
+                                <Tooltip content={`Delete ${selectedItems.length} files`}>
+                                    <Button className={"min-w-0"} aria-label={`Delete ${selectedItems.length} files`} color={"danger"}><FontAwesomeIcon icon={faTrashAlt}/></Button>
+                                </Tooltip>
+                            </Badge>
+                        </div>
+                    )}
+
+                </div>
+            </div>
+            <ServerFilesList
+                files={files}
+                selectedItems={selectedItems}
+                onSelectionChange={setSelectedItems}
+                path={path}
+                onPathChange={setPath}
+                loading={loading}
+            />
         </div>
     );
 }
