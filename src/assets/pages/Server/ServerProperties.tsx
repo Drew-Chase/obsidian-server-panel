1134ea346541c4fb0da27c8892d3cbdfe9c344a0
Drew (Linux) <dcmanproductions@gmail.com>
2024-10-27 14:13:16 -0400

    Refactor ServerProperties to fetch data from server
    Replaced hardcoded properties with dynamic fetching from the server using `useEffect` and `useSelectedServer` hook. Added loading state and skeleton components to improve user experience during data retrieval.

--- src/assets/pages/Server/ServerProperties.tsx
+++ src/assets/pages/Server/ServerProperties.tsx
@@ -1,11 +1,12 @@
 import {setTitle} from "../../../main.tsx";
-import {Input, ScrollShadow} from "@nextui-org/react";
+import {Input, ScrollShadow, Skeleton} from "@nextui-org/react";
 import ExtendedSwitch from "../../components/Extends/ExtendedSwitch.tsx";
-import {useState} from "react";
+import {useEffect, useState} from "react";
 import MagnifyGlass from "../../images/MagnifyGlass.svg.tsx";
 import OInput from "../../components/Extends/OInput.tsx";
+import {useSelectedServer} from "../../providers/SelectedServerProvider.tsx";
 
-interface ServerPropertiesItem
+export interface ServerPropertiesItem
 {
     name: string;
     value: string;
@@ -15,8 +16,21 @@ interface ServerPropertiesItem
 export default function ServerProperties()
 {
     setTitle("Server Properties");
-    const properties: ServerPropertiesItem[] = getServerProperties();
+    const [properties, setProperties] = useState<ServerPropertiesItem[]>([]);
     const [search, setSearch] = useState("");
+    const {server} = useSelectedServer();
+    const [loading, setLoading] = useState(false);
+
+    useEffect(() =>
+    {
+        setLoading(true);
+        if (server == null) return;
+        server
+            .properties()
+            .then(setProperties)
+            .finally(() => setLoading(false));
+    }, [server]);
+
 
     return (
         <div className={"flex flex-col bg-neutral-600 rounded-2xl shadow-lg p-8 grow w-full mx-2 gap-4"}>
@@ -32,93 +46,38 @@ export default function ServerProperties()
                 autoFocus
                 variant={"underlined"}
             />
-            <ScrollShadow className={"flex flex-col max-h-[calc(100dvh_-_250px)] gap-4 overflow-y-auto pr-4 shrink-0"}>
-                {properties
-                    .filter(({name}) => name.replace(/[-._]/g, " ").toLowerCase().includes(search.toLowerCase()))
-                    .sort((a, b) => a.name.localeCompare(b.name))
-                    .map(({name, value, type}) => (
-                        type === "string" || type === "number" ? (
-                            <OInput
-                                label={name}
-                                value={value}
-                            />
-                        ) : (
-                            <ExtendedSwitch
-                                label={name}
-                                toggle={value === "true"}
-                                className={"max-w-full shrink-0"}
-                            />
-                        )
-                    ))}
+            <ScrollShadow className={"flex flex-col max-h-[calc(100dvh_-_305px)] h-dvh gap-4 overflow-y-auto pr-4 shrink-0 pb-8"}>
+                {loading ? <>{Array.from({length: 10}).map(() => (<Skeleton><OInput/></Skeleton>))}</> :
+                    <>
+                        {properties
+                            .filter(({name}) => name.replace(/[-._]/g, " ").toLowerCase().includes(search.toLowerCase()))
+                            .sort((a, b) => a.name.localeCompare(b.name))
+                            .map(({name, value, type}) => (
+                                type === "string" || type === "number" ? (
+                                    <OInput
+                                        key={name}
+                                        label={name}
+                                        defaultValue={value}
+                                        onValueChange={value =>
+                                        {
+                                            server?.updateProperty(name, value);
+                                        }}
+                                    />
+                                ) : (
+                                    <ExtendedSwitch
+                                        key={name}
+                                        label={name}
+                                        toggle={value === "true"}
+                                        onToggle={async (toggle) =>
+                                        {
+                                            await server?.updateProperty(name, toggle);
+                                        }}
+                                        className={"max-w-full shrink-0"}
+                                    />
+                                )
+                            ))}
+                    </>}
             </ScrollShadow>
         </div>
     );
-}
-
-function getServerProperties(): ServerPropertiesItem[]
-{
-    return [
-        {name: "level-name", value: "world", type: "string"},
-        {name: "allow-flight", value: "false", type: "boolean"},
-        {name: "server-port", value: "25565", type: "number"},
-        {name: "accepts-transfers", value: "false", type: "boolean"},
-        {name: "allow-nether", value: "true", type: "boolean"},
-        {name: "broadcast-console-to-ops", value: "true", type: "boolean"},
-        {name: "broadcast-rcon-to-ops", value: "true", type: "boolean"},
-        {name: "bug-report-link", value: "", type: "string"},
-        {name: "debug", value: "false", type: "boolean"},
-        {name: "difficulty", value: "easy", type: "string"},
-        {name: "enable-command-block", value: "false", type: "boolean"},
-        {name: "enable-jmx-monitoring", value: "false", type: "boolean"},
-        {name: "enable-query", value: "false", type: "boolean"},
-        {name: "enable-rcon", value: "false", type: "boolean"},
-        {name: "enable-status", value: "true", type: "boolean"},
-        {name: "enforce-secure-profile", value: "true", type: "boolean"},
-        {name: "enforce-whitelist", value: "false", type: "boolean"},
-        {name: "entity-broadcast-range-percentage", value: "100", type: "number"},
-        {name: "force-gamemode", value: "false", type: "boolean"},
-        {name: "function-permission-level", value: "2", type: "number"},
-        {name: "gamemode", value: "survival", type: "string"},
-        {name: "generate-structures", value: "true", type: "boolean"},
-        {name: "generator-settings", value: "{}", type: "string"},
-        {name: "hardcore", value: "false", type: "boolean"},
-        {name: "hide-online-players", value: "false", type: "boolean"},
-        {name: "initial-disabled-packs", value: "", type: "string"},
-        {name: "initial-enabled-packs", value: "vanilla", type: "string"},
-        {name: "level-seed", value: "", type: "string"},
-        {name: "level-type", value: "minecraft\\:normal", type: "string"},
-        {name: "log-ips", value: "true", type: "boolean"},
-        {name: "max-chained-neighbor-updates", value: "1000000", type: "number"},
-        {name: "max-players", value: "20", type: "number"},
-        {name: "max-tick-time", value: "60000", type: "number"},
-        {name: "max-world-size", value: "29999984", type: "number"},
-        {name: "motd", value: "A Minecraft Server", type: "string"},
-        {name: "network-compression-threshold", value: "256", type: "number"},
-        {name: "online-mode", value: "true", type: "boolean"},
-        {name: "op-permission-level", value: "4", type: "number"},
-        {name: "player-idle-timeout", value: "0", type: "number"},
-        {name: "prevent-proxy-connections", value: "false", type: "boolean"},
-        {name: "pvp", value: "true", type: "boolean"},
-        {name: "query.port", value: "25565", type: "number"},
-        {name: "rate-limit", value: "0", type: "number"},
-        {name: "rcon.password", value: "", type: "string"},
-        {name: "rcon.port", value: "25575", type: "number"},
-        {name: "region-file-compression", value: "deflate", type: "string"},
-        {name: "require-resource-pack", value: "false", type: "boolean"},
-        {name: "resource-pack", value: "", type: "string"},
-        {name: "resource-pack-id", value: "", type: "string"},
-        {name: "resource-pack-prompt", value: "", type: "string"},
-        {name: "resource-pack-sha1", value: "", type: "string"},
-        {name: "server-ip", value: "", type: "string"},
-        {name: "simulation-distance", value: "10", type: "number"},
-        {name: "spawn-animals", value: "true", type: "boolean"},
-        {name: "spawn-monsters", value: "true", type: "boolean"},
-        {name: "spawn-npcs", value: "true", type: "boolean"},
-        {name: "spawn-protection", value: "16", type: "number"},
-        {name: "sync-chunk-writes", value: "true", type: "boolean"},
-        {name: "text-filtering-config", value: "", type: "string"},
-        {name: "use-native-transport", value: "true", type: "boolean"},
-        {name: "view-distance", value: "10", type: "number"},
-        {name: "white-list", value: "false", type: "boolean"}
-    ];
 }
