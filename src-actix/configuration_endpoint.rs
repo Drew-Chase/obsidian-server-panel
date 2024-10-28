bd0484a832dc032c4c2b499e507badb1854de9c2
Drew Chase (Work) <dcmanproductions@gmail.com>
2024-10-28 14:02:18 -0400

    Add configuration endpoint to Actix
    This commit introduces a new endpoint for configuration management in the Actix web application. It includes both GET and POST routes to retrieve and update the configuration respectively.

---
+++ src-actix/configuration_endpoint.rs
@@ -0,0 +1,12 @@
+use actix_web::{get, post, web, HttpResponse, Responder};
+
+#[get("")]
+pub async fn get_configuration() -> impl Responder {
+    HttpResponse::Ok().json("Configuration")
+}
+#[post("")]
+pub async fn update_configuration(
+    body: web::Json<configuration::config::ObsidianConfig>,
+) -> impl Responder {
+    HttpResponse::Ok().json("Configuration")
+}
