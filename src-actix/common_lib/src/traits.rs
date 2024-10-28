b908889eef8c411d25d983c2807a8b1ec67f16b0
Drew Chase (Work) <dcmanproductions@gmail.com>
2024-10-28 14:02:27 -0400

    Add `TransformPath` trait for path manipulations
    Introduced a new `TransformPath` trait providing methods to normalize, convert to absolute, and convert to relative paths. Implemented this trait for `PathBuf` with handling for canonicalization, current directory retrieval, and path prefix stripping. This addition enhances the path manipulation functionality within the `common_lib` module.

---
+++ src-actix/common_lib/src/traits.rs
@@ -0,0 +1,43 @@
+use std::path::PathBuf;
+
+pub trait TransformPath {
+    fn normalize(&self) -> PathBuf;
+    fn to_absolute(&self) -> Result<PathBuf, Box<dyn std::error::Error>>;
+    fn to_relative(&self) -> Result<PathBuf, Box<dyn std::error::Error>>;
+}
+impl TransformPath for PathBuf {
+    fn normalize(&self) -> PathBuf {
+        let path = self.canonicalize().expect("Failed to canonicalize path");
+        let path_str = path.to_str().expect("Failed to convert path to string");
+
+        // If the path starts with the extended-length path prefix, remove it.
+        let normalized_path = if path_str.starts_with(r"\\?\") {
+            &path_str[4..]
+        } else {
+            path_str
+        };
+
+        PathBuf::from(normalized_path)
+    }
+    fn to_absolute(&self) -> Result<PathBuf, Box<dyn std::error::Error>> {
+        let current_exe = std::env::current_exe()?;
+        let current_dir = current_exe.parent();
+        if let Some(dir) = current_dir {
+            let abs_path = dir.join(self);
+            Ok(abs_path)
+        } else {
+            Err("Failed to get current directory".into())
+        }
+    }
+
+    fn to_relative(&self) -> Result<PathBuf, Box<dyn std::error::Error>> {
+        let current_exe = std::env::current_exe()?;
+        let current_dir = current_exe.parent();
+        if let Some(dir) = current_dir {
+            let rel_path = self.strip_prefix(dir)?;
+            Ok(rel_path.to_path_buf())
+        } else {
+            Err("Failed to get current directory".into())
+        }
+    }
+}
