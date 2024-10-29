3780a3b9c2704024b8237a4c5ea06982298d3852
Drew (Linux) <dcmanproductions@gmail.com>
2024-10-29 18:31:07 -0400

    Add NotificationDropdown component to Actionbar
    Introduced a new `NotificationDropdown` component in the navigation action bar. The component features a popover with notification tabs and options to mark notifications as read. It integrates with NextUI and FontAwesome for UI elements.

---
+++ src/assets/components/Navigation/Actionbar/NotificationDropdown.tsx
@@ -0,0 +1,45 @@
+import {Badge, Button, Chip, PopoverContent, PopoverTrigger, Tab, Tabs, Tooltip} from "@nextui-org/react";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faBell} from "@fortawesome/free-solid-svg-icons";
+import OPopover from "../../Extends/OPopover.tsx";
+
+export default function NotificationDropdown()
+{
+    return (
+
+        <OPopover isOpen={true}>
+            <PopoverTrigger>
+                <div>
+                    <Badge content={"12"} showOutline={false} variant={"shadow"} hidden={false}>
+                        <div className={"p-1 cursor-pointer"}>
+                            <Tooltip content={"Notifications"}>
+                                <FontAwesomeIcon icon={faBell} fontSize={18} className={"cursor-pointer text-white opacity-50 hover:opacity-100 transition-all text-xl"}/>
+                            </Tooltip>
+                        </div>
+                    </Badge>
+                </div>
+            </PopoverTrigger>
+            <PopoverContent>
+                <div className={"flex flex-col min-w-[380px] p-0"}>
+                    <div className="flex w-full items-center justify-between px-5 py-2">
+                        <div className="inline-flex items-center gap-1">
+                            <h4 className="inline-block align-middle text-large font-medium">Notifications</h4>
+                            <Chip>12</Chip>
+                        </div>
+                        <Button
+                            variant={"light"}
+                            color={"primary"}
+                        >
+                            Mark all as read
+                        </Button>
+                    </div>
+                    <Tabs variant={"underlined"} color={"primary"}>
+                        <Tab title={<><span className={"mr-2"}>All</span> <Chip>9</Chip></>} />
+                        <Tab title={<><span className={"mr-2"}>Unread</span> <Chip>3</Chip></>} />
+                        <Tab title={<><span className={"mr-2"}>Archived</span> <Chip hidden>0</Chip></>} />
+                    </Tabs>
+                </div>
+            </PopoverContent>
+        </OPopover>
+    );
+}
