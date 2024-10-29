1907a0abb6ee719c26c13d7af448e3aa4c1ff981
Drew (Linux) <dcmanproductions@gmail.com>
2024-10-29 18:30:20 -0400

    Add push_notification macro and push function
    Introduces the `push_notification` macro for creating Notification objects. Also added a `push` function to streamline Notification instantiation with given parameters.

---
+++ src-actix/notifications/src/macros.rs
@@ -0,0 +1,35 @@
+use crate::data::{Notification, SenderType};
+
+#[macro_export]
+macro_rules! push_notification {
+	($title:expr, $message:expr, $sender:expr, $sender_type:expr) => {
+		let notification = Notification{
+			id: 0,
+			title: $title.to_string(),
+			message: $message.to_string(),
+			read: false,
+			archived: false,
+			action: vec![],
+			sender: $sender.to_string(),
+			sender_type: $sender_type,
+			date: "".to_string(),
+		}
+	};
+}
+
+
+fn push(title:String, message:String, sender:String, sender_type:SenderType) -> Notification {
+	Notification{
+		id: "".to_string(),
+		title: title,
+		message: message,
+		read: false,
+		archived: false,
+		action: vec![],
+		sender: sender,
+		receiver: "".to_string(),
+		sender_type: sender_type,
+		date: "".to_string(),
+	}
+	
+}
