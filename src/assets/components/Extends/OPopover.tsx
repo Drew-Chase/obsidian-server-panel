cf796ba7e6eaabe6a363f16ef056a8c4ffe19569
Drew (Linux) <dcmanproductions@gmail.com>
2024-10-29 18:31:35 -0400

    Enhance OPopover style and include children props
    Added a content class styling to make the popover full-width with a blurred background. Also included children components in the popover to enhance flexibility and usability.

--- src/assets/components/Extends/OPopover.tsx
+++ src/assets/components/Extends/OPopover.tsx
@@ -4,8 +4,13 @@ export default function OPopover(props: PopoverProps)
 {
     return (
         <Popover
+            classNames={{
+                ...props.classNames,
+                content: "w-full bg-default-100/75  backdrop-blur-md"
+            }}
             {...props}
         >
+            {props.children}
         </Popover>
     );
 }
