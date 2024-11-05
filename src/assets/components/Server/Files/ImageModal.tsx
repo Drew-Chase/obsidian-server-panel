77f3d5698ac416658b31be710045104346faa96b
Drew Chase (Work) <dcmanproductions@gmail.com>
2024-11-05 16:33:28 -0500

    Add ImageModal component for image previews
    This new component displays an image in a modal dialog with optional title and close functionality. It supports both base64 and URL image types and integrates with the NextUI library.

---
+++ src/assets/components/Server/Files/ImageModal.tsx
@@ -0,0 +1,34 @@
+import {Button, Image, Modal, ModalBody, ModalContent, ModalFooter, ModalHeader} from "@nextui-org/react";
+
+interface ImageModalProps
+{
+    title?: string;
+    image?: string | null;
+    type?: "base64" | "url";
+    onClose?: () => void;
+}
+
+export default function ImageModal(props: ImageModalProps)
+{
+    return (
+        <Modal isOpen={props.image !== null}>
+            <ModalContent>
+                {onClose => (
+                    <>
+                        <ModalHeader>{props.title || "Image Preview"}</ModalHeader>
+                        <ModalBody>
+                            <div className={"rounded-lg bg-neutral-800/50 text-white/75 text-[150px] flex justify-center items-center p-4 w-[200px] mx-auto"}>
+                                <Image src={props.image || ""} alt={props.title} />
+                            </div>
+                        </ModalBody>
+
+                        <ModalFooter>
+                            <Button color={"primary"} onClick={onClose}>Close</Button>
+                        </ModalFooter>
+
+                    </>
+                )}
+            </ModalContent>
+        </Modal>
+    );
+}
