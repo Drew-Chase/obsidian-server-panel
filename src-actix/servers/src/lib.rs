bdca0bdc664679c811fb26ab5ef87ebb15840554
Drew Chase (Home) <dcmanproductions@gmail.com>
2024-12-09 13:44:19 -0500

    Enforce stricter linting and add server modules.
    Implement clippy lints to prevent unwrap, expect, and panic usage, enhancing code safety and reliability. Introduce essential server-related modules, laying the groundwork for expanded functionality.

---
+++ src-actix/servers/src/lib.rs
@@ -0,0 +1,8 @@
+#![deny(clippy::unwrap_used)]
+#![deny(clippy::expect_used)]
+#![deny(clippy::panic)]
+#![deny(unused_must_use)]
+pub mod server;
+pub mod start_executable_type;
+pub mod server_status;
+pub mod server_database;
