939a59aa705164a2ced547a47216a242afba22fa
Drew Chase (Home) <dcmanproductions@gmail.com>
2024-12-09 14:03:05 -0500

    Add detailed documentation and improve serialization
    Expanded code documentation to clarify traits and struct purposes. Enhanced serialization logic for `Server<u64>` with additional comments, ensuring better understanding and maintainability. This update also handles id hashing more robustly by implementing `decode` functionality for `SimpleHashedIdentifier`.

--- src-actix/servers/src/server.rs
+++ src-actix/servers/src/server.rs
@@ -1,5 +1,5 @@
 use crate::server_status::ServerStatus;
-use crypto::hashids::encode;
+use crypto::hashids::{decode, encode};
 use serde::de::{self, Deserializer, MapAccess, Visitor};
 use serde::{Deserialize, Serialize, Serializer};
 use std::any::Any;
@@ -9,49 +9,71 @@ use std::path::PathBuf;
 use std::str::FromStr;
 use tokio::io::Stdout;
 
+/// The `SimpleHashedIdentifier` structure is used to represent
+/// an identifier in a hashed form, using the `serde_derive::Serialize` trait to enable serialization.
 #[derive(serde_derive::Serialize)]
 struct SimpleHashedIdentifier {
+    /// Holds the hashed value of the identifier
     hashed: String,
 }
 
+/// The `SimpleIdentifier` structure is intended as a straightforward identifier
+/// represented as a `u64`. Currently marked as dead code since it's not used.
 #[derive(serde_derive::Serialize)]
 struct SimpleIdentifier {
+    /// Holds the identifier in a simple numeric form
     id: u64,
 }
 
+/// `Identifier` trait defines the behavior for converting objects
+/// to a numeric form and into a hashed representation.
 pub trait Identifier {
+    /// Converts the identifier into a `u64` if possible.
     fn as_u64(&self) -> Option<u64>;
+
+    /// Converts this identifier into a hashed form.
     fn to_hashed(&self) -> Option<Box<dyn HashedIdentifier>>;
 
+    /// Returns a reference to the object as a trait object of `Any`.
     fn as_any(&self) -> &dyn Any;
 }
 
+/// `HashedIdentifier` trait is intended for identifiers that can return back
+/// to their original `u64` form from a hashed state.
 pub trait HashedIdentifier {
+    /// Attempts to recover the original `u64` from the hashed identifier.
     fn original(&self) -> Option<u64>;
+
+    /// Returns a reference to the object as a trait object of `Any`.
     fn as_any(&self) -> &dyn Any;
 }
 
+/// Implementation of the `Identifier` trait for `u64` values.
 impl Identifier for u64 {
     fn as_u64(&self) -> Option<u64> {
-        Some(*self)
+        Some(*self) // Returns the `u64` directly
     }
+
     fn to_hashed(&self) -> Option<Box<dyn HashedIdentifier>> {
+        // Encodes the `u64` into a hashed representation
         Some(Box::new(SimpleHashedIdentifier { hashed: encode(&[*self]) }))
     }
+
     fn as_any(&self) -> &dyn Any {
         self
     }
 }
 
-impl Identifier for String{
+/// Implementation of the `Identifier` trait for `String`.
+impl Identifier for String {
     fn as_u64(&self) -> Option<u64> {
+        // Attempts to parse the string as a `u64`
         u64::from_str(self).ok()
     }
 
     fn to_hashed(&self) -> Option<Box<dyn HashedIdentifier>> {
-        Some(Box::new(SimpleHashedIdentifier{
-            hashed: self.to_string(),
-        }))
+        // Creates a `SimpleHashedIdentifier` from the string
+        Some(Box::new(SimpleHashedIdentifier { hashed: self.to_string() }))
     }
 
     fn as_any(&self) -> &dyn Any {
@@ -59,91 +81,170 @@ impl Identifier for String{
     }
 }
 
+/// Provides a custom display implementation for any `HashedIdentifier`.
 impl fmt::Display for dyn HashedIdentifier {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         if let Some(simple_hashed_identifier) = self.as_any().downcast_ref::<SimpleHashedIdentifier>() {
+            // Displays the hashed value
             write!(f, "{}", simple_hashed_identifier.hashed)
         } else {
+            // Fallback message if downcasting fails
             write!(f, "Invalid HashedIdentifier")
         }
     }
 }
 
+/// Represents a server entity with attributes commonly used for server management and configuration.
 pub struct Server<T: Identifier> {
+    /// The server's unique identifier, constrained by the `Identifier` trait.
     pub id: T,
+    /// The name of the server, used for display and identification purposes.
     pub name: String,
+    /// The owner's unique identifier, typically representing a user or an account.
     pub owner: u64,
+    /// A list of identifiers for each member associated with this server.
     pub members: Vec<u64>,
+    /// Specifies the minimum required RAM (in MB) for the server to function.
     pub min_ram: u64,
+    /// Specifies the maximum allowable RAM (in MB) for the server.
     pub max_ram: u64,
+    /// Indicates whether the server should automatically start when the system boots.
     pub auto_start: bool,
+    /// An optional file path to a script used to initiate the server.
     pub start_script: Option<PathBuf>,
+    /// Optional arguments specific to Minecraft, affecting how the game runs.
     pub minecraft_arguments: Option<String>,
+    /// Optional Java arguments, influencing the Java Virtual Machine's behavior.
     pub java_arguments: Option<String>,
+    /// Represents the type of server loader being utilized, often an internal configuration value.
     pub loader_type: u8,
+    /// An optional string indicating the version of the server loader in use.
     pub loader_version: Option<String>,
+    /// Specifies the path to the server's operational working directory.
     pub directory: PathBuf,
+    /// The creation timestamp of the server, formatted in ISO 8601.
     pub created_at: String,
+    /// The timestamp of the last update made to the server, also formatted in ISO 8601.
     pub updated_at: String,
+    /// Represents the current operational status of the server, using the `ServerStatus` enum.
     pub status: Option<ServerStatus>,
+    /// Configuration related to the server's standard input stream.
     pub stdin: Option<Stdin>,
+    /// Configuration related to the server's standard output stream.
     pub stdout: Option<Stdout>,
 }
 
+// Default implementation for `Server<u64>`.
 impl Default for Server<u64> {
+    /// Provides a default implementation for the `Server<u64>` struct, returning a server
+    /// with pre-set initial values for all fields.
+    ///
+    /// A `Server<String>` instance with default values:
+    ///
+    /// - `id`: `0`.
+    /// - `name`: Empty string.
+    /// - `owner`: `0`.
+    /// - `members`: An empty vector.
+    /// - `auto_start`: `false`.
+    /// - `min_ram`: `0`.
+    /// - `max_ram`: `0`.
+    /// - `start_script`: `None`.
+    /// - `minecraft_arguments`: `None`.
+    /// - `java_arguments`: `None`.
+    /// - `loader_type`: `0`.
+    /// - `loader_version`: `None`.
+    /// - `directory`: New `PathBuf`.
+    /// - `created_at`: `"1970-01-01T00:00:00Z"`.
+    /// - `updated_at`: `"1970-01-01T00:00:00Z"`.
+    /// - `status`: `None`.
+    /// - `stdin`: `None`.
+    /// - `stdout`: `None`.
+    /// 
+    /// # Returns
+    /// * `Self` - An instance of `Server<u64>` with default values.
     fn default() -> Self {
         Self {
-            id: 0,
-            name: String::from(""),
-            owner: 0,
-            members: Vec::new(),
-            auto_start: false,
-            min_ram: 0,
-            max_ram: 0,
-            start_script: None,
-            minecraft_arguments: None,
-            java_arguments: None,
-            loader_type: 0,
-            loader_version: None,
-            directory: PathBuf::new(),
-            created_at: String::from("1970-01-01T00:00:00Z"),
-            updated_at: String::from("1970-01-01T00:00:00Z"),
-            status: None,
-            stdin: None,
-            stdout: None,
+            id: 0,                                            // Default ID, set to the lowest possible value for a new server.
+            name: String::from(""),                           // Empty name string for the server.
+            owner: 0,                                         // Default owner ID, initially unattached or new.
+            members: Vec::new(),                              // Initialize with an empty membership list.
+            auto_start: false,                                // By default, the server does not start automatically.
+            min_ram: 0,                                       // Minimum RAM requirement set to zero initially.
+            max_ram: 0,                                       // Maximum RAM allowance is also zero initially.
+            start_script: None,                               // No start script is associated by default.
+            minecraft_arguments: None,                        // No Minecraft-specific arguments provided.
+            java_arguments: None,                             // JVM arguments are absent by default.
+            loader_type: 0,                                   // Default loader type, often a placeholder value.
+            loader_version: None,                             // Loader version not specified initially.
+            directory: PathBuf::new(),                        // New path buffer for the server's directory.
+            created_at: String::from("1970-01-01T00:00:00Z"), // Default epoch start time in ISO 8601.
+            updated_at: String::from("1970-01-01T00:00:00Z"), // Default epoch update time in ISO 8601.
+            status: None,                                     // Server status is undefined by default.
+            stdin: None,                                      // Standard input stream configuration is absent.
+            stdout: None,                                     // Standard output stream configuration is absent.
         }
     }
 }
 
-impl Default for Server<String>{
+// Default implementation for `Server<String>`.
+impl Default for Server<String> {
+    /// Provides a default implementation for the `Server<String>` struct.
+    ///
+    /// This function initializes an instance of `Server<String>` with default values,
+    /// filling fields with neutral or placeholder values. It is particularly useful for
+    /// creating new instances without manually setting each field.
+    ///
+    /// # Returns
+    ///
+    /// A `Server<String>` instance with default values:
+    ///
+    /// - `id`: Empty string.
+    /// - `name`: Empty string.
+    /// - `owner`: `0`.
+    /// - `members`: An empty vector.
+    /// - `auto_start`: `false`.
+    /// - `min_ram`: `0`.
+    /// - `max_ram`: `0`.
+    /// - `start_script`: `None`.
+    /// - `minecraft_arguments`: `None`.
+    /// - `java_arguments`: `None`.
+    /// - `loader_type`: `0`.
+    /// - `loader_version`: `None`.
+    /// - `directory`: New `PathBuf`.
+    /// - `created_at`: `"1970-01-01T00:00:00Z"`.
+    /// - `updated_at`: `"1970-01-01T00:00:00Z"`.
+    /// - `status`: `None`.
+    /// - `stdin`: `None`.
+    /// - `stdout`: `None`.
     fn default() -> Self {
         Self {
-            id: "".to_string(),
-            name: String::from(""),
-            owner: 0,
-            members: Vec::new(),
-            auto_start: false,
-            min_ram: 0,
-            max_ram: 0,
-            start_script: None,
-            minecraft_arguments: None,
-            java_arguments: None,
-            loader_type: 0,
-            loader_version: None,
-            directory: PathBuf::new(),
-            created_at: String::from("1970-01-01T00:00:00Z"),
-            updated_at: String::from("1970-01-01T00:00:00Z"),
-            status: None,
-            stdin: None,
-            stdout: None,
+            id: "".to_string(),                               // Default empty string for server ID
+            name: String::from(""),                           // Default empty string for server name
+            owner: 0,                                         // Default owner ID of 0
+            members: Vec::new(),                              // Default to an empty list of members
+            auto_start: false,                                // Default auto-start to false
+            min_ram: 0,                                       // Default minimum RAM to 0 MB
+            max_ram: 0,                                       // Default maximum RAM to 0 MB
+            start_script: None,                               // Default start script to None
+            minecraft_arguments: None,                        // Default Minecraft arguments to None
+            java_arguments: None,                             // Default Java arguments to None
+            loader_type: 0,                                   // Default loader type as 0
+            loader_version: None,                             // Default loader version to None
+            directory: PathBuf::new(),                        // Default directory as an empty PathBuf
+            created_at: String::from("1970-01-01T00:00:00Z"), // Default creation timestamp
+            updated_at: String::from("1970-01-01T00:00:00Z"), // Default updated timestamp
+            status: None,                                     // Default status to None
+            stdin: None,                                      // Default standard input to None
+            stdout: None,                                     // Default standard output to None
         }
     }
 }
 
+// Implementation of `HashedIdentifier` trait for `SimpleHashedIdentifier`.
 impl HashedIdentifier for SimpleHashedIdentifier {
     fn original(&self) -> Option<u64> {
-        // Implement the logic to convert `hashed` back to `u64`, if possible
-        None
+        // Attempts to decode the hashed string back to its original `u64`
+        decode(self.hashed.as_str()).ok()?.first().copied()
     }
 
     fn as_any(&self) -> &dyn Any {
@@ -151,32 +252,69 @@ impl HashedIdentifier for SimpleHashedIdentifier {
     }
 }
 
-impl<T: Identifier> Server<T> {}
-
+// Serialization logic for `Server<u64>`.
 impl Serialize for Server<u64> {
     fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
     where
         S: Serializer,
     {
+        // Uses `serde` to serialize the fields of the `Server` struct
         use serde::ser::SerializeStruct;
 
+        // Starts the serialization process by creating a serialized representation
+        // of the `Server` struct with the specified number of fields (17 in this case)
         let mut state = serializer.serialize_struct("Server", 17)?;
-        state.serialize_field("id", &self.id.to_hashed().unwrap().to_string())?;
+
+        // Serializes the `id` field; uses a custom function to convert it to a hashed string
+        // If the `id` hashing fails, it defaults to "invalid id"
+        state.serialize_field("id", &self.id.to_hashed().map_or_else(|| "invalid id".to_string(), |id| id.to_string()))?;
+
+        // Serializes the `name` field of the server
         state.serialize_field("name", &self.name)?;
+
+        // Serializes the `owner` field; represents the owner information
         state.serialize_field("owner", &self.owner)?;
+
+        // Serializes the `members` field; represents the list of members associated with the server
         state.serialize_field("members", &self.members)?;
+
+        // Serializes the `min_ram` field; represents the minimum required RAM for the server
         state.serialize_field("min_ram", &self.min_ram)?;
+
+        // Serializes the `max_ram` field; represents the maximum RAM the server can use
         state.serialize_field("max_ram", &self.max_ram)?;
+
+        // Serializes the `auto_start` field; indicates if the server should start automatically
         state.serialize_field("auto_start", &self.auto_start)?;
+
+        // Serializes the `start_script` field; holds the script content needed to start the server
         state.serialize_field("start_script", &self.start_script)?;
+
+        // Serializes the `minecraft_arguments` field; contains arguments for Minecraft execution
         state.serialize_field("minecraft_arguments", &self.minecraft_arguments)?;
+
+        // Serializes the `java_arguments` field; holds the Java arguments used in server execution
         state.serialize_field("java_arguments", &self.java_arguments)?;
+
+        // Serializes the `loader_type` field; identifies the type of loader being used
         state.serialize_field("loader_type", &self.loader_type)?;
+
+        // Serializes the `loader_version` field; specifies the version of the loader
         state.serialize_field("loader_version", &self.loader_version)?;
+
+        // Serializes the `directory` field; denotes the directory where server files are located
         state.serialize_field("directory", &self.directory)?;
+
+        // Serializes the `created_at` field; records the timestamp when the server was created
         state.serialize_field("created_at", &self.created_at)?;
+
+        // Serializes the `updated_at` field; records the timestamp of the last server update
         state.serialize_field("updated_at", &self.updated_at)?;
+
+        // Serializes the `status` field; reflects the current status of the server
         state.serialize_field("status", &self.status)?;
+
+        // Ends the serialization process for the `Server` struct
         state.end()
     }
 }
@@ -204,7 +342,7 @@ impl<'de> Deserialize<'de> for Server<u64> {
             CreatedAt,
             UpdatedAt,
             Status,
-        };
+        }
 
         struct ServerVisitor;
 
@@ -219,6 +357,7 @@ impl<'de> Deserialize<'de> for Server<u64> {
             where
                 V: MapAccess<'de>,
             {
+                // Declare mutable variables for each field to store parsed values
                 let mut id = None;
                 let mut name = None;
                 let mut owner = None;
@@ -236,20 +375,25 @@ impl<'de> Deserialize<'de> for Server<u64> {
                 let mut updated_at = None;
                 let mut status = None;
 
+                // Iterate over each key-value pair in the map
                 while let Some(key) = map.next_key()? {
+                    // Match each key to its corresponding field
                     match key {
+                        // Parse and assign the 'id' field, ensuring no duplicates
                         Field::Id => {
                             if id.is_some() {
                                 return Err(de::Error::duplicate_field("id"));
                             }
                             id = Some(map.next_value()?);
                         }
+                        // Parse and assign the 'name' field, ensuring no duplicates
                         Field::Name => {
                             if name.is_some() {
                                 return Err(de::Error::duplicate_field("name"));
                             }
                             name = Some(map.next_value()?);
                         }
+                        // Parse and assign other fields similarly, checking for duplicates
                         Field::Owner => {
                             if owner.is_some() {
                                 return Err(de::Error::duplicate_field("owner"));
@@ -336,6 +480,8 @@ impl<'de> Deserialize<'de> for Server<u64> {
                         }
                     }
                 }
+
+                // Validate that all required fields have been set, and handle missing fields
                 let id = id.ok_or_else(|| de::Error::missing_field("id"))?;
                 let name = name.ok_or_else(|| de::Error::missing_field("name"))?;
                 let owner = owner.ok_or_else(|| de::Error::missing_field("owner"))?;
@@ -353,6 +499,7 @@ impl<'de> Deserialize<'de> for Server<u64> {
                 let updated_at = updated_at.ok_or_else(|| de::Error::missing_field("updated_at"))?;
                 let status = status;
 
+                // Construct and return the Server object
                 Ok(Server {
                     id,
                     name,
@@ -397,4 +544,3 @@ impl<'de> Deserialize<'de> for Server<u64> {
         deserializer.deserialize_struct("Server", FIELDS, ServerVisitor)
     }
 }
-
