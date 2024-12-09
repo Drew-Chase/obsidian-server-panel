54c909045d6b5d6b549eb2c3c732c967678af894
Drew Chase (Home) <dcmanproductions@gmail.com>
2024-12-09 10:09:56 -0500

    Refactor ServerProcess structure and streamline imports
    Removed the ServerStatus enum and associated traits, simplifying the ServerProcess structure. Added stdin, stdout, stderr, and exit_code fields to the ServerProcess struct while retaining the pid field and eliminating unnecessary imports and functions for a cleaner implementation.

--- src-actix/servers/src/server_process.rs
+++ src-actix/servers/src/server_process.rs
@@ -1,91 +1,17 @@
 use crate::server_db::Server;
-use lazy_static::lazy_static;
-use serde_derive::{Deserialize, Serialize};
-use std::error::Error;
-use std::fmt::Display;
-use std::str::FromStr;
-
-#[derive(Debug, Serialize, Deserialize, Clone)]
-pub enum ServerStatus {
-    Running,
-    Offline,
-    Restarting,
-    Crashed,
-}
-
-impl Display for ServerStatus {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        match self {
-            ServerStatus::Running => write!(f, "Running"),
-            ServerStatus::Offline => write!(f, "Offline"),
-            ServerStatus::Restarting => write!(f, "Restarting"),
-            ServerStatus::Crashed => write!(f, "Crashed"),
-        }
-    }
-}
-
-impl FromStr for ServerStatus {
-    type Err = String;
-
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        match s {
-            "Running" => Ok(ServerStatus::Running),
-            "Offline" => Ok(ServerStatus::Offline),
-            "Restarting" => Ok(ServerStatus::Restarting),
-            "Crashed" => Ok(ServerStatus::Crashed),
-            _ => Err(format!("Unknown ServerStatus: {}", s)),
-        }
-    }
-}
+use std::io::Stdin;
+use std::process::Stdio;
 
 pub struct ServerProcess {
-    pub server: Server,
-    pid: Option<u32>,
-    process_stdout: Option<String>,
-    pub status: ServerStatus,
-}
-
-trait ToProcess {
-    fn to_process(&self) -> Result<ServerProcess, Box<dyn Error>>;
-}
-
-impl ToProcess for Server {
-    fn to_process(&self) -> Result<ServerProcess, Box<dyn Error>> {
-        Ok(ServerProcess {
-            server: self.clone(),
-            pid: None,
-            process_stdout: None,
-            status: ServerStatus::Offline,
-        })
-    }
-}
-
-lazy_static! {
-    static ref SERVERS: Vec<ServerProcess> = vec![];
+    pub stdin: Stdin,
+    pub stdout: Stdin,
+    pub stderr: Stdin,
+    pub pid: i32,
+    pub exit_code: Option<i32>,
 }
 
 impl ServerProcess {
-    pub fn new(server: Server) -> Self {
-        Self {
-            server,
-            pid: None,
-            process_stdout: None,
-            status: ServerStatus::Offline,
-        }
-    }
-
-    pub fn start_server(&mut self) -> Result<(), Box<dyn Error>> {
-        todo!("Implement the start_server function")
-    }
-    pub fn stop_server(&mut self) -> Result<(), Box<dyn Error>> {
-        todo!("Implement the stop_server function")
-    }
-
-    pub fn restart_server(&mut self) -> Result<(), Box<dyn Error>> {
-        todo!("Implement the restart_server function")
-    }
-
-    pub fn kill_server(&mut self) -> Result<(), Box<dyn Error>> {
-        todo!("Implement the kill_server function")
+    pub fn start(server: &Server){
+        
     }
 }
