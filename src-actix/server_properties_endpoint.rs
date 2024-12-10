d2db801365c240e1a6a03e98f6531f1963ff9102
Drew Chase (Home) <dcmanproductions@gmail.com>
2024-12-10 14:45:09 -0500

    Refactor server properties endpoint for cleaner logic.
    Simplified the `get` and `post` endpoints by reusing `Server` methods, reducing code duplication and improving maintainability. This update also improves error handling by utilizing `Result` types for better error propagation.

--- src-actix/server_properties_endpoint.rs
+++ src-actix/server_properties_endpoint.rs
@@ -1,84 +1,37 @@
 use actix_web::{get, post, web, HttpMessage, HttpRequest, HttpResponse, Responder};
 use authentication::data::User;
 use crypto::hashids::decode;
-use log::error;
 use serde_json::json;
-use servers::properties::Properties;
-use servers::server_db;
-use std::path::Path;
+use servers::server::Server;
+use servers::server_database::ServerDatabase;
+use servers::server_properties::ServerProperties;
+use std::error::Error;
 
 #[get("")]
-pub async fn get_server_properties(id: web::Path<String>, req: HttpRequest) -> impl Responder {
+pub async fn get_server_properties(id: web::Path<String>, req: HttpRequest) -> Result<impl Responder, Box<dyn Error>> {
     if let Some(user) = req.extensions().get::<User>() {
-        let id_number: u32 = match decode(id.as_str()) {
-            Ok(id_number) => id_number[0] as u32,
-            Err(_) => return HttpResponse::BadRequest().json(json!({"error":"Invalid ID"})),
+        let id_number = match decode(id.as_str()) {
+            Ok(id_number) => id_number[0],
+            Err(_) => return Ok(HttpResponse::BadRequest().json(json!({"error":"Invalid ID"}))),
         };
-        let server = match server_db::get_server_by_id(id_number) {
-            Some(s) => s,
-            None => {
-                let msg = format!("Server with id: {} not found", id_number);
-                error!("{}", msg);
-                return HttpResponse::BadRequest().json(json!({"error":msg}));
-            }
-        };
-        if server.owner == user.id {
-            let properties: Properties = match Properties::new(&Path::join(
-                Path::new(server.directory.unwrap().as_str()),
-                Path::new("server.properties"),
-            )) {
-                Ok(p) => p,
-                Err(e) => {
-                    error!("{}", e);
-                    return HttpResponse::BadRequest().json(json!({"error":e}));
-                }
-            };
-
-            return HttpResponse::Ok().json(properties.items);
-        }
+        let server = Server::get_owned_server(id_number, user.id as u64)?;
+        return Ok(HttpResponse::Ok().json(server.get_properties()?));
     }
 
-    HttpResponse::Unauthorized().json(json!({"error":"Unauthorized"}))
+    Ok(HttpResponse::Unauthorized().json(json!({"error":"Unauthorized"})))
 }
 #[post("/{key}")]
-pub async fn set_server_property(
-    path: web::Path<(String, String)>,
-    body: String,
-    req: HttpRequest,
-) -> impl Responder {
+pub async fn set_server_property(path: web::Path<(String, String)>, body: String, req: HttpRequest) -> Result<impl Responder, Box<dyn Error>> {
     if let Some(user) = req.extensions().get::<User>() {
         let (id, key) = path.into_inner();
-        let id_number: u32 = match decode(id.as_str()) {
-            Ok(id_number) => id_number[0] as u32,
-            Err(_) => return HttpResponse::BadRequest().json(json!({"error":"Invalid ID"})),
-        };
-        let server = match server_db::get_owned_server_by_id(id_number, user.id) {
-            Some(s) => s,
-            None => {
-                let msg = format!("Server with id: {} not found", id_number);
-                error!("{}", msg);
-                return HttpResponse::BadRequest().json(json!({"error":msg}));
-            }
-        };
-        let mut properties: Properties = match Properties::new(&Path::join(
-            Path::new(server.directory.unwrap().as_str()),
-            Path::new("server.properties"),
-        )) {
-            Ok(p) => p,
-            Err(e) => {
-                error!("{}", e);
-                return HttpResponse::BadRequest().json(json!({"error":e}));
-            }
-        };
 
-        properties.set(&key, &body);
-        return match properties.write() {
-            Ok(_) => HttpResponse::Ok().json(json!({"success":"Property set"})),
-            Err(e) => {
-                error!("{}", e);
-                HttpResponse::BadRequest().json(json!({"error":e}))
-            }
+        let id_number = match decode(id.as_str()) {
+            Ok(id_number) => id_number[0],
+            Err(_) => return Ok(HttpResponse::BadRequest().json(json!({"error":"Invalid ID"}))),
         };
+        let server = Server::get_owned_server(id_number, user.id as u64)?;
+        server.set_property(&*key, &*body)?;
+        return Ok(HttpResponse::Ok().finish());
     }
-    HttpResponse::Unauthorized().json(json!({"error":"Unauthorized"}))
+    Ok(HttpResponse::Unauthorized().json(json!({"error":"Unauthorized"})))
 }
